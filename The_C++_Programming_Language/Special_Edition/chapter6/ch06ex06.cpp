a84ef6190a81587a48bee7a90343fabac8364cd4
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-06-11 14:01:15 +0200

    * Added Exercise 6 from Chapter 6.

---
+++ The_C++_Programming_Language/Special_Edition/chapter6/ch06ex06.cpp
@@ -0,0 +1,15 @@
+/*
+ * (*1) Find 10 different examples of nonportable C++ code.
+ *
+ *
+ * TODO:
+ *   - Find some more examples.
+ */
+
+int main()
+{
+  // Found these on
+  //   http://tomplusplus.blogspot.de/2006/05/665-nonportable-c-code.html
+  char a = 32; //depends on ascii
+  int foo$b a; //depends on extension of allowable names
+}
