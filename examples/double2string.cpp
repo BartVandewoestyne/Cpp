11cbfedc56ab2d383efaca4d45f6d9bc5cb6348d
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-06-21 14:41:38 +0200

    * Minor change.

--- examples/double2string.cpp
+++ examples/double2string.cpp
@@ -1,3 +1,7 @@
+/**
+ * Illustrates how a double can be converted to a string.
+ */
+
 #include <iostream>
 #include <sstream>
 
@@ -7,12 +11,13 @@ int main() {
 
   // This works.
   stringstream s1;
-  s1 << " " << 5.0;
+  s1 << 5.0;
   cout << s1.str() << endl;
 
-  // This doesn't.
+  // This doesn't work.
   //string s2;
   //s2.append(" " + 5.0);
 
   return 0;
+
 }
