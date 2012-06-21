bb6818bff992ee59ba8b93ff5cafd398d5c95f39
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-06-21 10:28:12 +0200

    * Renamed file and some minor modifications.

---
+++ examples/complex.cpp
@@ -0,0 +1,19 @@
+/**
+ * Demonstrate the use of complex numbers.
+ */
+
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+int main()
+{
+  complex<double> a(1, 0);
+  complex<double> b(1, 1);
+
+  cout << a << endl;
+  cout << b << endl;
+
+  return 0;
+}
