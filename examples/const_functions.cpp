abfe74939d110a3850d5948eec1105745b2933c8
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-06-21 10:41:01 +0200

    * Improved content a bit.

--- examples/const_functions.cpp
+++ examples/const_functions.cpp
@@ -1,45 +1,55 @@
-/* The const at the end of a member function tells whether this member
-   function changes the this object or not. */
+/**
+ * Demonstrate the use of const functions:
+ *
+ * The const at the end of a member function tells whether this member
+ * function changes the this object or not.
+ */
 
 class Person {
 
   public:
-    Person( int age_in );
-    int getAge() const;		// A read-only function.
-    void setAge( int age_in );	// A write function.  Can't be const.
+
+    Person(int age_in);
+    int getAge() const;       // A read-only function.
+    void setAge(int age_in);  // A write function.  This one cannot be const.
   
   private:
+
     int age;
 
 };
+
 
 Person::Person(int age_in)
 {
   age = age_in;
 }
 
+
 int Person::getAge() const
 {
-   return age;        // Doesn't modify anything, so better to put const behind it.
+   return age;  // This doesn't modify anything, so better put const behind it.
 }
 
 
 void Person::setAge( int age_in )
 {
-   age = age_in;          // Modifies data member
+   age = age_in;  // Modifies data member, so function cannot have const.
+
+   // Note: if you do make this function const, then you get the error:
+   //
+   //   "error: assignment of data-member ‘Person::age’ in read-only structure"
 }
 
 
 int main()
 {
-   Person myBrother( 28 );
-   const Person myMother( 64 );
+   Person myBrother(28);
+   const Person myMother(64);
 
-   myBrother.getAge();     // Okay
-   myBrother.setAge( 4 );  // Okay
+   myBrother.getAge();    // Okay
+   myBrother.setAge(29);  // Okay
 
-   myMother.getAge();     // Okay
-   //myMother.setAge( 4 );  // Error
-
+   myMother.getAge();      // Okay
+   //myMother.setAge(65);  // Error
 }
-
