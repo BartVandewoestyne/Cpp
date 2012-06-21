a2593f92a2659aa824433931934c3775d4bce52b
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-06-21 14:51:05 +0200

    * Minor changes.

--- examples/vector.cpp
+++ examples/vector.cpp
@@ -1,27 +1,32 @@
+/**
+ * Illustrates the use of the vector class template.
+ */
+
 #include <iostream>
 #include <vector>
+
 using namespace std;
  
 int main()
 {
-    vector<double> myVector;
+  vector<double> myVector;
 
-    myVector.push_back(1);
-    myVector.push_back(2);
-    myVector.push_back(3);
+  myVector.push_back(1);
+  myVector.push_back(2);
+  myVector.push_back(3);
  
-    // disadvantage: we repeatedly call the size() member function!!!
-    for (vector<double>::size_type i = 0; i < myVector.size(); i++)
-    {
-      cout << myVector[i] << endl;
-    }
+  // Disadvantage: we repeatedly call the size() member function!!!
+  for (vector<double>::size_type i = 0; i < myVector.size(); i++)
+  {
+    cout << myVector[i] << endl;
+  }
 
-    for (vector<double>::iterator it = myVector.begin();
-                                  it != myVector.end();
-                                  ++it)
-    {
-        cout << *it << endl;
-    }
+  for (vector<double>::iterator it = myVector.begin();
+                                it != myVector.end();
+                                ++it)
+  {
+    cout << *it << endl;
+  }
  
-    return 0;
+  return 0;
 }
