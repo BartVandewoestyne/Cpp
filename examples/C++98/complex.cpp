e5828a886c775cf703f9a9928f87ac66bf4f0757
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-06-22 16:53:33 +0200

    * Added more examples.

--- examples/C++98/complex.cpp
+++ examples/C++98/complex.cpp
@@ -9,11 +9,23 @@ using namespace std;
 
 int main()
 {
-  complex<double> a(1, 0);
-  complex<double> b(1, 1);
+  complex<double> a(1, 1);
+  complex<double> b(3, 0);
+
+  // This gives the compiler-error:
+  //   error: missing template arguments before ‘b’
+  //complex b(3, 0);
 
   cout << a << endl;
   cout << b << endl;
 
+  // This works.
+  cout << a/b << endl;
+
+  // This gives the error:
+  //   error: no match for ‘operator/’ in ‘a / 3’
+  //cout << a/3 << endl;
+
   return 0;
+
 }
