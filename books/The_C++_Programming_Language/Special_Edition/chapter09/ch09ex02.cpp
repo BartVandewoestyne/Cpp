399aff2e897ceab6b5387a5eef27da070160d2fb
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-06 09:43:22 +0200

    * Added Exercise 2 from Chapter 9.

---
+++ books/The_C++_Programming_Language/Special_Edition/chapter09/ch09ex02.cpp
@@ -0,0 +1,18 @@
+/**
+ * (*2) Where are the headers for nonstandard' 'foundation" libraries kept?
+ */
+
+
+/*
+ * I'm not sure what exactly is meant by 'foundation' library, but I assume
+ * it's just nonstandard libraries such as the following:
+ *
+ *   - Microsoft Foundation Classes
+ *   - Enlightenment Foundation Libraries
+ *   - ...
+ *
+ * These are located under
+ *
+ *   /usr/include
+ *
+ */
