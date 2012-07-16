f2cfe5fd8f89993422faddda876a50e88b234269
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-16 14:13:46 +0200

    * Added program 10.

---
+++ books/How_Not_To_Program_in_C++/program10_faulty.cpp
@@ -0,0 +1,16 @@
+/************************************************
+ * divide -- Program to figure out how many     *
+ *      digits are printed in floating point    *
+ *      by print 1/3 or 0.333333.               *
+ ************************************************/
+#include <iostream>
+
+int main()
+{
+    float result;       // Result of the divide
+
+    result = 1/3;       // Assign result something
+
+    std::cout << "Result is " << result << '\n';
+    return (0);
+}
