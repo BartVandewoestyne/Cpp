5a6ab70b6e2460ef505f37c19ec445380154d196
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-16 11:52:08 +0200

    * Added program 1.

---
+++ books/How_Not_To_Program_in_C++/program01_faulty.cpp
@@ -0,0 +1,9 @@
+/************************************************
+* The "standard" hello world program.           *
+*************************************************/
+#include <iostream>
+
+void main(void)
+{
+    std::cout << "Hello world!\n";
+}
