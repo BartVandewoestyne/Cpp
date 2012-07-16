30e755d4a7bd2be06bf88d739289f1b1fdd752fc
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-16 16:05:28 +0200

    * Added program 23.

---
+++ books/How_Not_To_Program_in_C++/program23_correct.cpp
@@ -0,0 +1,43 @@
+/************************************************
+ * Compute the length of a string entered by    *
+ * the user.                                    *
+ ************************************************/
+#include <iostream>
+
+/************************************************
+ * length -- Find the length of a string        *
+ *      (strlen does a better job.)             *
+ *                                              *
+ * Returns:                                     *
+ *      length of the string.                   *
+ ************************************************/
+static int length(
+    const char string[] // String to check
+)
+{
+    int index;       // index into the string
+
+    /*
+     * Loop until we reach the
+     * end of string character
+     */
+     for (index=0; string[index] != '\0';++index) {
+         /* do nothing */
+     }
+
+     return (index);
+}
+
+int main()
+{
+    char line[100];    // Input line from user
+
+    while (1) {
+        std::cout << "Enter a string: ";
+        std::cin.getline(line, sizeof(line));
+
+        std::cout << "Length is " <<
+            length(line) << '\n';
+    }
+    return (0);
+}
