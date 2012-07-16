d899e619eddf37494b1d4558f4f562a307c86490
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-16 16:26:34 +0200

    * Added program 27.

---
+++ books/How_Not_To_Program_in_C++/program27_faulty.cpp
@@ -0,0 +1,16 @@
+/************************************************
+ * if_test -- Simple test of the if statement.  *
+ ************************************************/
+#include <iostream>
+
+int main()
+{
+    int i1 = 12;        // A number
+    int i2 = 3;         // Another number
+
+    if (i1 & i2)
+        std::cout << "Both numbers are non-zero\n";
+    else
+        std::cout << "At least one number is zero\n";
+    return (0);
+}
