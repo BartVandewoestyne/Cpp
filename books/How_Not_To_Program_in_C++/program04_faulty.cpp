f3aa65696327eb89bc6738a4869bfccdd2cf5ea0
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-16 13:41:33 +0200

    Added program 4.

---
+++ books/How_Not_To_Program_in_C++/program04_faulty.cpp
@@ -0,0 +1,20 @@
+/************************************************
+ * A program to sum the numbers from 1 to 100   *
+ * using a brute force algorithm.               *
+ ************************************************/
+#include <iostream>
+
+int main()
+{
+    int sum;    // The running sum
+    int count;  // The current number
+
+    for (count = 1; count <= 100; ++count)
+        sum += count;
+
+    std::cout <<
+        "The sum of the numbers " <<
+        "between 1 and 100 is " <<
+        sum << '\n';
+    return (0);
+}
