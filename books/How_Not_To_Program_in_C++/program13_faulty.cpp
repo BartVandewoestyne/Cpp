a19c4f19b5ef5331acf20d78f5251f278ae0489a
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-16 14:54:14 +0200

    * Added program 13.

---
+++ books/How_Not_To_Program_in_C++/program13_faulty.cpp
@@ -0,0 +1,38 @@
+/************************************************
+ * Billing -- Print out how much we owe         *
+ *      customers or they owe us.               *
+ ************************************************/
+#include <iostream>
+
+// Number of pennies in a dollar
+const int DOLLAR = 100;
+
+/************************************************
+ * billing -- do the billing.                   *
+ *      If the customer owes us money           *
+ *              -- output debt.                 *
+ *      If we owe more than $100                *
+ *              -- output credit.               *
+ *      Between $0 and $100 just ignore the     *
+ *              account.                        *
+ ************************************************/
+int billing(
+    // Current balance (in cents)
+    const int balance
+) {
+    if (balance < 0)
+        if (balance < - (100*DOLLAR))
+            std::cout << "Credit " << -balance << endl;
+    else
+        std::cout << "Debt " << balance << endl;
+
+    return (0);
+}
+
+int main()
+{
+    /* Test code */
+    billing(50);
+    billing(-10);
+    return (0);
+}
