8a6b24f4ff73c87a15e5c24bae36471336e99776
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-16 15:34:01 +0200

    * Added program 19.

---
+++ books/How_Not_To_Program_in_C++/program19_faulty.cpp
@@ -0,0 +1,26 @@
+/************************************************
+ * prime -- A very dump program to check to see *
+ *       if the numbers 2-9 are prime.          *
+ ************************************************/
+#include <iostream>
+
+int main()
+{
+    int i;       // Number we are checking
+
+    for (i = 2; i < 10; ++i) {
+        switch(i) {
+            case 2:
+            case 3:
+            case 5:
+            case 7:
+                std::cout << i << " is prime\n";
+                break;
+            defualt:
+                std::cout << i <<
+                    " is not prime\n";
+                break;
+        }
+    }
+    return (0);
+}
