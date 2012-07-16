757480544695b4bc5959e6d68b7230c59681e31b
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-16 11:46:05 +0200

    * Added program 5 from 'How not to program in C++'.

---
+++ books/How_Not_To_Program_in_C++/program05_faulty.cpp
@@ -0,0 +1,15 @@
+/************************************************
+ * A program to answer the question of a five   *
+ *      year old:                               *
+ *              "What is 2 + 2?"                *
+ ************************************************/
+#include <iostream>
+
+int main()
+{
+    int result; // Result of the addition
+
+    result = 2+2;
+    std::cout << "The answer is " << result;
+    return (0);
+}
