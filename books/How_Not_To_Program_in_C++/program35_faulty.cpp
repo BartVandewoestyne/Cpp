93b28bd6b44f92745b9f6ffd365551f924979185
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-17 13:26:36 +0200

    * Added program 35.

---
+++ books/How_Not_To_Program_in_C++/program35_faulty.cpp
@@ -0,0 +1,18 @@
+/************************************************
+ * print the value on one million.              *
+ ************************************************/
+#include <iostream>
+
+int main()
+{
+    // Variable to hold a million
+    long int one_million;
+
+    // Set the variable
+    one_million = 1,000,000;
+
+    std::cout <<
+        "One million " << one_million <<
+        std::endl;
+    return (0);
+}
