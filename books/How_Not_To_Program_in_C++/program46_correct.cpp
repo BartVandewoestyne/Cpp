26d4880d6dd67a4d8e9be672084c08943e09dbe8
Bart Vandewoestyne <bart_vandewoestyne@agilent.com>
2012-07-25 10:35:35 +0200

    * Added program 46.

---
+++ books/How_Not_To_Program_in_C++/program46_correct.cpp
@@ -0,0 +1,84 @@
+/************************************************
+ * list -- Test out the command list decoder.   *
+ *                                              *
+ * Read a command from the input and check to   *
+ * see if the command decoder can find it.      *
+ ************************************************/
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+static inline void do_open() {
+   std::cout << "do_open called\n";
+}
+static inline void do_close() {
+   std::cout << "do_close called\n";
+}
+static inline void do_save() {
+   std::cout << "do_save called\n";
+}
+static inline void do_quit() {
+    exit(0);
+}
+/*
+ * The command as a string and
+ * as a function to execute
+ */
+struct cmd_info {
+    char *const cmd;
+    void (*funct)();
+};
+
+/*
+ * List of all possible commands
+ */
+static cmd_info cmd_list[] = {
+    {"open", do_open},
+    {"close", do_close},
+    {"save", do_save},
+    {"quit", do_quit},
+    {NULL, NULL}
+};
+
+/************************************************
+ * do_cmd -- Decode a command an execute it.    *
+ *    If the command is not found, output an    *
+ *    error.                                    *
+ ************************************************/
+static void do_cmd(
+    const char *const cmd
+) {
+    struct cmd_info *cur_cmd;
+
+    cur_cmd = cmd_list;
+
+    //while  ( (cur_cmd->cmd != NULL) &&
+    //         (std::strcmp(cur_cmd->cmd, cmd) != 0) ) {
+    while (1)
+    {
+        if (cur_cmd->cmd == NULL)
+          break;
+        if (std::strcmp(cur_cmd->cmd, cmd) == 0)
+          break;
+        cur_cmd++;
+    }
+    if (cur_cmd->cmd == NULL) { // book solution is also wrong here!!!
+        std::cout << "Command not found\n";
+    } else {
+        cur_cmd->funct();
+    }
+}
+
+/************************************************
+ * main -- Simple test program.                 *
+ ************************************************/
+int main()
+{
+    char cmd[100];
+    while (1) {
+        std::cout << "Cmd: ";
+        std::cin.getline(cmd, sizeof(cmd));
+
+        do_cmd(cmd);
+    }
+}
