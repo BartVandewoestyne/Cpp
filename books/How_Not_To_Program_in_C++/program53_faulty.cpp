fb27ab9a5bfa5731a2d260156b086a7186c9d312
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-07-26 09:51:56 +0200

    * Added program 53.

---
+++ books/How_Not_To_Program_in_C++/program53_faulty.cpp
@@ -0,0 +1,32 @@
+/************************************************
+ * test_max -- Test the max function.           *
+ ************************************************/
+#include <iostream>
+
+/************************************************
+ * max -- return the larger of two integers.    *
+ *                                              *
+ * Returns:                                     *
+ *      biggest of the two numbers.             *
+ ************************************************/
+const int &max(
+    const int &i1,      // A number
+    const int &i2       // Another number
+)
+{
+    if (i1 > i2)
+        return (i1);
+    return (i2);
+}
+
+int main()
+{
+    // I is the biggest of the two expression
+    const int &i = max(1+2, 3+4);
+
+    std::cout <<
+        "The biggest expression is " <<
+        i << std::endl;
+
+    return (0);
+}
