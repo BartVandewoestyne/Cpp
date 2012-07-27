64ff67dccd59ac529160b473610c35ca137339d4
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-07-27 13:46:48 +0200

    * Add program 60.

---
+++ books/How_Not_To_Program_in_C++/program60_faulty.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+/************************************************
+ * tmp_name -- return a temporary file name     *
+ *                                              *
+ * Each time this function is called, a new     *
+ * name will be returned.                       *
+ *                                              *
+ * Returns *
+ *      String containing the name.             *
+ ************************************************/
+std::string &tmp_name()
+{
+    // The name we are generating
+    std::string name;
+
+    // Sequence number for last digit
+    static int sequence = 0;
+
+    ++sequence; // Move to the next file name
+
+    name = "tmp";
+
+    // Put in the squence digit
+    name += static_cast<char>(sequence + '0');
+
+    return(name);
+}
+
+int main()
+{
+    std::string name1 = tmp_name();
+
+    std::cout <<"Name1: " << name1 << '\n';
+    return(0);
+}
