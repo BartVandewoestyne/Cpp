d1a2b01c5aba488d12f6f7ddf8a40e07c5ed6a2d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-07-30 17:06:01 +0200

    * Added program 62.

---
+++ books/How_Not_To_Program_in_C++/program62_faulty.cpp
@@ -0,0 +1,24 @@
+/************************************************
+ * print_zip -- Print out a couple of zip codes.*
+ ************************************************/
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    // Zip code for San Diego
+    const long int san_diego_zip = 92126;
+
+    // Zip code for Boston
+    const long int boston_zip = 02126;
+
+    std::cout << "San Diego " << std::setw(5) <<
+        std::setfill('0') <<
+        san_diego_zip << std::endl;
+
+    std::cout << "Boston " << std::setw(5) <<
+        std::setfill('0') <<
+        boston_zip << std::endl;
+
+    return (0);
+}
