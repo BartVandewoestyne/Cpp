ea48623d3c8b2c3306be7cf23e043ea78f52f4bf
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-07-31 23:29:29 +0200

    * Added program 66.

---
+++ books/How_Not_To_Program_in_C++/program66_faulty.cpp
@@ -0,0 +1,35 @@
+/************************************************
+ * buffer demo.  Show how big buffers can speed *
+ * up I/O.                                      *
+ ************************************************/
+#include <stdio.h>
+
+/* Nice big buffer */
+#define BUF_SIZE  (50 * 1024)
+
+/************************************************
+ * print_stuff -- Print a bunch of stuff in a   *
+ *      big buffer.                             *
+ ************************************************/
+void print_stuff(void)
+{
+    // Buffer to hold the data
+    char buffer[BUF_SIZE];
+
+    // Printing counter.
+    int i;
+
+    /* Things go much faster with this */
+    setbuf(stdout, buffer);
+
+    for (i = 0; i < 10; ++i)
+        printf("Hello world\n");
+}
+
+
+int main()
+{
+    print_stuff();
+    printf("That's all\n");
+    return (0);
+}
