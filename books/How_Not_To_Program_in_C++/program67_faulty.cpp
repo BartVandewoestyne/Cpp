351b09178b48016dd386fd4746f2fe24c716ac2e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-07-31 23:36:08 +0200

    * Added program 67.

---
+++ books/How_Not_To_Program_in_C++/program67_faulty.cpp
@@ -0,0 +1,34 @@
+/************************************************
+ * Compute a simple average.  Because this      *
+ * takes a long time (?) we output some         *
+ * chatter as we progress through the system.   *
+ ************************************************/
+#include <stdio.h>
+
+/************************************************
+ * average -- Compute the average given the     *
+ *      total of the series and the number      *
+ *      of items in the series.                 *
+ *                                              *
+ * Returns:                                     *
+ *      The average.                            *
+ ************************************************/
+int average(
+    const int total,// The total of the series
+    const int count // The number of items
+)
+{
+    return (total/count);
+}
+
+int main()
+{
+    int ave;    // Average of the number
+
+    printf("Starting....");
+    ave = average(32, 0);
+    printf("..done\n");
+
+    printf("The answer is %d\n", ave);
+    return (0);
+}
