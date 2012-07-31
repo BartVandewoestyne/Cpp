feb29e80e33ecb8c4cbb93e284e9d2815ce2b873
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-07-31 21:30:46 +0200

    * Added program 64.

---
+++ books/How_Not_To_Program_in_C++/program64_correct.cpp
@@ -0,0 +1,14 @@
+/************************************************
+ * PI -- Test program to see verify that        *
+ *      the value  of "pi" in math.h is         *
+ *      correct.                                *
+ ************************************************/
+/* math.h defines M_PI */
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("pi is %f\n", M_PI);
+    return (0);
+}
---
+++ books/How_Not_To_Program_in_C++/program64_faulty.cpp
@@ -0,0 +1,14 @@
+/************************************************
+ * PI -- Test program to see verify that        *
+ *      the value  of "pi" in math.h is         *
+ *      correct.                                *
+ ************************************************/
+/* math.h defines M_PI */
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("pi is %d\n", M_PI);
+    return (0);
+}
