03e946c5811cfa175aad2b46b893fb803dfa67e9
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-08-28 16:56:27 +0200

    * Added program 77.

---
+++ books/How_Not_To_Program_in_C++/program77_correct.cpp
@@ -0,0 +1,25 @@
+/************************************************
+ * double -- Print a double table for the       *
+ *      numbers 1 through 10.                   *
+ ************************************************/
+#include <iostream>
+
+/************************************************
+ * DOUBLE -- Macro to double the value of a     *
+ * number.                                      *
+ ************************************************/
+#define DOUBLE(value) ((value) + (value))
+
+int main()
+{
+    // Counter for the double list
+    int counter;
+
+    for (counter = 1; counter <= 10; ++counter)
+    {
+        std::cout << "Twice " << counter << " is " <<
+            DOUBLE(counter) << '\n';
+    }
+
+    return (0);
+}
