a37fc08239ef772c68b9d8f383ccd80915cd7b2f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-08-28 16:30:25 +0200

    * Added program 73.

---
+++ books/How_Not_To_Program_in_C++/program73_correct.cpp
@@ -0,0 +1,11 @@
+/************************************************
+ * Toy program that declares two variables      *
+ ************************************************/
+typedef char* CHAR_PTR;
+
+int main()
+{
+    CHAR_PTR sam, joe;
+
+    return (0);
+}
---
+++ books/How_Not_To_Program_in_C++/program73_faulty.cpp
@@ -0,0 +1,11 @@
+/************************************************
+ * Toy program that declares two variables      *
+ ************************************************/
+#define CHAR_PTR char *
+
+int main()
+{
+    CHAR_PTR sam, joe;
+
+    return (0);
+}
