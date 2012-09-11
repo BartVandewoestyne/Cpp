4e2d53d7225d55f0a8b7494623a09b88acd47968
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-09-11 14:27:50 +0200

    * Added Exercise 3 from Chapter 10.

---
+++ books/The_C++_Programming_Language/Special_Edition/chapter10/ch10ex03.cpp
@@ -0,0 +1,17 @@
+/**
+ * (*2) Find a Date class that is in commercial use. Critique the facilities it
+ * offers. If possible, then discuss that Date with a real user.
+ */
+
+/**
+ * I have found the following implementations:
+ *
+ * 1. Boost:
+ *    http://www.boost.org/doc/libs/1_51_0/doc/html/date_time/gregorian.html
+ *
+ * 2. http://www.cs.duke.edu/~ola/ap/dates/date.h
+ *    http://www.cs.duke.edu/~ola/ap/dates/date.cpp
+ *
+ * 3. http://www.eng.uwaterloo.ca/~sd121/Date.h
+ *    http://www.eng.uwaterloo.ca/~sd121/Date.cpp
+ */
