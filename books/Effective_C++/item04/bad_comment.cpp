27ac94fc92fd3d9b91d4a173822d94c0546f8e2d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-09-12 16:34:14 +0200

    * Added item04.

---
+++ books/Effective_C++/item04/bad_comment.cpp
@@ -0,0 +1,14 @@
+int main()
+{
+
+  int a = 1;
+  int b = 2;
+
+  if ( a > b ) {
+    /*  int temp = a;    /* swap a and b */
+        a = b;
+        b = temp;
+    */
+  }
+
+}
---
+++ books/Effective_C++/item04/good_comment.cpp
@@ -0,0 +1,12 @@
+int main()
+{
+  int a = 1;
+  int b = 2;
+
+  if ( a > b ) {
+      // int temp = a;    // swap a and b
+      // a = b;
+      // b = temp;
+  }
+
+}
