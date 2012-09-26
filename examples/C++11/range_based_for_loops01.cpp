22305f91bcf9769f3e52ae1b3918e7f765df68a6
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-09-26 15:13:57 +0200

    * Added example on range based for loops.

--- examples/C++11/range_based_for_loops01.cpp
+++ examples/C++11/range_based_for_loops01.cpp
@@ -1,3 +1,8 @@
+/*
+ * References:
+ *   [1] http://yapb-soc.blogspot.de/2012/09/range-based-for-loop-on-int-range.html
+ */
+
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -16,6 +21,11 @@ int main ()
   for (auto i : { 2, 4, 5, 7 })
   {
     cout << i << endl;
+  }
+
+  for (int x : {1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+  {
+    cout << x << endl;
   }
 
 }
