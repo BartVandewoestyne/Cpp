0b691aa118b220e329d4d7c2afe3ea0934f26b00
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-10-16 14:29:17 +0200

    * Added Chapter 4 code.

---
+++ books/Accelerated_C++/chapter04/Student_info.h
@@ -0,0 +1,18 @@
+#ifndef GUARD_Student_info
+#define GUARD_Student_info
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Student_info {
+	std::string name;
+	double midterm, final;
+	std::vector<double> homework;
+};
+
+bool compare(const Student_info&, const Student_info&);
+std::istream& read(std::istream&, Student_info&);
+std::istream& read_hw(std::istream&, std::vector<double>&);
+
+#endif
---
+++ books/Accelerated_C++/chapter04/median.h
@@ -0,0 +1,8 @@
+#ifndef GUARD_median_h
+#define GUARD_median_h
+
+#include <vector>
+
+double median(std::vector<double>);
+
+#endif
