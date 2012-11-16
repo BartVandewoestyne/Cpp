30c24270a9b7311c9ab46012478e4acef267b8a1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2012-11-16 09:44:54 +0100

    * Added example that shows how to iterate over a vector in C++11.

---
+++ examples/C++11/vector.cpp
@@ -0,0 +1,25 @@
+/**
+ * Illustrates how in C++11 we can use auto to iterate over a vector.
+ *
+ * References:
+ *
+ *   [1] http://my.collaboration.agilent.com/personal/tparker/Blog
+ */
+
+#include <iostream>
+#include <vector>
+
+int main()
+{
+  std::vector<double> myVector;
+  myVector.push_back(1);
+  myVector.push_back(2);
+  myVector.push_back(3);
+
+  for (auto iter = myVector.begin(); iter != myVector.end(); ++iter)
+  {
+    std::cout << *iter << std::endl;
+  }
+
+  return 0;
+}
