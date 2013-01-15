b0b36643a49e47ebc724611b4d3722f0c44129c1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-01-15 20:50:43 +0100

    * Added reference.

--- examples/C++11/constexpr01.cpp
+++ examples/C++11/constexpr01.cpp
@@ -1,7 +1,12 @@
 /**
+ * A constexpr function is a named constant expression with parameters.  It’s
+ * meant to replace macros and hardcoded literals without sacrificing
+ * performance or type safety.
+ *
  * References:
  *
  *   [1] http://www.cprogramming.com/c++11/c++11-compile-time-processing-with-constexpr.html
+ *   [2] http://blog.smartbear.com/software-quality/bid/248591/using-constexpr-to-improve-security-performance-and-encapsulation-in-c
  */
 
 constexpr int multiply(int x, int y)
