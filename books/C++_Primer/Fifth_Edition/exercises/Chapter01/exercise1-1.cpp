c6a60764c0658d47a1ebbab2a6991e39620e5913
bvw <bvw@traficon.com>
2013-01-31 10:11:22 +0100

    * Started adding exercises from C++ Primer 5th edition.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter01/exercise1-1.cpp
@@ -0,0 +1,10 @@
+/**
+ * Compile with:
+ *
+ *   g++ -o exercise1-1 exercise1-1.cpp
+ */
+
+int main()
+{
+    return 0;
+}
---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter01/exercise1-2.cpp
@@ -0,0 +1,15 @@
+/**
+ * To see the status indicator returned by the program, on UNIX systems
+ * you can write:
+ *
+ *   $ echo $?
+ *
+ * while on Windows systems, you can use
+ *
+ *   $ echo %ERRORLEVEL%
+ */
+
+int main()
+{
+    return -1;
+}
