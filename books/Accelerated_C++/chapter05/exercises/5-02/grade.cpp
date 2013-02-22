734fc8f8dc4a5caa10f6c945717259761c5da514
bvw <bvw@traficon.com>
2013-02-22 10:22:53 +0100

    Remove tabs and reorganize comments.

--- books/Accelerated_C++/chapter05/exercises/5-02/grade.cpp
+++ books/Accelerated_C++/chapter05/exercises/5-02/grade.cpp
@@ -6,11 +6,11 @@
 
 using std::domain_error;  using std::vector;
 
-
-// compute a student's overall grade from midterm and final exam grades and homework grade
+// compute a student's overall grade from midterm and final exam grades and
+// homework grade
 double grade(double midterm, double final, double homework)
 {
-	return 0.2 * midterm + 0.4 * final + 0.4 * homework;
+  return 0.2 * midterm + 0.4 * final + 0.4 * homework;
 }
 
 // compute a student's overall grade from midterm and final exam grades
@@ -18,14 +18,14 @@ double grade(double midterm, double final, double homework)
 // this function does not copy its argument, because `median' does so for us.
 double grade(double midterm, double final, const vector<double>& hw)
 {
-	if (hw.size() == 0)
-		throw domain_error("student has done no homework");
-	return grade(midterm, final, median(hw));
+  if (hw.size() == 0)
+      throw domain_error("student has done no homework");
+  return grade(midterm, final, median(hw));
 }
 
 double grade(const Student_info& s)
 {
-	return grade(s.midterm, s.final, s.homework);
+    return grade(s.midterm, s.final, s.homework);
 }
 
 // predicate to determine whether a student failed
