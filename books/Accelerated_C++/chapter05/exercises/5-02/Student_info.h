8c3cb71ba02996ba35551c98b37c73fecd9b50fe
bvw <bvw@traficon.com>
2013-02-22 11:02:42 +0100

    Remove left-over defines.

--- books/Accelerated_C++/chapter05/exercises/5-02/Student_info.h
+++ books/Accelerated_C++/chapter05/exercises/5-02/Student_info.h
@@ -17,9 +17,4 @@ bool compare(const Student_info&, const Student_info&);
 std::istream& read(std::istream&, Student_info&);
 std::istream& read_hw(std::istream&, std::vector<double>&);
 
-vector<Student_info> extract_fails1(vector<Student_info>& students);
-vector<Student_info> extract_fails2(vector<Student_info>& students);
-vector<Student_info> extract_fails3(vector<Student_info>& students);
-list<Student_info> extract_fails4(list<Student_info>& students);
-
 #endif
