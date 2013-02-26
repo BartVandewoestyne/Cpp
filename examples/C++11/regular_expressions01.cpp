8e73e6b0ccde08a83fd5392ae48b7e9bf20a61e5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-02-26 20:22:07 +0100

    Update example on regular expressions.

--- examples/C++11/regular_expressions01.cpp
+++ examples/C++11/regular_expressions01.cpp
@@ -1,5 +1,10 @@
 /**
- * TODO: this doesn't seem to compile with g++ 4.6.2.
+ * TODO:
+ *   - This compiles with g++ 4.7.2 but gives the error:
+ *
+ *       terminate called after throwing an instance of 'std::regex_error'
+ *         what():  regex_error
+ *       Aborted (core dumped)
  */
 
 #include <iostream>
@@ -10,21 +15,26 @@ using namespace std;
 
 int main()
 {
-        string input;
-        regex integer("(\\+|-)?[[:digit:]]+");
-        //As long as the input is correct ask for another number
-        while(true)
+    string input;
+    regex integer("(\\+|-)?[[:digit:]]+");
+
+    // As long as the input is correct ask for another number.
+    while (true)
+    {
+        cout << "Give me an integer!" << endl;
+        cin >> input;
+
+        // Exit when the user inputs q.
+        if (input == "q")
+            break;
+
+        if ( regex_match(input, integer) )
         {
-                cout<<"Give me an integer!"<<endl;
-                cin>>input;
-                //Exit when the user inputs q
-                if(input=="q")
-                        break;
-                if(regex_match(input,integer))
-                        cout<<"integer"<<endl;
-                else
-                {
-                        cout<<"Invalid input"<<endl;
-                }
+            cout << "Integer." << endl;
         }
+        else
+        {
+            cout << "Invalid input!" << endl;
+        }
+    }
 }
