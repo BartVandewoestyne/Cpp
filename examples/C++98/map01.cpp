5789dd45ca1794577ade53d08639a2c5d676edd3
bvw <bvw@traficon.com>
2013-03-08 10:38:55 +0100

    Add some examples on std::map.

---
+++ examples/C++98/map01.cpp
@@ -0,0 +1,33 @@
+/**
+ * References:
+ *
+ *   [1] The C++ Standard Library, A Tutorial and Reference, Jossutis, 1999,
+ *       page 180-183.
+ */
+
+#include <iostream>
+#include <string>
+#include <map>
+
+int main()
+{
+    std::map<std::string,float> coll;
+
+    // ...
+
+    if (coll.insert(std::make_pair("otto", 22.3)).second) {
+        std::cout << "OK, could insert otto/22.3" << std::endl;
+    }
+    else
+    {
+        std::cout << "Oops, could not insert otto/22.3 "
+                  << "(key otto already exists)" << std::endl;
+    }
+
+    // remove all elements with the passed key
+    std::string key("otto");
+    coll.erase(key);
+
+    // Note that if the index does not exist yet, new element will be created.
+    coll["otto"] = 7.7;
+}
