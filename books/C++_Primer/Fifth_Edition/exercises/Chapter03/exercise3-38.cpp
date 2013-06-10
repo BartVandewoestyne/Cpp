e8b5594d743f512f2302989ddc39152dc3acc209
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-06-10 11:56:35 +0200

    Add C++ Primer exercise 3-38.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter03/exercise3-38.cpp
@@ -0,0 +1,9 @@
+/**
+ * Pointers are like numbers that denote positions on a
+ * scale of some kind. It makes sense to subtract two such
+ * positions to find the interval between them, and it makes
+ * sense to add two intervals, or to add a position and an
+ * interval. But it doesn't make sense to add two positions;
+ * one can only do so by agreeing on a zero reference and then
+ * converting one or both positions to intervals.
+ */
