a9209b960034f9b12ef83b7ed8c8d960bb6d6538
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-06-13 09:59:50 +0200

    Add C++ Primer exercise 4-6.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter04/exercise4-06.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int i = 4;
+
+    if (i % 2 == 0)
+    {
+        std::cout << i << " is even." << std::endl;
+    }
+    else
+    {
+        std::cout << i << " is odd." << std::endl;
+    }
+}
