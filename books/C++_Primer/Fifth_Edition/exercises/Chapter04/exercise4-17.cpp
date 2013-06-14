94941d9b7f902d8e478f7be5061d1b0bb315c172
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-06-14 11:50:40 +0200

    Add C++ Primer exercise 4-17.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter04/exercise4-17.cpp
@@ -0,0 +1,7 @@
+/**
+ * The prefix form increments (or decrements) its operand and yields the
+ * changed object as its result.
+ *
+ * The postfix operators increment (or decrement) the operand but yield a copy
+ * of the original, unchanged value as its result.
+ */
