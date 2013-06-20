88c5c67554e284f390503f5bcc42a8f52019ea1d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-06-20 09:05:45 +0200

    Add C++ Primer exercise 5-1.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter05/exercise5-1.cpp
@@ -0,0 +1,7 @@
+/**
+ * A null statement is a single semicolon.
+ *
+ * A null statement is useful where the language requires a statement but the
+ * program's logic does not.  Such usage is most common when a loop's work can
+ * be done within its condition.
+ */
