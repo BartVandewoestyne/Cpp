67e21c223b527ff891c62407635dee0997082656
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-07-10 21:20:05 +0200

    Expand iomanip example.

--- examples/C++98/iomanip.cpp
+++ examples/C++98/iomanip.cpp
@@ -7,4 +7,29 @@ int main()
             << std::showpoint
             << std::setw(25) << -0.1234567890
             << std::setw(25) << -0.1234567890 << std::endl;
+
+  std::cout << std::fixed
+            << std::setprecision(1)
+            << 1.234 << std::endl;
+
+  std::cout << std::setprecision(1)
+            << 1.234 << std::endl;
+
+  std::cout << std::showpoint
+            << 1.234 << std::endl;
+
+  std::cout << std::noshowpoint
+            << std::setprecision(0)
+            << 1.234 << std::endl;
+
+  std::cout << std::setw(1) << 123 << std::endl;
+  std::cout << std::setw(2) << 123 << std::endl;
+  std::cout << std::setw(3) << 123 << std::endl;
+  std::cout << std::setw(4) << 123 << std::endl;
+
+  std::cout << std::setw(1) << "--" << std::endl;
+  std::cout << std::setw(2) << "--" << std::endl;
+  std::cout << std::setw(3) << "--" << std::endl;
+  std::cout << std::setw(4) << "--" << std::endl;
+
 }
