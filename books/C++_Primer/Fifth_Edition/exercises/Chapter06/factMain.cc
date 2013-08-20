4de11b20438745b99928d275d442f5908934f3b2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-08-20 15:53:20 +0200

    Add C++ Primer exercise 6-09.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter06/factMain.cc
@@ -0,0 +1,13 @@
+// Compîle and link with:
+//
+//   gcc -c factMain.cc
+//   gcc -c fact.cc
+//   gcc factMain.o fact.o -o main
+
+#include "Chapter6.h"
+
+int main()
+{
+  int j = fact(5);
+  return 0;
+}
