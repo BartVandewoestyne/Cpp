afc7bad2c3b57d5f58c75148fec256e0a9bb47e1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-09-05 17:16:40 +0200

    Add C++ Primer exercise 7.20 and 7.21.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter07/exercise7-20.cpp
@@ -0,0 +1,10 @@
+/*
+ * 1) When are friends useful?
+ *    -> When a class or function needs access to another class to access its
+ *    nonpublic members.
+ *
+ * 2) Discuss the pros and cons of using friends?
+ *    -> pro: friends are not afected by the access control of the section in
+ *    which they are declared.
+ *    -> con: friends are not members of the class.
+ */
