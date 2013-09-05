966634b30a3ac0468a496cdf8346349380542da6
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-09-05 16:09:01 +0200

    Add C++ Primer exercises 7.17 and 7.18.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter07/exercise7-18.cpp
@@ -0,0 +1,6 @@
+/*
+ * What is encapsulation?  Why is it useful?
+ *
+ * -> Encapsulation has to do with the fact that users can not reach inside an
+ * object and meddle with its implementation.
+ */
