8a826d3544555076cd21112929b16ccb41d0ef37
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-09-05 11:48:20 +0200

    Fix naming issue.

--- books/C++_Primer/Fifth_Edition/exercises/Chapter07/exercise7-05.cpp
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter07/exercise7-05.cpp
@@ -4,8 +4,8 @@ struct Person {
 
     // These functions must be const because they do not change the object on
     // which they are called.
-    std::string name() const { return name; }
-    std::string address() const { return address; }
+    std::string getName() const { return name; }
+    std::string getAddress() const { return address; }
 
     std::string name;
     std::string address;
