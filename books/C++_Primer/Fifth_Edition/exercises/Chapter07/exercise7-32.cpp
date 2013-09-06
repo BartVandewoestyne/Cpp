fd798723c284d033c5cde2107d07c76efa8d78a7
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2013-09-06 16:01:28 +0200

    Add C++ Primer exercise 7.32-7.33.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter07/exercise7-32.cpp
@@ -0,0 +1,103 @@
+// TODO: couldn't get this one working yet...
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cout;
+
+class Windows_mgr {
+public:
+    using ScreenIndex = std::vector<Screen>::size_type;
+    void clear(ScreenIndex);
+private:
+    std::vector<Screen> screens{Screen(24, 80, ' ')};
+};
+
+class Screen {
+
+friend void Window_mgr::clear(ScreenIndex);
+
+public:
+    typedef std::string::size_type pos;
+
+    Screen() = default;
+    Screen(pos ht, pos wd) : height(ht), width(wd), contents(ht*wd, ' ') { }
+    Screen(pos ht, pos wd, char c) : height(ht), width(wd),
+                                     contents(ht*wd, c) { }
+
+    char get() const
+        { return contents[cursor]; }
+    inline char get(pos ht, pos wd) const;
+
+    Screen set(char);
+    Screen set(pos, pos, char);
+
+    void some_member() const;
+    Screen move(pos r, pos c);
+
+    Screen display(std::ostream &os)
+            { do_display(os); return *this; }
+    const Screen display(std::ostream &os) const
+            { do_display(os); return *this; }
+
+private:
+    pos cursor = 0;
+    pos height = 0, width = 0;
+    std::string contents;
+    mutable size_t access_ctr;
+
+    void do_display(std::ostream &os) const {os << contents;}
+};
+
+
+void Window_mgr::clear(ScreenIndex i)
+{
+    Screen &s = screens[i];
+    s.contents = string(s.height*s.width, ' ');
+}
+
+void Screen::some_member() const
+{
+    ++access_ctr;
+}
+
+inline
+Screen Screen::move(pos r, pos c)
+{
+    pos row = r * width;
+    cursor = row + c;
+    return *this;
+}
+
+char Screen::get(pos r, pos c) const
+{
+    pos row = r * width;
+    return contents[row + c];
+}
+
+inline Screen Screen::set(char c)
+{
+    contents[cursor] = c;
+    return *this;
+}
+
+inline Screen Screen::set(pos r, pos col, char ch)
+{
+    contents[r*width + col] = ch;
+    return *this;
+}
+
+int main()
+{
+    Screen myScreen(5, 5, 'X');
+
+    // move creates first copy, then set creates a copy of this copy where it
+    // sets '#' and then this one is displayed.
+    myScreen.move(4,0).set('#').display(cout);
+    cout << "\n";
+
+    // Shows the original unmodified screen.
+    myScreen.display(cout);
+    cout << "\n";
+}
