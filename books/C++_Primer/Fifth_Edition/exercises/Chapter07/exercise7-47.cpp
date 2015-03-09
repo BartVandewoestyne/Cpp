636dcc3e826b302f048ddd0b0254f270048a6e83
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2015-03-09 18:31:40 +0100

    Add exercise 7-47.

---
+++ books/C++_Primer/Fifth_Edition/exercises/Chapter07/exercise7-47.cpp
@@ -0,0 +1,13 @@
+// TODO: benefits?  disadvantages?  More argumentation???
+
+// If the Sales_data constructor that takes a string is explicit:
+//
+//   * then the constructor cannot be used to implicitly create a Sales_data
+//     object.
+//   * then the constructor can only be used for direct initialization.
+//   * this is good for when certain class-type conversions are not useful.
+//
+// If the Sales_data constructor that takes a string is not explicit:
+//
+//   * then the constructor can be used to implicitly create a Sales_data
+//     object.
