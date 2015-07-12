84a1d79c5fab1d40b1d0bb6551c1226387b2644c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2015-07-13 00:40:56 +0200

    Add 'AAA Style' and 'decltype(auto)' idiom files.

---
+++ idioms/C++11/almost_always_auto.cpp
@@ -0,0 +1,12 @@
+/**
+ *
+ * Some examples on Herb Sutter's 'AAA Style (Almost Always Auto)'.
+ *
+ * References:
+ *
+ *   [1] http://herbsutter.com/2013/08/12/gotw-94-solution-aaa-style-almost-always-auto/
+ *
+ * TODO:
+ *
+ *   - add examples from this GotW article.
+ */
---
+++ idioms/C++14/decltype_auto.cpp
@@ -0,0 +1,6 @@
+/**
+ * References:
+ *
+ *   [1] http://stackoverflow.com/questions/24109737/what-are-some-uses-of-decltypeauto
+ *   [2] http://thbecker.net/articles/auto_and_decltype/section_01.html
+ */
