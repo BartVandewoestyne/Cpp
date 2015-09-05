4d6675e85f15a0b746acdef8e608f6351a1cbf34
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2015-09-05 12:16:51 +0200

    Update erase-remove example.

--- idioms/C++98/erase-remove.cpp
+++ idioms/C++98/erase-remove.cpp
@@ -1,7 +1,13 @@
 /**
- * Erase-Remove idiom.
+ * Program to illustrate the Erase-Remove idiom.  This idiom is used if you
+ * have a vector and you want to remove certain elements from it for which a
+ * certain predicate is true.  E.g. you want to remove all odd elements or so.
  *
- * http://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
+ * References:
+ *
+ *   [1] http://en.wikipedia.org/wiki/Erase%E2%80%93remove_idiom
+ *   [2] Scott Meyers Effective STL book
+ *   [3] C9 lectures on the STL by Stephan T Lavavej, part 2 of N (around 40:00)
  */
 
 #include <algorithm>
@@ -22,12 +28,14 @@ void print(const std::vector<int>& vec)
 int main()
 {
     std::vector<int> v;
+
     for (unsigned int i = 0; i < 10; ++i)
     {
         v.push_back(i);
     }
     print(v);
 
+    // Use the Erase-Remove idiom to remove all elements that are 5.
     v.erase( std::remove( v.begin(), v.end(), 5), v.end() );
     print(v);
 }
