a7bbf07d94cc6dd758bd81c43bc8dbb1d62b390e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2015-10-15 10:03:20 +0200

    Add example on 'this' pointer.

---
+++ examples/C++98/this.cpp
@@ -0,0 +1,44 @@
+/*!
+ * Illustrates how in the body of a nonstatic member function, the keyword
+ * 'this' is a non-lvalue expression whose value is the address of the object for
+ * which the function is called.
+ *
+ * References:
+ *
+ *   [1] ISO/IEC 14882:1998(E), section 9.3.2 'The this pointer' [class.this]
+ */
+
+#include <iostream>
+
+class Foo {
+public:
+
+    // Nonstatic non-const member function: the type of this is Foo*
+    void f_nonconst()
+    {
+        member = 1;        // perfectly legal, because *this is not const
+        this->member = 1;  // the same
+
+        //this = new Foo;    // error: lvalue required as left operand of assignment
+    }
+
+    // Nonstatic const member function: the type of this is const Foo* (this is
+    // a pointer to const; that is, *this has const type)
+    void f_const() const
+    {
+        //member = 1;        // error: assignment of member 'Foo::member' in read-only object
+        //this->member = 1;  // error: assignment of member 'Foo::member' in read-only object
+
+        //this = new Foo();  // error: lvalue required as left operand of assignment
+    }
+
+private:
+    int member;
+};
+
+int main()
+{
+    Foo myFoo;
+    myFoo.f_nonconst();
+    myFoo.f_const();
+}
