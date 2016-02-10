1510d0a2e6b0922b99533aebbd1b3c84c799a7b3
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-02-11 00:27:04 +0100

    Add CRTP idiom example.

---
+++ idioms/CRTP.cpp
@@ -0,0 +1,68 @@
+/**
+ * References:
+ *
+ *   [1] https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern
+ *   [2] http://codeyarns.com/2015/07/09/how-to-understand-crtp/
+ */
+
+#include <iostream>
+using namespace std;
+
+template <typename Derived>
+struct Creature
+{
+    int eye_num;
+
+    friend bool operator == (const Derived& lhs, const Derived& rhs)
+    {
+        return lhs.IsEqual(rhs);
+    }
+
+    friend bool operator != (const Derived& lhs, const Derived& rhs)
+    {
+        return !(lhs.IsEqual(rhs));
+    }
+};
+
+struct Bird: Creature<Bird>
+{
+    int wing_num;
+
+    bool IsEqual(const Bird& b) const
+    {
+        return eye_num == b.eye_num && wing_num == b.wing_num;
+    }
+};
+
+struct Fish: Creature<Fish>
+{
+    int fin_num;
+
+    bool IsEqual(const Fish& f) const
+    {
+        return eye_num == f.eye_num && fin_num == f.fin_num;
+    }
+};
+
+int main()
+{
+    Bird b1, b2, b3;
+
+    b1.eye_num  = 2;
+    b2.eye_num  = 2;
+    b3.eye_num  = 2;
+
+    b1.wing_num = 2;
+    b2.wing_num = 2;
+    b3.wing_num = 3;
+
+    if (b1 != b2) cout << "Bird equal\n";
+    else          cout << "Bird not equal\n";
+    if (b1 == b3) cout << "Bird equal\n";
+    else          cout << "Bird not equal\n";
+
+    // You can do the same with Fish:
+    // Fish f1, f2, f3;
+    // f1.fin_num = 5;
+    // and so on ...
+}
