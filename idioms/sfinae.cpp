8f48b9d4e807ee004ff8b79e1e287bed5c7c8bb3
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-02-24 13:25:11 +0100

    Add reference for SFINAE.

--- idioms/sfinae.cpp
+++ idioms/sfinae.cpp
@@ -3,4 +3,5 @@
  *
  *   [1] http://codeofthedamned.com/index.php/sfinae
  *   [2] http://jguegant.github.io/blogs/tech/sfinae-introduction.html
+ *   [3] http://www.bfilipek.com/2016/02/notes-on-c-sfinae.html
  */
