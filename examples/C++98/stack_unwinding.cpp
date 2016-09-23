4fb0dc1b21747723b55624fc2d8202494524ebb9
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-09-23 12:47:20 +0200

    Add reference for stack unwinding example.

--- examples/C++98/stack_unwinding.cpp
+++ examples/C++98/stack_unwinding.cpp
@@ -1,3 +1,7 @@
 /*
  * TODO: add program that demonstrates how destructors are called when an exception occurs.
+ *
+ * References:
+ *
+ *   [1] https://msdn.microsoft.com/en-us/library/hh254939.aspx
  */
