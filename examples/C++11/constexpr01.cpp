d5d90fa2c1fc8f2e494ef93b7e6192c7e9f8d54f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-10-04 22:27:28 +0200

    Add references for constexpr.

--- examples/C++11/constexpr01.cpp
+++ examples/C++11/constexpr01.cpp
@@ -7,6 +7,9 @@
  *
  *   [1] http://www.cprogramming.com/c++11/c++11-compile-time-processing-with-constexpr.html
  *   [2] http://blog.smartbear.com/software-quality/bid/248591/using-constexpr-to-improve-security-performance-and-encapsulation-in-c
+ *   [3] http://enki-tech.blogspot.be/2012/09/c11-compile-time-calculator-with.html
+ *   [4] https://akrzemi1.wordpress.com/2011/05/06/compile-time-computations/
+ *   [5] http://avidinsight.uk/2015/10/how-to-check-that-a-function-is-constexpr/
  */
 
 constexpr int multiply(int x, int y)
