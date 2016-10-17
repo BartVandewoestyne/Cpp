8aa312203cd7d3458643294584f13b2db98640d2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-10-17 09:49:32 +0200

    Add example on pure virtual function with implementation.

---
+++ examples/C++98/pure_virtual_function_with_base_implementation.cpp
@@ -0,0 +1,50 @@
+/**
+ * Illustrate that pure virtual functions *can* have an implementation in the
+ * base class, but that this is not used often.
+ *
+ * Possible use cases (according to StackOverflow):
+ *
+ *   - There is a more-or-less reasonable default behavior, but the class
+ *     designer wants that sort-of-default behavior be invoked only explicitly.
+ *
+ *   - It can also be the case what you want derived classes to always perform
+ *     their own work but also be able to call a common set of functionality.
+ * 
+ * References:
+ *
+ *   [1] http://stackoverflow.com/questions/2089083/pure-virtual-function-with-implementation
+ *   [2] http://stackoverflow.com/questions/25530108/implementing-pure-virtual-function-in-the-abstract-class-is-there-any-use-of-i
+ */
+
+#include <iostream>
+
+class Base {
+public:
+    virtual void f() = 0;
+};
+
+void Base::f()
+{
+    std::cout << "Base::f() called." << std::endl;
+}
+
+
+class Derived : public Base {
+public:
+    virtual void f()
+    {
+        Base::f();  // Derived's implementation can call Base implementation (if access permissions allow it)... but this is not so often used?
+
+        std::cout << "Derived::f() called." << std::endl;  // Derived's implementation.
+    }
+};
+
+
+int main()
+{
+    //Base b;  // does not work, because cannot declare variable 'b' to be of abstract type 'Base'.
+    //b.f();
+
+    Derived d;
+    d.f();
+}
