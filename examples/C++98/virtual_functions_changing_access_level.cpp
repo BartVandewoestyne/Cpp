b4c11cc184dcca9195affcbb3a7984af6dcaec00
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-10-17 13:39:35 +0200

    Add example on changing the access level of virtual functions.

---
+++ examples/C++98/virtual_functions_changing_access_level.cpp
@@ -0,0 +1,57 @@
+/*
+ * Example that demonstrates how one can change the access level of a virtual
+ * function (not that it is good style though!)
+ *
+ * References:
+ *
+ *   [1]
+ *   http://stackoverflow.com/questions/4094534/how-and-why-is-it-possible-to-change-access-level-of-a-member
+ */
+
+#include <iostream>
+
+class Base {
+public:
+    virtual void f() { std::cout << "Base::f()" << std::endl; }
+};
+
+class DerivedPublic : public Base {
+public:
+    virtual void f() { std::cout << "DerivedPublic::f()" << std::endl; }
+};
+
+class DerivedProtected : public Base {
+protected:
+    virtual void f() { std::cout << "DerivedProtected::f()" << std::endl; }
+};
+
+class DerivedPrivate : public Base {
+private:
+    virtual void f() { std::cout << "DerivedPrivate::f()" << std::endl; }
+};
+
+int main()
+{
+    Base* b = new Base();
+    b->f();
+
+
+    Base* db_public = new DerivedPublic();
+    db_public->f();
+
+    Base* db_protected = new DerivedProtected();
+    db_protected->f();  // allowed!
+
+    Base* db_private = new DerivedPrivate();
+    db_private->f();  // allowed!
+
+
+    DerivedPublic* d_public = new DerivedPublic();
+    d_public->f();
+
+    DerivedProtected* d_protected = new DerivedProtected();
+    //d_protected->f();  // Error: 'virtual void DerivedProtected::f()' is protected
+
+    DerivedPrivate* d_private = new DerivedPrivate();
+    //d_private->f();  // Error: 'virtual void DerivedPrivate::f()' is private
+}
