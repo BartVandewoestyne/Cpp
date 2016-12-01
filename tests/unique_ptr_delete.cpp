1fd5f3935d5866f267131cdf7d489462ee7f8b9c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-12-01 22:42:24 +0100

    Add example on erroneous deletion of unique_ptr.

---
+++ tests/unique_ptr_delete.cpp
@@ -0,0 +1,34 @@
+/*
+ * Test program to exiperiment with passing const std::unique_ptr& as parameter.
+ * (which is not done according to [1], solution 3d.
+ * 
+ * References:
+ *
+ *   [1] https://herbsutter.com/2013/06/05/gotw-91-solution-smart-pointer-parameters/
+ */
+
+#include <memory>
+
+class Widget
+{};
+
+void f(const std::unique_ptr<Widget>& pw)
+{
+    delete pw.get();  // allowed, but runtime error: causes double delete
+}
+
+void g(Widget* pw)
+{
+    delete pw;  // allowed, but bad to delete p2 here...
+}
+
+int main()
+{
+    auto p1 = std::make_unique<Widget>();
+    auto p2 = new Widget();
+
+    f(p1);
+    g(p2);
+
+    //delete p2;  // better to delete p2 here
+}
