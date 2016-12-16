a6820b429b67ab93fca8be9611b472158bbf90cb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-12-16 22:43:29 +0100

    Add reference to blog article about const.

--- examples/C++98/const_for_local_variables.cpp
+++ examples/C++98/const_for_local_variables.cpp
@@ -1,4 +1,5 @@
 /*
  * References:
  *   [1] http://www.bfilipek.com/2016/12/please-declare-your-variables-as-const.html
+ *   [2] https://akrzemi1.wordpress.com/2016/12/16/more-const-fewer-bugs/
  */
