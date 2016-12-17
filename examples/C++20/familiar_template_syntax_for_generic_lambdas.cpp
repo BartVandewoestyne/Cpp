4bb98942196715d8242d5023f758a173dbe29026
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-12-17 22:42:57 +0100

    Add reference for 'familiar template syntax for generic lambdas'.

---
+++ examples/C++20/familiar_template_syntax_for_generic_lambdas.cpp
@@ -0,0 +1,5 @@
+/*
+ * References:
+ *   [1] http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0428r0.pdf
+ *   [2] https://www.reddit.com/r/cpp/comments/5irdg1/a_nice_c17_feature_that_is_being_overlooked/
+ */
