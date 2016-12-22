fd95d9b7641e7108f91f588dd8736f7607deac37
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2016-12-22 21:25:48 +0100

    Add SFINAE references.

--- idioms/sfinae.cpp
+++ idioms/sfinae.cpp
@@ -5,4 +5,6 @@
  *   [2] http://jguegant.github.io/blogs/tech/sfinae-introduction.html
  *   [3] http://www.bfilipek.com/2016/02/notes-on-c-sfinae.html
  *   [4] http://www.bfilipek.com/2016/02/sfinae-followup.html
+ *   [5] http://en.wikipedia.org/wiki/Substitution_failure_is_not_an_error
+ *   [6] https://foonathan.github.io/blog/2015/11/30/overload-resolution-4.html
  */
