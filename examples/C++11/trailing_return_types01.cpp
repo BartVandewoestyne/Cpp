ce553b0582824efca2fa1222b1a7565fcb92454a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-01-18 08:31:04 +0100

    Add reference for trailing return types.

--- examples/C++11/trailing_return_types01.cpp
+++ examples/C++11/trailing_return_types01.cpp
@@ -3,6 +3,7 @@
  *
  *   [1] https://www.ibm.com/developerworks/community/blogs/5894415f-be62-4bc0-81c5-3956e82276f3/entry/introduction_to_the_c_11_feature_trailing_return_types?lang=en
  *   [2] http://arne-mertz.de/2016/11/trailing-return-types-everywhere/
+ *   [3] https://blog.petrzemek.net/2017/01/17/pros-and-cons-of-alternative-function-syntax-in-cpp/
  */
 
 #include <iostream>
