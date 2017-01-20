429fbd5fc62662752514e923c0fe908faf57a832
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-01-20 09:00:51 +0100

    Add reference for std::array.

--- examples/C++11/array.cpp
+++ examples/C++11/array.cpp
@@ -2,4 +2,5 @@
  * References:
  *
  *   [1] http://www.modernescpp.com/index.php/std-array-dynamic-memory-no-thanks
+ *   [2] https://a4z.bitbucket.io/blog/2017/01/19/A-convenient-constructor-for-std::array.html
  */
