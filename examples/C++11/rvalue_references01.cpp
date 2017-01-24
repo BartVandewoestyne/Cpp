630b99798e1a2add6a1cd3ed161a322c59d76f88
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-01-24 08:53:43 +0100

    Add two references on the usefulness of const rvalue references.

--- examples/C++11/rvalue_references01.cpp
+++ examples/C++11/rvalue_references01.cpp
@@ -2,4 +2,6 @@
  * References:
  *   [1] http://www.artima.com/cppsource/rvalue.html
  *   [2] http://thbecker.net/articles/rvalue_references/section_01.html
+ *   [3] http://www.codesynthesis.com/~boris/blog/2012/07/24/const-rvalue-references/
+ *   [4] https://www.youtube.com/watch?v=dTeKf5Oek2c&feature=youtu.be&t=2501
  */
