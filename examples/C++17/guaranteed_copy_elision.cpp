1517923a7827f48e0553ed65b32322b6d18ee579
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-01-24 08:59:58 +0100

    Add two references on RVO.

--- examples/C++17/guaranteed_copy_elision.cpp
+++ examples/C++17/guaranteed_copy_elision.cpp
@@ -2,4 +2,6 @@
  * References:
  *   [1] http://insights.dice.com/2016/12/15/breaking-down-current-state-c-17/
  *   [2] http://www.bfilipek.com/2017/01/cpp17features.html#guaranteed-copy-elision
+ *   [3] http://www.fluentcpp.com/2016/11/28/return-value-optimizations/
+ *   [4] http://definedbehavior.blogspot.be/2011/08/value-semantics-copy-elision.html
  */
