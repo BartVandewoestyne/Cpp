9bac00eec8dce0d1efeabcbe17a187a509140a97
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-01-24 23:10:24 +0100

    Add reference for lambdas.

--- examples/C++11/lambda_expressions02.cpp
+++ examples/C++11/lambda_expressions02.cpp
@@ -1,7 +1,8 @@
 /**
  * Reference:
  *
- *   http://www.cprogramming.com/c++11/c++11-lambda-closures.html
+ *   [1] http://www.cprogramming.com/c++11/c++11-lambda-closures.html
+ *   [2] http://www.fluentcpp.com/2017/01/23/stl-function-objects-stateless-is-stressless/
  */
 
 #include <iostream>
