d540e2216cad9a650a750998ad34163ffe6f3bb2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-01-26 23:32:46 +0100

    Add references to PIMPL idiom.

---
+++ idioms/pimpl.cpp
@@ -0,0 +1,5 @@
+/*
+ * References:
+ *   [1] https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl/
+ *   [2] https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl-reloaded/
+ */
