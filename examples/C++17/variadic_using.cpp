67b404e35ba1b3cb7bbeeb140214ec94db796ad0
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-01-31 23:54:40 +0100

    Add references for variadic using.

---
+++ examples/C++17/variadic_using.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *   [1] https://youtu.be/1gNzhE-Tn40
+ *   [2] http://www.bfilipek.com/2017/01/cpp17features.html#pack-expansions-in-using-declarations
+ *   [3] http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0195r2.html
+ */
