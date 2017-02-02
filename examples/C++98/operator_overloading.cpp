ac10c347e7a1c94e4a5fc6758d199aa9d597ef28
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-02-02 09:02:24 +0100

    Add reference for operator overloading.

---
+++ examples/C++98/operator_overloading.cpp
@@ -0,0 +1,5 @@
+/*
+ * References:
+ *   [1] https://www.nextptr.com/question/a363004267/overloading-one-operator-in-terms-of-other
+ *   [2] https://www.reddit.com/r/cpp/comments/5rdv0z/overloading_one_operator_in_terms_of_other/
+ */
