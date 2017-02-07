72f648df1d851a1f6796ae5706659e3ff6ddbc8e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-02-07 09:02:22 +0100

    Add reference for valarray.

---
+++ examples/C++98/valarray.cpp
@@ -0,0 +1,8 @@
+/*
+ * Note: seems like valarray is redundant and could use revision or removal,
+ *       see [2].  Probably better not to use it.
+ *
+ * References:
+ *   [1] http://cppisland.com/?p=518
+ *   [2] http://www.gotw.ca/publications/mill01.htm
+ */
