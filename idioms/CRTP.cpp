4aa2bc36c5ca312b4c310b34639d4d181d0057a2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-03-30 01:03:24 +0200

    Add link to paper about CRTP.

--- idioms/CRTP.cpp
+++ idioms/CRTP.cpp
@@ -4,6 +4,7 @@
  *   [1] https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern
  *   [2] http://codeyarns.com/2015/07/09/how-to-understand-crtp/
  *   [3] http://www.grimm-jaud.de/index.php/blog/c-ist-doch-lazy
+ *   [4] http://stevedewhurst.com/once_weakly/once-weakly20170328/once-weakly20170328.pdf
  */
 
 #include <iostream>
