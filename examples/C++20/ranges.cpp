c70f7ef58b7df1f60d2254a9d72e406ee3b71dd2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-04-19 14:29:56 +0200

    Add reference to blog post related to ranges library.

--- examples/C++20/ranges.cpp
+++ examples/C++20/ranges.cpp
@@ -3,4 +3,5 @@
  *
  *   [1] http://arne-mertz.de/2017/01/ranges-stl-next-level/
  *   [2] http://www.modernescpp.com/index.php/the-new-ranges-library
+ *   [3] http://www.fluentcpp.com/2017/04/14/understand-ranges-better-with-the-new-cartesian-product-adaptor/
  */
