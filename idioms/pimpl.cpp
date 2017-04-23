e8b316d158053dabe353a0fd026a1ef961778ae9
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-04-23 23:22:20 +0200

    Add reference for PIMPL-idiom.

--- idioms/pimpl.cpp
+++ idioms/pimpl.cpp
@@ -2,4 +2,5 @@
  * References:
  *   [1] https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl/
  *   [2] https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl-reloaded/
+ *   [3] http://lattix.com/blog/2017/04/21/reduce-c-build-times-part-2-pimpl-idiom
  */
