813c67712ab39e039f2f1e2428ead32610b16b76
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-04-24 15:20:04 +0200

    Add reference for std::string_view.

--- examples/C++17/string_view.cpp
+++ examples/C++17/string_view.cpp
@@ -2,4 +2,5 @@
  * References:
  *   [1] http://insights.dice.com/2016/12/15/breaking-down-current-state-c-17/
  *   [2] http://foonathan.net/blog/2017/03/22/string_view-temporary.html
+ *   [3] http://www.grimm-jaud.de/index.php/blog/c-17-vermeide-kopieren-mit-std-string-view
  */
