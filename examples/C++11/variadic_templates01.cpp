4fd1b56d957be946874ec31975e0dc6577e81c23
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-05-15 01:15:09 +0200

    Add reference for variadic templates.

--- examples/C++11/variadic_templates01.cpp
+++ examples/C++11/variadic_templates01.cpp
@@ -3,4 +3,5 @@
  *   [1] http://channel9.msdn.com/Events/GoingNative/GoingNative-2012/Variadic-Templates-are-Funadic
  *   [2] http://arne-mertz.de/2016/11/modern-c-features-variadic-templates/
  *   [3] http://arne-mertz.de/2016/11/more-variadic-templates/
+ *   [4] http://eli.thegreenplace.net/2014/variadic-templates-in-c/
  */
