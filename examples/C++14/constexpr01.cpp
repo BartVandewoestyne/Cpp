df8b215af8e2b1565994a596d2c2f8ac9eb18deb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-05-15 01:21:54 +0200

    Add reference for C++14 constexpr.

--- examples/C++14/constexpr01.cpp
+++ examples/C++14/constexpr01.cpp
@@ -3,4 +3,5 @@
  *
  *   [1] https://blog.quasardb.net/demystifying-constexpr/
  *   [2] http://stoppels.blog/posts/compile-time-primes
+ *   [3] https://ryanmichaelcollins.github.io/blog/2017/05/14/fun-with-constexpr-and-variadic-templates
  */
