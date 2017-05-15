a2ba9f09c28a22532176a4213a1d01458c3f4fbd
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-05-15 15:13:55 +0200

    Add example for std::weak_ptr.

---
+++ examples/C++11/weak_ptr_for_solving_dangling_pointer_problem.cpp
@@ -0,0 +1,54 @@
+/*
+ * Example to illustrate how using std::weak_ptr is a good way to solve the dangling
+ * pointer problem.
+ *
+ * References:
+ *   [1] http://stackoverflow.com/questions/12030650/when-is-stdweak-ptr-useful
+ */
+
+#include <iostream>
+#include <memory>
+
+int main()
+{
+    // OLD, problem with dangling pointer
+    // PROBLEM: ref will point to undefined data!
+
+    int* ptr = new int(10);
+    int* ref = ptr;
+    delete ptr;
+
+    // NEW
+    // SOLUTION: check expired() or lock() to determine if pointer is valid
+
+    // empty definition
+    std::shared_ptr<int> sptr;
+
+    // takes ownership of pointer
+    sptr.reset(new int);
+    *sptr = 10;
+
+    // get pointer to data without taking ownership
+    std::weak_ptr<int> weak1 = sptr;
+
+    // deletes managed object, acquires new pointer
+    sptr.reset(new int);
+    *sptr = 5;
+
+    // get pointer to new data without taking ownership
+    std::weak_ptr<int> weak2 = sptr;
+
+    // weak1 is expired!
+
+    if(auto tmp = weak1.lock())
+        std::cout << *tmp << '\n';
+    else
+        std::cout << "weak1 is expired\n";
+
+    // weak2 points to new data (5)
+
+    if(auto tmp = weak2.lock())
+        std::cout << *tmp << '\n';
+    else
+        std::cout << "weak2 is expired\n";
+}
