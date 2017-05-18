0c0cde4bf49b536e4eb2dc5e1a511d1a8b9b9be1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-05-19 01:40:38 +0200

    Add reference to a talk from Eric Niebler's about ranges.

--- examples/C++20/ranges.cpp
+++ examples/C++20/ranges.cpp
@@ -4,4 +4,5 @@
  *   [1] http://arne-mertz.de/2017/01/ranges-stl-next-level/
  *   [2] http://www.modernescpp.com/index.php/the-new-ranges-library
  *   [3] http://www.fluentcpp.com/2017/04/14/understand-ranges-better-with-the-new-cartesian-product-adaptor/
+ *   [4] https://youtu.be/4OgAjT6HTG8 (and slides at http://nwcpp.org/talks/2017/HelloRangesTS.pptx)
  */
