5429af80fe0732a3a1aaaade57fc8fd0215b1397
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-04 01:06:04 +0200

    Add links to YouTube presentations on coroutines.

--- examples/C++20/coroutines.cpp
+++ examples/C++20/coroutines.cpp
@@ -2,4 +2,6 @@
  * References:
  *   [1] http://www.modernescpp.com/index.php/coroutines
  *   [2] https://toby-allsopp.github.io/2017/04/22/coroutines-reference-params.html
+ *   [3] https://youtu.be/ZTqHjjm86Bw
+ *   [4] https://youtu.be/UhrIKqDADX8
  */
