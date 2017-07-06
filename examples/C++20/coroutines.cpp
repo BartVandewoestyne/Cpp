03c5d715652f363cd925537e38eade83ddbbc274
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-06 22:46:41 +0200

    Add reference for coroutines.

--- examples/C++20/coroutines.cpp
+++ examples/C++20/coroutines.cpp
@@ -2,10 +2,9 @@
  * References:
  *   [1] http://www.modernescpp.com/index.php/coroutines
  *   [2] https://toby-allsopp.github.io/2017/04/22/coroutines-reference-params.html
-<<<<<<< HEAD
  *   [3] https://youtu.be/ZTqHjjm86Bw
  *   [4] https://youtu.be/UhrIKqDADX8
-=======
- *   [3] https://blogs.msdn.microsoft.com/vcblog/2017/05/19/using-c-coroutines-with-boost-c-libraries/
->>>>>>> 6a8e8d5dc6290fc78ba4ec06a30d93bd142224df
+ *   [5] https://blogs.msdn.microsoft.com/vcblog/2017/05/19/using-c-coroutines-with-boost-c-libraries/
+ *   [6] http://www.kirit.com/How%20C%2B%2B%20coroutines%20work/My%20first%20coroutine
+ *   [7] http://www.kirit.com/How%20C%2B%2B%20coroutines%20work/A%20more%20realistic%20coroutine
  */
