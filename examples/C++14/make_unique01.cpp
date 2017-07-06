89ef81c88b5b26fac0c8e387b76fa3432d16fe8a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-06 22:54:48 +0200

    Add references for make_unique.

--- examples/C++14/make_unique01.cpp
+++ examples/C++14/make_unique01.cpp
@@ -3,6 +3,8 @@
  * 
  *   [1] http://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique
  *   [2] http://shaharmike.com/cpp/unique-ptr/ (haven't read this one yet)
+ *   [3] https://stackoverflow.com/questions/22571202/differences-between-stdmake-unique-and-stdunique-ptr
+ *   [4] https://stackoverflow.com/questions/37514509/advantages-of-using-make-unique-over-new-operator
  */
 
 #include <iostream>
