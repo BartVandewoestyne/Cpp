706ab44bd583af4a52dabe8080dc51667cb45047
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-10 12:34:20 +0200

    Add links to C++17 attributes info.

--- examples/C++17/attributes.cpp
+++ examples/C++17/attributes.cpp
@@ -3,4 +3,5 @@
  *
  *   [1] https://infektor.net/posts/2017-01-19-using-cpp17-attributes-today.html
  *   [2] https://developers.redhat.com/blog/2017/03/10/wimplicit-fallthrough-in-gcc-7/
+ *   [3] http://www.bfilipek.com/2017/07/cpp17-in-details-attributes.html
  */
