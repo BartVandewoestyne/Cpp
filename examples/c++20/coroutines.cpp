dbcb4f0e69bf970771d0f41f9b15cedbc0f50861
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-10 12:37:27 +0200

    Changes in references.

--- examples/c++20/coroutines.cpp
+++ examples/c++20/coroutines.cpp
@@ -2,8 +2,9 @@
  * References:
  *   [1] http://www.modernescpp.com/index.php/coroutines
  *   [2] https://toby-allsopp.github.io/2017/04/22/coroutines-reference-params.html
- *   [3] https://blogs.msdn.microsoft.com/vcblog/2017/05/19/using-c-coroutines-with-boost-c-libraries/
+ *   [3] https://youtu.be/ZTqHjjm86Bw
  *   [4] https://youtu.be/UhrIKqDADX8
- *   [5] https://youtu.be/ZTqHjjm86Bw
- *   [6] http://www.kirit.com/How%20C%2B%2B%20coroutines%20work
+ *   [5] https://blogs.msdn.microsoft.com/vcblog/2017/05/19/using-c-coroutines-with-boost-c-libraries/
+ *   [6] http://www.kirit.com/How%20C%2B%2B%20coroutines%20work/My%20first%20coroutine
+ *   [7] http://www.kirit.com/How%20C%2B%2B%20coroutines%20work/A%20more%20realistic%20coroutine
  */
