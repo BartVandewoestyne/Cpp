786c596ff9bac3fa1cf5f2dcaa13b0d27644eda5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-24 15:34:08 +0200

    Add reference to CppCast Episode 110 on Coroutines.

--- examples/c++20/coroutines.cpp
+++ examples/c++20/coroutines.cpp
@@ -2,9 +2,11 @@
  * References:
  *   [1] http://www.modernescpp.com/index.php/coroutines
  *   [2] https://toby-allsopp.github.io/2017/04/22/coroutines-reference-params.html
- *   [3] https://youtu.be/ZTqHjjm86Bw
- *   [4] https://youtu.be/UhrIKqDADX8
+ *   [3] https://channel9.msdn.com/Events/CPP/CppCon-2016/CppCon-2016-James-McNellis-Introduction-to-C-Coroutines
+ *   [4] "Concurrency, Parallelism and Coroutines - Anthony Williams [ACCU 2017]" (https://youtu.be/UhrIKqDADX8)
  *   [5] https://blogs.msdn.microsoft.com/vcblog/2017/05/19/using-c-coroutines-with-boost-c-libraries/
  *   [6] http://www.kirit.com/How%20C%2B%2B%20coroutines%20work/My%20first%20coroutine
  *   [7] http://www.kirit.com/How%20C%2B%2B%20coroutines%20work/A%20more%20realistic%20coroutine
+ *   [8] https://channel9.msdn.com/Events/CPP/CppCon-2016/CppCon-2016-Gor-Nishanov-C-Coroutines-Under-the-covers
+ *   [9] http://cppcast.com/2017/07/gor-nishanov/
  */
