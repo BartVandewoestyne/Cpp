77a61cd5d962c6173ac0e67a3473249fbacdb212
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-25 00:44:46 +0200

    Add references for C++17 attributes.

--- examples/C++17/attributes.cpp
+++ examples/C++17/attributes.cpp
@@ -1,7 +1,9 @@
 /*
  * References:
  *
- *   [1] https://infektor.net/posts/2017-01-19-using-cpp17-attributes-today.html
- *   [2] https://developers.redhat.com/blog/2017/03/10/wimplicit-fallthrough-in-gcc-7/
- *   [3] http://www.bfilipek.com/2017/07/cpp17-in-details-attributes.html
+ *   [1] https://arne-mertz.de/2016/12/modern-c-features-attributes/
+ *   [2] https://infektor.net/posts/2017-01-19-using-cpp17-attributes-today.html
+ *   [3] https://developers.redhat.com/blog/2017/03/10/wimplicit-fallthrough-in-gcc-7/
+ *   [4] http://www.bfilipek.com/2017/07/cpp17-in-details-attributes.html
+ *   [5] https://blog.tartanllama.xyz/c++/2017/07/20/c++17-attributes/
  */
