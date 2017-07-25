4c42eb61f2800b3f894107265fcf9da4985588fc
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-25 13:56:15 +0200

    Improve pointer to pointer to int example.

--- examples/C++98/const_pointer_to_pointer.cpp
+++ examples/C++98/const_pointer_to_pointer.cpp
@@ -1,23 +1,42 @@
 /*
- * My own example of constness for Foo** pointers.
+ * My own example of constness for int** pointers.
  *
  * References:
  *   [1] https://isocpp.org/wiki/faq/const-correctness#constptrptr-conversion
+ *   [2] https://stackoverflow.com/questions/4246445/const-qualifier-for-pointers-to-pointers
  */
 
 int main()
 {
-    int a = 1;
-    int* pa = &a;
-    int** ppa = &pa;
-    **ppa = 11;
+    int x = 99;
+    int *px = &x;
 
-    int b = 2;
-    int* pb = &b;
-    //const int** ppb = &pb;      // error: invalid conversion from 'int**' to 'const int**' [-fpermissive]
-    const int* const* ppb = &pb;  // ok
-    //**ppb = 22;                 // error: assignment of read-only location '*(const int*)(* ppb)'
-    //*ppb = &a;                  // error: assignment of read-only location '* ppb'
-    //ppb = &a;                   // error: cannot convert 'int*' to 'const int* const*' in assignment
-    ppb = &pa;                    // ok
+    {
+      int a = 1;
+      int* pa = &a;
+      int** ppa = &pa;
+      **ppa = 11;
+    }
+
+    {
+      int a = 1;
+      int* pa = &a;
+      //const int** ppa = &pa;      // error: invalid conversion from 'int**' to 'const int**' [-fpermissive]
+      const int* const* ppa = &pa;  // ok
+      //**ppa = 11;                 // error: assignment of read-only location '*(const int*)(* ppa)'
+      //*ppa = &x;                  // error: assignment of read-only location '* ppa'
+      //ppa = &x;                   // error: cannot convert 'int*' to 'const int* const*' in assignment
+      ppa = &px;                    // ok
+    }
+
+    {
+      int a = 1;
+      int* pa = &a;
+      //const int** ppa = &pa;      // error: invalid conversion from 'int**' to 'const int**' [-fpermissive]
+      const int* const* const ppa = &pa;  // ok
+      //**ppa = 11;                 // error: assignment of read-only location '*(const int*)(* ppa)'
+      //*ppa = &x;                  // error: assignment of read-only location '* ppa'
+      //ppa = &x;                   // error: cannot convert 'int*' to 'const int* const*' in assignment
+      //ppa = &px;                    // error: error: assignment of read-only variable 'ppa'
+    }
 }
