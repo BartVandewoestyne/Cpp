48ded099da3d26318273e9a0b31f300894eb4234
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-27 13:38:10 +0200

    Add references for the detection idiom.

---
+++ idioms/detection_idiom.cpp
@@ -0,0 +1,5 @@
+/*
+ * References:
+ *   [1] C++Now 2017: Marshall Clow "The 'Detection idiom:' A Better Way to SFINAE" (https://youtu.be/U3jGdnRL3KI)
+ *   [2] http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/n4502.pdf
+ */
