c0ae4d948e6bb2f6a94fa7dd3e36abb7964e8b30
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-07-28 10:23:29 +0200

    Add example code for 2D array initialization.

---
+++ examples/C++98/array_2d_initialization.cpp
@@ -0,0 +1,42 @@
+/*
+ * References:
+ *   [1] https://stackoverflow.com/questions/3948290/whats-the-safe-way-to-fill-multidimensional-array-using-stdfill
+ *   [2] https://stackoverflow.com/questions/3586774/fill-multidimensional-array-elements-with-0s
+ */
+
+#include <iostream>
+
+const int NB_ROWS = 2;
+const int NB_COLS = 3;
+
+void print(const int myArray[NB_ROWS][NB_COLS])  // TODO: check if correct.
+{
+    for (int i = 0; i < NB_ROWS; ++i)
+    {
+        for (int j = 0; j < NB_COLS; ++j)
+        {
+          std::cout << myArray[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    // Initialize to any value.
+    {
+        int x[NB_ROWS][NB_COLS];
+
+        std::fill(x[0], x[0] + NB_ROWS*NB_COLS, 69);
+        print(x);
+    }
+
+    // Initializing to zero.
+    {
+        int x1[NB_ROWS][NB_COLS] = {};
+        print(x1);
+
+        int x2[NB_ROWS][NB_COLS] = {{0}};
+        print(x2);
+    }
+}
