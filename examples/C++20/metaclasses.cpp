15f8e5c4276107fbb2b07de36d6c510ff3d6aa95
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-08-09 03:29:12 +0200

    Add references for metaclasses.

---
+++ examples/C++20/metaclasses.cpp
@@ -0,0 +1,7 @@
+/*
+ * References:
+ *   [1] https://herbsutter.files.wordpress.com/2017/07/p0707r1.pdf
+ *   [2] https://herbsutter.com/2017/07/26/metaclasses-thoughts-on-generative-c/
+ *   [3] Thoughts on Metaclasses - Herb Sutter - Keynote [ACCU 2017] (https://youtu.be/6nsyX37nsRs)
+ *   [4] http://www.fluentcpp.com/2017/08/04/metaclasses-cpp-summary/
+ */
--- examples/C++98/array_2d_initialization.cpp
+++ examples/C++98/array_2d_initialization.cpp
@@ -9,6 +9,11 @@
 #include <algorithm>
 #include <iostream>
 
+class Foo {
+public:
+  int x;
+};
+
 const int NB_ROWS = 2;
 const int NB_COLS = 3;
 
@@ -24,14 +29,40 @@ void print(const int myArray[NB_ROWS][NB_COLS])  // TODO: check if correct.
     }
 }
 
+void print(const Foo myArray[NB_ROWS][NB_COLS])  // TODO: check if correct.
+{
+    for (int i = 0; i < NB_ROWS; ++i)
+    {
+        for (int j = 0; j < NB_COLS; ++j)
+        {
+          std::cout << myArray[i][j].x << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main()
 {
-    // Initialize to any value.
+    // Initialize to any value
     {
         int x[NB_ROWS][NB_COLS];
 
-        std::fill(x[0], x[0] + NB_ROWS*NB_COLS, 69);
+        std::fill(&x[0][0], &x[0][0] + sizeof(x)/sizeof(x[0][0]), 1);
         print(x);
+
+        std::fill(&x[0][0], &x[0][0] + NB_ROWS*NB_COLS, 2);
+        print(x);
+
+        std::fill(x[0], x[0] + NB_ROWS*NB_COLS, 3);
+        print(x);
+
+
+        Foo y[NB_ROWS][NB_COLS];
+
+        // This is C++11 using a lambda.  How to do it in C++98???
+        //std::for_each( &y[0][0], &y[0][0] + sizeof(y)/sizeof(y[0][0]),
+        //   [] (Foo& foo) { foo.x = 4; } );
+        //print(y);
     }
 
     // Initializing to zero.
