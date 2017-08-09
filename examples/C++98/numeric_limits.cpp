5c819de59cdf1dfd4e30ee472b09124f3862746a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-08-10 00:46:12 +0200

    Add numeric limits for double.

--- examples/C++98/numeric_limits.cpp
+++ examples/C++98/numeric_limits.cpp
@@ -11,6 +11,8 @@ int main () {
   cout << boolalpha;
   cout << "Minimum value for int: " << numeric_limits<int>::min() << endl;
   cout << "Maximum value for int: " << numeric_limits<int>::max() << endl;
+  cout << "Minimum value for double: " << numeric_limits<double>::min() << endl;
+  cout << "Maximum value for double: " << numeric_limits<double>::max() << endl;
   cout << "int is signed: " << numeric_limits<int>::is_signed << endl;
   cout << "Non-sign bits in int: " << numeric_limits<int>::digits << endl;
   cout << "int has infinity: " << numeric_limits<int>::has_infinity << endl;
