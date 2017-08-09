c1484e8681d7347e2503b4331f8c52ba67837bb8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-08-09 10:07:37 +0200

    Add more references for PIMPL idiom.

--- idioms/pimpl.cpp
+++ idioms/pimpl.cpp
@@ -3,4 +3,6 @@
  *   [1] https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl/
  *   [2] https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl-reloaded/
  *   [3] http://lattix.com/blog/2017/04/21/reduce-c-build-times-part-2-pimpl-idiom
+ *   [4] http://www.cppsamples.com/common-tasks/pimpl.html
+ *   [5] https://jonasdevlieghere.com/reference-counting-pimpl-idiom/
  */
