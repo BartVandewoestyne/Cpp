16fd806834e106d77c8b8a4e8d9df899954c62b4
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-08-09 15:52:25 +0200

    Improve references for C++98 Meyers Singleton.

--- idioms/C++98/meyers_singleton.cpp
+++ idioms/C++98/meyers_singleton.cpp
@@ -2,11 +2,59 @@
  * This implementation is not thread-safe??? (check why exactly)
  *
  * References:
- *   [1] More Effective C++, Scott Meyers, Item 26 'Limiting the number of
- *       objects of a class.' (check!)
- *   [2] Effective C++, Second Edition, Scott Meyers, Item 47: 'Ensure that
- *       non-local static objects are initialized before they're used.'
- *   [3] C++ and the Perils of Double-Checked Locking, Meyers S. and
- *       Alexandrescu Andrei,
- *       URL: http://www.aristeia.com/Papers/DDJ_Jul_Aug_2004_revised.pdf
+ *
+ *   [meyers2004alexandrescu] C++ and the Perils of Double-Checked Locking,
+ *     Meyers S. and Alexandrescu Andrei,
+ *     URL: http://www.aristeia.com/Papers/DDJ_Jul_Aug_2004_revised.pdf
+ *
+ *   [vlissides1998] John Vlissides. Pattern Hatching: Design Patterns Applied. AddisonWesley, 1998.
+ *     (The discussion of the 'Meyers Singleton' is on pp. 69ff.)
+ *
+ *   [meyers1998] Effective C++, Second Edition, Scott Meyers, Item 47: 'Ensure
+ *     that non-local static objects are initialized before they're used.'
+ *
+ *   [meyers1996] More Effective C++, Scott Meyers, Item 26 'Limiting the
+ *     number of objects of a class.'
+ *
+ *   [] https://stackoverflow.com/questions/13047526/difference-between-singleton-implemention-using-pointer-and-using-static-object
+ *
+ *   [] https://stackoverflow.com/questions/1661529/is-meyers-implementation-of-the-singleton-pattern-thread-safe
  */
+
+#include <iostream>
+
+namespace SingletonStuff
+{
+  class Singleton {
+  public: 
+    void doStuff() { std::cout << "Singleton::doStuff()" << std::endl; }
+  friend Singleton& theSingleton();
+  private:
+    Singleton();
+    Singleton(const Singleton& rhs);
+  };
+
+  Singleton& theSingleton()
+  {
+    static Singleton s;
+    return s;
+  }
+
+  Singleton::Singleton()
+  {
+    std::cout << "Singleton::Singleton()" << std::endl;
+  }
+}
+
+int main()
+{
+  {  // Method 1: include name of the namespace
+    SingletonStuff::theSingleton().doStuff();
+  }
+
+  {  // Method 2: employing a using declaration to save typing
+    using SingletonStuff::theSingleton;
+
+    theSingleton().doStuff();
+  }
+}
