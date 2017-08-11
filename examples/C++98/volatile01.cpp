083f40b3995577359b342137d28407db638d76b7
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-08-11 02:01:07 +0200

    Add reference for volatile.

--- examples/C++98/volatile01.cpp
+++ examples/C++98/volatile01.cpp
@@ -6,6 +6,7 @@
  *   [3] 'Concurrency with Modern C++', Meyers Singleton example around page 161
  *   [4] https://msdn.microsoft.com/en-us/library/12a04hfd.aspx
  *   [5] https://stackoverflow.com/questions/2484980/why-is-volatile-not-considered-useful-in-multithreaded-c-or-c-programming
+ *   [6] https://software.intel.com/en-us/blogs/2007/11/30/volatile-almost-useless-for-multi-threaded-programming
  *
  * TODO:
  *   There is a nice example for the volatile keyword in [3].
