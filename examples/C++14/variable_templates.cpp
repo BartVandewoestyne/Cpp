37f637d7ca6a8f0d83eb892ae4668d5a5f5ad21d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-08-23 00:36:30 +0200

    Add references for C++14 variable templates.

---
+++ examples/C++14/variable_templates.cpp
@@ -0,0 +1,8 @@
+/*
+ * References:
+ *   [1] https://baptiste-wicht.com/posts/2017/08/simplify-your-type-traits-with-c%2B%2B14-variable-templates.html
+ *   [2] https://www.ibm.com/developerworks/community/blogs/5894415f-be62-4bc0-81c5-3956e82276f3/entry/Introduction_to_Variable_Templates_of_C_14?lang=en
+ *   [3] https://meetingcpp.com/tl_files/mcpp/2015/talks/peter%20sommerlad%20-%20meetingc++2015variadicvariabletemplates.pdf
+ *   [4] https://isocpp.org/files/papers/N3651.pdf
+ *   [5] https://accu.org/index.php/journals/2087
+ */
