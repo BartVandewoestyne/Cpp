194aa56b7c593267983ee59258cb59caaaa93950
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-08-23 00:03:22 +0200

    Add example and references for std::optional.

--- examples/C++17/optional.cpp
+++ examples/C++17/optional.cpp
@@ -1,4 +1,29 @@
 /*
  * References:
- *   [1] TODO
+ *   [1] http://en.cppreference.com/w/cpp/utility/optional
+ *   [2] Overload 140, page 26
+ *       [https://accu.org/var/uploads/journals/Overload140.pdf]
  */
+
+#include <string>
+#include <iostream>
+#include <optional>
+ 
+// optional can be used as the return type of a factory that may fail
+std::optional<std::string> create(bool b) {
+    if (b)
+        return "Godzilla";
+    return {};
+}
+ 
+int main()
+{
+    std::cout << "create(false) returned "
+              << create(false).value_or("empty") << '\n';
+ 
+    // optional-returning factory functions are usable as conditions of
+    // while and if
+    if (auto str = create(true)) {
+        std::cout << "create(true) returned " << *str << '\n';
+    }
+}
