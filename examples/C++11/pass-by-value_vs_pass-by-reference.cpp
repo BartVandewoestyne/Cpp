e30526810653a7fd77808b2b7e3413341aae93a7
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-09-06 11:32:22 +0200

    Add references for pass-by-value vs pass-by-reference in C++11.

---
+++ examples/C++11/pass-by-value_vs_pass-by-reference.cpp
@@ -0,0 +1,16 @@
+/*
+ * References:
+ *   [meyers2014] Effective Modern C++, item 41.
+ *   [chopanza2014] WANT SPEED? DON’T (ALWAYS) PASS BY VALUE.
+ *                  https://juanchopanzacpp.wordpress.com/2014/05/11/want-speed-dont-always-pass-by-value/
+ *   [user2030677_2013] Why do we copy then move?
+ *                      https://stackoverflow.com/questions/16724657/why-do-we-copy-then-move
+ *   [kolpackov2012a] Efficient argument passing in C++11, Part 1
+ *                    http://www.codesynthesis.com/~boris/blog/2012/06/19/efficient-argument-passing-cxx11-part1/
+ *   [kolpackov2012b] Efficient argument passing in C++11, Part 2
+ *                    http://codesynthesis.com/~boris/blog/2012/06/26/efficient-argument-passing-cxx11-part2/
+ *   [kolpackov2012c] Efficient argument passing in C++11, Part 3
+ *                    http://codesynthesis.com/~boris/blog/2012/07/03/efficient-argument-passing-cxx11-part3/
+ *   [abrahams2009] Want Speed? Pass by Value
+ *                  https://web.archive.org/web/20140205194657/http://cpp-next.com/archive/2009/08/want-speed-pass-by-value/
+ */
