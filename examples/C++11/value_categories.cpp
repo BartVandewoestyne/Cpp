d8f3fe51e331f83693c8eabd82be68b17ef4c6d2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-09-07 16:29:53 +0200

    Add more references for explanations about value categories.

---
+++ examples/C++11/value_categories.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [mcnellis2010] What are rvalues, lvalues, xvalues, glvalues, and prvalues?
+ *                  https://stackoverflow.com/questions/3601602/what-are-rvalues-lvalues-xvalues-glvalues-and-prvalues
+ */
