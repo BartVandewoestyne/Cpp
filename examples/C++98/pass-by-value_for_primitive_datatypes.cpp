b1f74325b4b541e46b4306910f98c86b0cdc7410
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-09-15 09:35:56 +0200

    Add references to Stackoverflow posts about pass by value for primitive types.

---
+++ examples/C++98/pass-by-value_for_primitive_datatypes.cpp
@@ -0,0 +1,12 @@
+/*
+ * References:
+ *
+ *   [Nav2010] Is it better to pass by value or by reference for basic datatypes? [duplicate]
+ *             https://stackoverflow.com/questions/4112914/is-it-better-to-pass-by-value-or-by-reference-for-basic-datatypes
+ *
+ *   [user997112] Is it counter-productive to pass primitive types by reference? [duplicate]
+ *                https://stackoverflow.com/questions/14013139/is-it-counter-productive-to-pass-primitive-types-by-reference
+ *
+ *   [figueroa2012] Reasons to not pass simple types by reference?
+ *                  https://stackoverflow.com/questions/10770410/reasons-to-not-pass-simple-types-by-reference
+ */
