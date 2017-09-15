6725bc22f5bd92f75268f52309d3fa06fcffff7b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-09-15 09:24:36 +0200

    Add some references for const for non-reference parameters.

--- examples/C++98/const_for_non-reference_arguments.cpp
+++ examples/C++98/const_for_non-reference_arguments.cpp
@@ -1,6 +1,17 @@
 /*
+ * Example that illustrates how using const for non-reference parameters is
+ * about self-documenting your code and your assumptions.
+ *
  * References:
- *   [samaursa2011] https://stackoverflow.com/questions/6125425/const-for-non-reference-arguments
+ *
+ *   [scocco2012] Isn't 'const' redundant when passing by value?
+ *                https://stackoverflow.com/questions/8714250/isnt-const-redundant-when-passing-by-value
+ *
+ *   [samaursa2011] const for non-reference arguments
+ *                  https://stackoverflow.com/questions/6125425/const-for-non-reference-arguments
+ *
+ *   [rob2008] Use of 'const' for function parameters
+ *             https://stackoverflow.com/questions/117293/use-of-const-for-function-parameters
  */
 
 void f(const int x, int y)
