43139fd91ed48be501a661174224fb5a535f6896
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-09-18 11:40:52 +0200

    Add reference for std::pow.

---
+++ examples/C++98/pow.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [wicht2017] C++11 Performance tip: When to use std::pow ?
+ *               https://baptiste-wicht.com/posts/2017/09/cpp11-performance-tip-when-to-use-std-pow.html
+ */
