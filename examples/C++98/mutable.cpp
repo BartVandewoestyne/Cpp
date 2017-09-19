a4672724ae8218ce52fa206865a4ac9e2f89e68f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-09-19 11:30:12 +0200

    Add more reference for the mutable keyword.

---
+++ examples/C++98/mutable.cpp
@@ -0,0 +1,33 @@
+/**
+ * References:
+ *
+ *   [1] C++ Primer, fifth edition, page 274.
+ *
+ *   [save2012] Motivation for mutable in C++ [duplicate]
+ *              https://stackoverflow.com/questions/10212350/motivation-for-mutable-in-c
+ *
+ *   [acidzombie242010] When have you used C++ 'mutable' keyword?
+ *                      https://stackoverflow.com/questions/4554031/when-have-you-used-c-mutable-keyword
+ *
+ *   [pravasi] C++ mutable keyword
+ *             http://www.geeksforgeeks.org/c-mutable-keyword/
+ */
+
+#include <cstdlib>
+
+class Screen {
+
+public:
+    void some_member() const;
+
+private:
+    mutable size_t access_ctr;  // may change even in a const object
+    // other members as before
+
+};
+
+void Screen::some_member() const
+{
+    ++access_ctr;  // keep a count of the calls to any member function
+    // whatever other work this member needs to do
+}
