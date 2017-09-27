f4cd8e5b8382aafe543a7502f0d94ca0661ae96a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-09-27 08:48:18 +0200

    Add reference for undefined behavior.

--- examples/undefined_behavior.cpp
+++ examples/undefined_behavior.cpp
@@ -1,4 +1,5 @@
 /*
  * References:
  *   [1] https://www.nayuki.io/page/undefined-behavior-in-c-and-cplusplus-programs
+ *   [walfridsson2017] https://kristerw.blogspot.be/2017/09/why-undefined-behavior-may-call-never.html
  */
