03686042de8c9e48df229d4a6646fd2a1ac7ecce
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-09-29 09:56:22 +0200

    Add reference on metaclasses.

--- examples/C++20/metaclasses.cpp
+++ examples/C++20/metaclasses.cpp
@@ -1,7 +1,18 @@
 /*
  * References:
- *   [1] https://herbsutter.files.wordpress.com/2017/07/p0707r1.pdf
- *   [2] https://herbsutter.com/2017/07/26/metaclasses-thoughts-on-generative-c/
- *   [3] Thoughts on Metaclasses - Herb Sutter - Keynote [ACCU 2017] (https://youtu.be/6nsyX37nsRs)
- *   [4] http://www.fluentcpp.com/2017/08/04/metaclasses-cpp-summary/
+ *
+ *   [sutter2017cppcon] Meta: Thoughts on generative C++, Herb Sutter, CppCon 2017
+ *     https://youtu.be/4AfRAVcThyA
+ *
+ *   [boccara2017] A Summary of the Metaclasses Proposal for C++
+ *     http://www.fluentcpp.com/2017/08/04/metaclasses-cpp-summary/
+ *
+ *   [sutter2017accu] Thoughts on Metaclasses, Herb Sutter, ACCU 2017
+ *     https://youtu.be/6nsyX37nsRs)
+ *
+ *   [sutter2017metaclasses2] Metaclasses: Thoughts on generative C++
+ *     https://herbsutter.com/2017/07/26/metaclasses-thoughts-on-generative-c/
+ *
+ *   [sutter2017metaclasses1] Metaclasses: Generative C++
+ *     https://herbsutter.files.wordpress.com/2017/07/p0707r1.pdf
  */
