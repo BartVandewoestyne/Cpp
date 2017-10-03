5a9a89fc4d3cd9ca1f3b184364955e8d676d317c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-03 09:00:43 +0200

    Add reference for modules.

--- examples/C++20/modules.cpp
+++ examples/C++20/modules.cpp
@@ -1,4 +1,8 @@
 /*
  * References:
- *   [1] https://channel9.msdn.com/Events/CPP/CppCon-2016/CppCon-2016-Richard-Smith-There-and-Back-Again-An-Incremental-C-Modules-Design
+ *
+ *   [codesynthesis2017] The build2 Build System
+ *     https://build2.org/build2/doc/build2-build-system-manual.xhtml
+ *                       
+ *   [smith2016] https://channel9.msdn.com/Events/CPP/CppCon-2016/CppCon-2016-Richard-Smith-There-and-Back-Again-An-Incremental-C-Modules-Design
  */
