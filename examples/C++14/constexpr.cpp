63fb7f5a451254aac103d64f7c52f6cb4d781965
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-06 15:06:57 +0200

    Update references for constexpr.

--- examples/C++14/constexpr.cpp
+++ examples/C++14/constexpr.cpp
@@ -1,7 +1,12 @@
 /*
  * References:
  *
- *   [1] https://blog.quasardb.net/demystifying-constexpr/
- *   [2] http://stoppels.blog/posts/compile-time-primes
- *   [3] https://ryanmichaelcollins.github.io/blog/2017/05/14/fun-with-constexpr-and-variadic-templates
+ *   [collins2017] Fun with C++ constexpr, variadic templates, and fold expressions.
+ *     https://ryanmichaelcollins.github.io/blog/2017/05/14/fun-with-constexpr-and-variadic-templates
+ *
+ *   [stoppels2017] Compile-time primes.
+ *     http://stoppels.blog/posts/compile-time-primes
+ *
+ *   [edouard2016] Demystifying constexpr.
+ *     https://blog.quasardb.net/demystifying-constexpr/
  */
