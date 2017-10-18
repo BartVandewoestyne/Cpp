b4a6143d81b7fa1a34ce6bfc1a037bf0809e3363
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-18 10:30:00 +0200

    Add reference for std::accumulate.

--- examples/C++98/accumulate.cpp
+++ examples/C++98/accumulate.cpp
@@ -1,4 +1,9 @@
 /*
  * References:
- *   [1] https://channel9.msdn.com/Events/CPP/CppCon-2016/CppCon-2016-Ben-Deane-stdaccumulate-Exploring-an-Algorithmic-Empire
+ *
+ *   [boccara2017] (std::)Accumulate Knowledge On STL Algorithms
+ *     https://www.fluentcpp.com/2017/10/17/stdaccumulate-your-knowledge-on-algorithms/
+ *
+ *   [deane2016cppcon] std::accumulate: Exploring an Algorithmic Empire
+ *     https://channel9.msdn.com/Events/CPP/CppCon-2016/CppCon-2016-Ben-Deane-stdaccumulate-Exploring-an-Algorithmic-Empire
  */
