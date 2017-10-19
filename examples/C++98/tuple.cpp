e7fdfa785dbcf46a81cc759c68108c41632bd31e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-19 21:18:51 +0200

    Add reference for std::tuple.

--- examples/C++98/tuple.cpp
+++ examples/C++98/tuple.cpp
@@ -1,4 +1,8 @@
 /*
  * References:
+ *
+ *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
+ *     https://youtu.be/XkDEzfpdcSg?t=28m54s
+ *
  *   [1] https://arne-mertz.de/2017/03/smelly-pair-tuple/
  */
