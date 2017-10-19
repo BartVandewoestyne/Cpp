6b25c98ced53ef7f8b75daeb2a5bbb78ff5bbad5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-19 21:28:14 +0200

    Add reference for structured bindings.

--- examples/C++17/structured_bindings.cpp
+++ examples/C++17/structured_bindings.cpp
@@ -1,6 +1,12 @@
 /*
  * References:
+ *
+ *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
+ *     https://youtu.be/XkDEzfpdcSg?t=34m15s
+ *
  *   [1] http://insights.dice.com/2016/12/15/breaking-down-current-state-c-17/
+ *
  *   [2] https://skebanga.github.io/structured-bindings/
+ *
  *   [3] http://www.nuonsoft.com/blog/2017/07/26/c17-structured-bindings/
  */
