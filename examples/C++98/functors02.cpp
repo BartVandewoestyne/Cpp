bfb67eab667a87b3aee97d9aa8ae828b56619def
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-20 13:51:55 +0200

    Add reference for functors.

--- examples/C++98/functors02.cpp
+++ examples/C++98/functors02.cpp
@@ -1,4 +1,8 @@
 /*
  * References:
+ *
+ *   [boccara2017] Think of Function Objects as Functions Rather Than Objects
+ *     https://www.fluentcpp.com/2017/10/20/think-function-objects-functions-rather-objects/
+ *
  *   [1] http://www.fluentcpp.com/2017/01/23/stl-function-objects-stateless-is-stressless/
  */
