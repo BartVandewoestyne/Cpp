62db5a3d53b2b116cad07bafe7b6fe85267a6090
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-24 13:19:22 +0200

    Add references for nested classes.

---
+++ idioms/nested_classes.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [jayakumar2010] Why would one use nested classes in C++?
+ *     https://stackoverflow.com/questions/4571355/why-would-one-use-nested-classes-in-c
+ *
+ *   [ibm] Nested classes (C++ only)
+ *     https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.cbclx01/cplr061.htm
+ * /
