fde3db81087201699d117228bfb7f331b844bc55
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-28 15:43:43 +0200

    Add example code on std::any.

--- examples/C++17/any.cpp
+++ examples/C++17/any.cpp
@@ -1,4 +1,23 @@
 /*
  * References:
- *   [1] TODO
+ *
+ *   [grimm2017cpplibrary2nd] The C++ Standard Library, Second Edition, Rainer Grimm.
  */
+
+#include <any>
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct MyClass{};
+
+int main()
+{
+    std::vector<std::any> anyVec({true, 2017, std::string("test"), 3.14, MyClass()});
+
+    std::cout << std::any_cast<bool>(anyVec[0]) << std::endl;  // true
+    std::cout << std::any_cast<int>(anyVec[1]) << std::endl;  // 2017
+
+    std::cout << anyVec[0].type().name() << std::endl;  // b
+    std::cout << anyVec[1].type().name() << std::endl;  // i
+}
