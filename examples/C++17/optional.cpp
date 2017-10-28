66a422abb7eedc5453d150a0072b2f1678935fec
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-10-28 15:53:51 +0200

    Improve example on std::optional.

--- examples/C++17/optional.cpp
+++ examples/C++17/optional.cpp
@@ -4,20 +4,21 @@
  *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
  *     https://youtu.be/XkDEzfpdcSg?t=32m17s
  *
- *   [1] http://en.cppreference.com/w/cpp/utility/optional
- *
- *   [2] Overload 140, page 26
- *       [https://accu.org/var/uploads/journals/Overload140.pdf]
+ *   [fertig2017overload] About the C++ Core Guidelines, Andreas Fertig, Overload 140, page 26.
+ *     https://accu.org/var/uploads/journals/Overload140.pdf
  */
 
-#include <string>
 #include <iostream>
 #include <optional>
+#include <string>
  
-// optional can be used as the return type of a factory that may fail
-std::optional<std::string> create(bool b) {
+// std::optional can be used as the return type of a factory that may fail.
+std::optional<std::string> create(bool b)
+{
     if (b)
+    {
         return "Godzilla";
+    }
     return {};
 }
  
@@ -25,9 +26,14 @@ int main()
 {
     std::cout << "create(false) returned "
               << create(false).value_or("empty") << '\n';
+
+    std::cout << "create(true) returned "
+              << create(true).value_or("empty") << '\n';
  
-    // optional-returning factory functions are usable as conditions of
-    // while and if
+
+    // Optional-returning factory functions are usable as conditions of
+    // while and if.
+
     if (auto str = create(true)) {
         std::cout << "create(true) returned " << *str << '\n';
     }
