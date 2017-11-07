738b99d3b8bf913d1b1ce227ce6072e8e623e3eb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-07 09:30:14 +0100

    Add references for [[nodiscard]] attribute.

--- examples/C++17/nodiscard.cpp
+++ examples/C++17/nodiscard.cpp
@@ -3,4 +3,10 @@
  *
  *   [filipek2017nodiscard] [[nodiscard]]
  *     http://www.bfilipek.com/2017/11/nodiscard.html
+ *
+ *   [turner2016nodiscard] C++ Weekly - Ep 30 - C++17's [[nodiscard]] Attribute
+ *     https://youtu.be/l_5PF3GQLKc
+ *
+ *   [tomazos2015] Proposal of [[unused]], [[nodiscard]] and [[fallthrough]] attributes.
+ *     http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/p0068r0.pdf
  */
