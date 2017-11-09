0dc36208156561b6c1c4208d960721789a20fc53
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-09 09:40:19 +0100

    Add more references for auto.

--- examples/C++11/auto.cpp
+++ examples/C++11/auto.cpp
@@ -1,29 +1,32 @@
 /**
  * References:
  *
+ *   [guzman2017] Don't use C++ auto? Restricting auto is not the best decision for your company, and it is unfair to your developers. Instead, train them on how to use it.
+ *     http://swdevmastery.com/dont-use-c-auto-restricting-auto-is-not-the-best-decision-for-your-company-and-it-is-unfair-to-your-developers-instead-train-them-on-how-to-use-it/
+ *
  *   [carnie2016] Getting your head around auto's type-deduction rules
- *                https://blog.feabhas.com/2016/11/getting-head-around-autos-type-deduction-rules/
+ *     https://blog.feabhas.com/2016/11/getting-head-around-autos-type-deduction-rules/
  *
  *   [haldar2016] C++ 11 Auto: How to use and avoid abuse
- *                http://www.acodersjourney.com/2016/02/c-11-auto/
+ *     http://www.acodersjourney.com/2016/02/c-11-auto/
  *
  *   [barker2016] C++11 auto declaration with and without pointer declarator
- *                https://stackoverflow.com/questions/34560365/c11-auto-declaration-with-and-without-pointer-declarator
+ *     https://stackoverflow.com/questions/34560365/c11-auto-declaration-with-and-without-pointer-declarator
  *
  *   [mertz2015] Modern C++ Features - auto for variables
- *               http://arne-mertz.de/2015/07/new-c-features-auto-for-variables/
+ *     http://arne-mertz.de/2015/07/new-c-features-auto-for-variables/
  *
  *   [sutter2013] GotW #94 Solution: AAA Style (Almost Always Auto)
- *                http://herbsutter.com/2013/08/12/gotw-94-solution-aaa-style-almost-always-auto/
+ *     http://herbsutter.com/2013/08/12/gotw-94-solution-aaa-style-almost-always-auto/
  *
  *   [becker2013] C++ auto and decltype Explained
- *                http://thbecker.net/articles/auto_and_decltype/section_01.html
+ *     http://thbecker.net/articles/auto_and_decltype/section_01.html
  *
  *   [felics2012] Does auto make C++ code harder to understand?
- *                http://programmers.stackexchange.com/questions/180216/does-auto-make-c-code-harder-to-understand/
+ *     http://programmers.stackexchange.com/questions/180216/does-auto-make-c-code-harder-to-understand/
  *
  *   [roy???] With Respect to « Proper Usage » of auto
- *            http://h-deb.clg.qc.ca/Sujets/Divers--cplusplus/auto-when.html
+ *     http://h-deb.clg.qc.ca/Sujets/Divers--cplusplus/auto-when.html
  */
 
 #include <list>
