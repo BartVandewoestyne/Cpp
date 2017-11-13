a47b78fd65ea2383369b1d356f4d636bf1e0fadd
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-13 22:04:00 +0100

    Improve references for std::atomic.

--- examples/C++11/atomic.cpp
+++ examples/C++11/atomic.cpp
@@ -1,12 +1,28 @@
 /*
  * References:
- *   [1] https://www.quora.com/How-does-std-atomic-work-in-C++11
- *   [2] https://baptiste-wicht.com/posts/2012/07/c11-concurrency-tutorial-part-4-atomic-type.html
- *   [3] http://www.modernescpp.com/index.php/atomics
- *   [4] https://stackoverflow.com/questions/18850752/must-i-call-atomic-load-store-explicitly?noredirect=1&lq=1
- *   [5] https://stackoverflow.com/questions/34915926/is-assignment-equivalent-to-load-store-for-stdatomicbool?noredirect=1&lq=1
- *   [6] https://channel9.msdn.com/Shows/Going+Deep/Cpp-and-Beyond-2012-Herb-Sutter-atomic-Weapons-1-of-2
- *   [7] https://channel9.msdn.com/Shows/Going+Deep/Cpp-and-Beyond-2012-Herb-Sutter-atomic-Weapons-2-of-2
+ *
+ *   [grimm2016atomic] Atomics
+ *     http://www.modernescpp.com/index.php/atomics
+ *
+ *   [bgr2016] Is assignment equivalent to load/store for std::atomic<bool>
+ *     https://stackoverflow.com/questions/34915926/is-assignment-equivalent-to-load-store-for-stdatomicbool?noredirect=1&lq=1
+ *     
+ *   [meyers2014emc++] Effective Modern C++, Item 40: Use std::atomic for concurrency, volatile for special memory, Scott Meyers.
+ *
+ *   [quora2014] How does std::atomic work in C++11?
+ *     https://www.quora.com/How-does-std-atomic-work-in-C++11
+ *
+ *   [bavaza2013] Must I call atomic load/store explicitly?
+ *     https://stackoverflow.com/questions/18850752/must-i-call-atomic-load-store-explicitly?noredirect=1&lq=1
+ *
+ *   [wicht2012] C++11 Concurrency Tutorial - Part 4: Atomic Types
+ *     https://baptiste-wicht.com/posts/2012/07/c11-concurrency-tutorial-part-4-atomic-type.html
+ *
+ *   [sutter2012weapons1] C++ and Beyond 2012: Herb Sutter - atomic<> Weapons, 1 of 2
+ *     https://channel9.msdn.com/Shows/Going+Deep/Cpp-and-Beyond-2012-Herb-Sutter-atomic-Weapons-1-of-2
+ *
+ *   [sutter2012weapons2] C++ and Beyond 2012: Herb Sutter - atomic<> Weapons, 2 of 2
+ *     https://channel9.msdn.com/Shows/Going+Deep/Cpp-and-Beyond-2012-Herb-Sutter-atomic-Weapons-2-of-2
  */
 
 #include <thread>
