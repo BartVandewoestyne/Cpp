a532e03c3b74fb326c30cec5307b902f5193c6b2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-15 09:33:49 +0100

    Add reference for gsl::not_null.

--- examples/gsl/not_null.cpp
+++ examples/gsl/not_null.cpp
@@ -6,4 +6,7 @@
  *
  *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
  *     https://youtu.be/XkDEzfpdcSg?t=41m29s
+ *
+ *   [gregory2016] Using the not_null Template for Pointers That Must Never Be Null
+ *     https://visualstudiomagazine.com/articles/2016/06/01/using-the-not_null-template.aspx
  */
