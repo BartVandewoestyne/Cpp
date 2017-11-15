f45d7edfa717aafb7caa485e2d9c6a77ba624825
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-15 09:01:17 +0100

    Add reference to 'constexpr all the things' presentation.

--- examples/C++14/constexpr.cpp
+++ examples/C++14/constexpr.cpp
@@ -1,6 +1,10 @@
 /*
  * References:
  *
+ *   [deane2017turner] CppCon 2017: Ben Deane & Jason Turner “constexpr ALL the Things!”
+ *     https://youtu.be/PJwd4JLYJJY
+ *     https://github.com/CppCon/CppCon2017/tree/master/Presentations/constexpr%20ALL%20the%20things
+ *
  *   [collins2017] Fun with C++ constexpr, variadic templates, and fold expressions.
  *     https://ryanmichaelcollins.github.io/blog/2017/05/14/fun-with-constexpr-and-variadic-templates
  *
