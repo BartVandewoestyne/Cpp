50e26149a14aaa83f4e56e0a7addd9a4c760a0aa
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-15 09:29:42 +0100

    Add reference for gsl::not_null.

--- examples/gsl/not_null.cpp
+++ examples/gsl/not_null.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [filipek] How not_null can improve your code?
+ *     http://www.bfilipek.com/2017/10/notnull.html
+ *
  *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
  *     https://youtu.be/XkDEzfpdcSg?t=41m29s
  */
