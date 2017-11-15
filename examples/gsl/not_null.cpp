b863ae3ad537119cc85d66cda48d55af2f0fa3b9
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-15 09:51:40 +0100

    Add reference for gsl::not_null.

--- examples/gsl/not_null.cpp
+++ examples/gsl/not_null.cpp
@@ -1,7 +1,7 @@
 /*
  * References:
  *
- *   [filipek] How not_null can improve your code?
+ *   [filipek2017] How not_null can improve your code?
  *     http://www.bfilipek.com/2017/10/notnull.html
  *
  *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
@@ -9,4 +9,7 @@
  *
  *   [gregory2016] Using the not_null Template for Pointers That Must Never Be Null
  *     https://visualstudiomagazine.com/articles/2016/06/01/using-the-not_null-template.aspx
+ *
+ *   [mikhail2015] gsl::not_null<T*> vs. std::reference_wrapper<T> vs. T&
+ *     https://stackoverflow.com/questions/33306553/gslnot-nullt-vs-stdreference-wrappert-vs-t
  */
