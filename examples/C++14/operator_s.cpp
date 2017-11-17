d02b414cb76a6608668a6b083eafd6fa5e172f26
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-17 23:56:29 +0100

    Add references on operator""s.

---
+++ examples/C++14/operator_s.cpp
@@ -0,0 +1,8 @@
+/*
+ * References:
+ *
+ *   [cppreference] http://en.cppreference.com/w/cpp/string/basic_string/operator%22%22s
+ *
+ *   [guzman2017] A SIMPLE way to make your C++ programs easier to read NOW, Suffix for basic_string Literals - this little known feature can be extremely valuable
+ *     http://swdevmastery.com/a-simple-way-to-make-your-c-programs-easier-to-read-now-suffix-for-basic_string-literals-this-little-known-feature-can-be-extremely-valuable/
+ */
