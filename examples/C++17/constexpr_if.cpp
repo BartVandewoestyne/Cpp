e9f6408f0c314ea6f039376e077050aea58292f8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-20 11:44:15 +0100

    Add reference for constexpr if.

--- examples/C++17/constexpr_if.cpp
+++ examples/C++17/constexpr_if.cpp
@@ -1,5 +1,9 @@
 /*
  * References:
  *
- *   [1] https://tartanllama.github.io/c++/2016/12/12/if-constexpr/
+ *   [groarke2017] Simplifying Compile-Time Options With if constexpr
+ *     https://philippegroarke.com/blog/2017/11/20/simplifying-compile-time-options-with-if-constexpr/
+ *
+ *   [llama2016] Simplifying templates and #ifdefs with if constexpr
+ *     https://tartanllama.github.io/c++/2016/12/12/if-constexpr/
  */
