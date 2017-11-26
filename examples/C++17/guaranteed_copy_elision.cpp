da3f9bc740f5536f76d23f443033d9a4789d2c5e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-26 23:04:51 +0100

    Add reference for guaranteed copy elision.

--- examples/C++17/guaranteed_copy_elision.cpp
+++ examples/C++17/guaranteed_copy_elision.cpp
@@ -1,5 +1,9 @@
 /*
  * References:
+ *
+ *   [doumler2017] Timur Doumler - The new C++17 and why it's good for you.
+ *     https://youtu.be/ky_eXBMcJK0?t=23m30s
+ *
  *   [1] http://insights.dice.com/2016/12/15/breaking-down-current-state-c-17/
  *   [2] http://www.bfilipek.com/2017/01/cpp17features.html#guaranteed-copy-elision
  *   [3] http://www.fluentcpp.com/2016/11/28/return-value-optimizations/
