9eadd7cd7c5d4d0cfb4bad0a9b9ad75fc6159647
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-11-29 09:34:23 +0100

    Add reference for Lippincot functions.

---
+++ idioms/lippincot_functions.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [turner2017] C++ Weekly - Ep 91 - Using Lippincott Functions
+ *     https://youtu.be/-amJL3AyADI
+ *
+ *   [guillemot2013] Using a Lippincott Function for Centralized Exception Handling
+ *     http://cppsecrets.blogspot.be/2013/12/using-lippincott-function-for.html
+ */
