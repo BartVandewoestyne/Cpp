bf8307bbcc33eaf54786bce448bf8f2e75c38dce
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-12-07 10:56:04 +0100

    Add/update references for C++98/C++11 iterator invalidation rules.

--- examples/C++11/iterator/iterator_invalidation_rules.cpp
+++ examples/C++11/iterator/iterator_invalidation_rules.cpp
@@ -3,4 +3,7 @@
  *
  *   [lachecki2011] Iterator Invalidation Rules (C++0x)
  *     http://kera.name/articles/2011/06/iterator-invalidation-rules-c0x/
+ *
+ *   [feng2011] C++ Container Iterator Invalidation
+ *     http://www.outofcore.com/2011/04/c-container-iterator-invalidation/
  */
---
+++ examples/C++98/iterator/iterator_invalidation.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [langer2002] Invalidation of STL Iterators
+ *     http://www.angelikalanger.com/Conferences/Slides/CppInvalidIterators-DevConnections-2002.pdf
+ */
