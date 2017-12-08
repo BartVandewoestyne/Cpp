778fbafa06b60d31697615ae8f0977e9ab66a15d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-12-08 09:19:47 +0100

    Update references for std::valarray.

--- examples/C++98/valarray.cpp
+++
@@ -1,8 +0,0 @@
-/*
- * Note: seems like valarray is redundant and could use revision or removal,
- *       see [2].  Probably better not to use it.
- *
- * References:
- *   [1] http://cppisland.com/?p=518
- *   [2] http://www.gotw.ca/publications/mill01.htm
- */
---
+++ examples/C++98/valarray/valarray.cpp
@@ -0,0 +1,12 @@
+/*
+ * Note: seems like valarray is redundant and could use revision or removal,
+ *       see [2].  Probably better not to use it.
+ *
+ * References:
+ *
+ *   [gonen20170206] Introduction To valarray
+ *     http://cppisland.com/?p=518
+ *
+ *   [sutter199801XX] C++ State of the Union
+ *     http://www.gotw.ca/publications/mill01.htm
+ */
