9a0493b8fb7c06c09d27ff8020ea11dfaf69fa97
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-12-24 03:40:58 +0100

    Add reference for aggregate initialization.

---
+++ examples/C++98/aggregate_initialization.cpp
@@ -0,0 +1,11 @@
+/**
+ * References:
+ *
+ *   [cppreference] aggregate initialization
+ *     http://en.cppreference.com/w/cpp/language/aggregate_initialization
+ */
+
+int main()
+{
+    int ar[] = {1, 2, 3};  // ar is int[3]
+}
