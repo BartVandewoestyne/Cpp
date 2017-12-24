ba3418a35d6862b26d85b774d45fdf990a6dac1f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2017-12-24 03:13:50 +0100

    Improve whitespace in references.

--- idioms/detection_idiom.cpp
+++ idioms/detection_idiom.cpp
@@ -2,12 +2,11 @@
  * References:
  *
  *   [clow2017] The 'Detection idiom:' A Better Way to SFINAE, Marshall Clow at C++Now2017.
- *              https://youtu.be/U3jGdnRL3KI
+ *     https://youtu.be/U3jGdnRL3KI
  *
  *   [brand2017] Detection Idiom - A Stopgap for Concepts
- *               https://blog.tartanllama.xyz/detection-idiom/
+ *     https://blog.tartanllama.xyz/detection-idiom/
  *
- *   [brown2015] Proposing Standard Library Support for the C++ Detection Idiom,
-v2
- *               http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/n4502.pdf
+ *   [brown2015] Proposing Standard Library Support for the C++ Detection Idiom, v2
+ *     http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/n4502.pdf
  */
