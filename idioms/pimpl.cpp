773c5199a35d23eaf28cba0a0f9a1049a20debb5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-01-11 09:40:17 +0100

    Update PIMPL references.

--- idioms/pimpl.cpp
+++ idioms/pimpl.cpp
@@ -1,8 +1,21 @@
 /*
  * References:
- *   [1] https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl/
- *   [2] https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl-reloaded/
- *   [3] http://lattix.com/blog/2017/04/21/reduce-c-build-times-part-2-pimpl-idiom
- *   [4] http://www.cppsamples.com/common-tasks/pimpl.html
- *   [5] https://jonasdevlieghere.com/reference-counting-pimpl-idiom/
+ *
+ *   [cpppatterns] The PIMPL idiom
+ *     http://www.cppsamples.com/common-tasks/pimpl.html
+ *
+ *   [filipek20180108] The Pimpl Pattern - what you should know
+ *     http://www.bfilipek.com/2018/01/pimpl.html
+ *
+ *   [devlieghere20170808] Reference Counting PIMPL Idiom
+ *     https://jonasdevlieghere.com/reference-counting-pimpl-idiom/
+ *
+ *   [barow20170421] Reduce C++ Build Times (Part 2) with the Pimpl Idiom
+ *     http://lattix.com/blog/2017/04/21/reduce-c-build-times-part-2-pimpl-idiom
+ *
+ *   [mutz20110814] Pimp My Pimpl - Reloaded
+ *     https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl-reloaded/
+ *
+ *   [mutz20110416] Pimp My Pimpl (Part One)
+ *     https://marcmutz.wordpress.com/translated-articles/pimp-my-pimpl/
  */
