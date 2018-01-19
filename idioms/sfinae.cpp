73748d731a6cc860aea93874b7e8363460afb6c0
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-01-20 00:25:12 +0100

    Improve references for SFINAE.

--- idioms/sfinae.cpp
+++ idioms/sfinae.cpp
@@ -1,10 +1,24 @@
 /*
  * References:
  *
- *   [1] http://codeofthedamned.com/index.php/sfinae
- *   [2] http://jguegant.github.io/blogs/tech/sfinae-introduction.html
- *   [3] http://www.bfilipek.com/2016/02/notes-on-c-sfinae.html
- *   [4] http://www.bfilipek.com/2016/02/sfinae-followup.html
- *   [5] http://en.wikipedia.org/wiki/Substitution_failure_is_not_an_error
- *   [6] https://foonathan.github.io/blog/2015/11/30/overload-resolution-4.html
+ *   [wikipedia] Substitution failure is not an error
+ *     http://en.wikipedia.org/wiki/Substitution_failure_is_not_an_error
+ *
+ *   [warzecha20171110] An inspiring introduction to Templatemetaprogramming.
+ *     https://youtu.be/UnIc_qJ0DRc?t=19m43s
+ *
+ *   [filipek20160225] SFINAE Followup
+ *     http://www.bfilipek.com/2016/02/sfinae-followup.html
+ *
+ *   [filipek20160218] Notes on C++ SFINAE
+ *     http://www.bfilipek.com/2016/02/notes-on-c-sfinae.html
+ *
+ *   [mueller20151130] Controlling overload resolution #4: SFINAE
+ *     https://foonathan.github.io/blog/2015/11/30/overload-resolution-4.html
+ *
+ *   [guegant20151031] An introduction to C++'s SFINAE concept: compile-time introspection of a class member
+ *     http://jguegant.github.io/blogs/tech/sfinae-introduction.html
+ *
+ *   [watt20150127] C++: SFINAE
+ *     http://codeofthedamned.com/index.php/sfinae
  */
