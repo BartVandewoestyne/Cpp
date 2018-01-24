9a92a779ec6fc62e98ee52fc0b6453570e750978
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-01-24 16:52:03 +0100

    Improve references for mutable.

--- examples/C++98/mutable.cpp
+++ examples/C++98/mutable.cpp
@@ -1,22 +1,23 @@
 /**
  * References:
  *
- *   [mertz2017] Mutable
+ *   [mertz20171018] Mutable
  *     https://arne-mertz.de/2017/10/mutable/
  *
- *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
+ *   [gregory20170927] 10 Core Guidelines You Need to Start Using Now
  *     https://youtu.be/XkDEzfpdcSg?t=14m38s
  *
- *   [1] C++ Primer, fifth edition, page 274.
+ *   [pravasi20140705] C++ mutable keyword
+ *     http://www.geeksforgeeks.org/c-mutable-keyword/
  *
- *   [save2012] Motivation for mutable in C++ [duplicate]
- *              https://stackoverflow.com/questions/10212350/motivation-for-mutable-in-c
+ *   [lipmann20120816] C++ Primer, fifth edition, page 274.
+ *     http://www.informit.com/store/c-plus-plus-primer-9780321714114
  *
- *   [acidzombie242010] When have you used C++ 'mutable' keyword?
- *                      https://stackoverflow.com/questions/4554031/when-have-you-used-c-mutable-keyword
+ *   [save20120418] Motivation for mutable in C++ [duplicate]
+ *     https://stackoverflow.com/questions/10212350/motivation-for-mutable-in-c
  *
- *   [pravasi] C++ mutable keyword
- *             http://www.geeksforgeeks.org/c-mutable-keyword/
+ *   [user34537_20101229] When have you used C++ 'mutable' keyword?
+ *     https://stackoverflow.com/questions/4554031/when-have-you-used-c-mutable-keyword
  */
 
 #include <cstdlib>
@@ -28,6 +29,7 @@ public:
 
 private:
     mutable size_t access_ctr;  // may change even in a const object
+
     // other members as before
 
 };
@@ -35,5 +37,6 @@ private:
 void Screen::some_member() const
 {
     ++access_ctr;  // keep a count of the calls to any member function
+
     // whatever other work this member needs to do
 }
