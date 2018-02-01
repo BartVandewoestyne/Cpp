d89e8a4d31713f18b399bf7bad2a52a22ecb8ca1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-02-01 14:16:33 +0100

    Add reference for private virtuals.

---
+++ idioms/private_virtuals.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [C++Super-FAQ] When should someone use private virtuals?
+ *     https://isocpp.org/wiki/faq/strange-inheritance#private-virtuals
+ *
+ *   [silverburgh20100131] Private virtual method in C++
+ *     https://stackoverflow.com/questions/2170688/private-virtual-method-in-c
+ */
