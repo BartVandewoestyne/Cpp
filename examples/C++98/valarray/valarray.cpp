2b844844d7e6540c399a7c899b7251fdcea75cd2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-02-01 11:56:32 +0100

    Add reference for valarray.

--- examples/C++98/valarray/valarray.cpp
+++ examples/C++98/valarray/valarray.cpp
@@ -2,10 +2,16 @@
  * Note: seems like valarray is redundant and could use revision or removal,
  *       see [sutter199801XX].  Probably better not to use it.
  *
+ * TODO: check what the alternatives to std::valarray are.  Is there something in
+ *       the standard?  Does one need to use a library?
+ *
  * References:
  *
  *   [gonen20170206] Introduction To valarray
  *     http://cppisland.com/?p=518
+ *
+ *   [andand20100405] What's the future of std::valarray look like?
+ *     https://stackoverflow.com/questions/2576688/whats-the-future-of-stdvalarray-look-like
  *
  *   [sutter199801XX] C++ State of the Union
  *     http://www.gotw.ca/publications/mill01.htm
