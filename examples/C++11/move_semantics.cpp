dd72b3a953f427583b6aeabb8647381e9b8d3566
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-02-13 09:03:23 +0100

    Add reference for move semantics.

--- examples/C++11/move_semantics.cpp
+++ examples/C++11/move_semantics.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [Z01_20180212] My Precious Compile Time Bool (long introduction to Move Semantics)
+ *     https://medium.com/@gaussnoise/my-precious-compile-time-bool-long-introduction-to-move-semantics-c9ee73c370c7
+ *
  *   [maudal20180205] Modern C++ Explained: Move Semantics, Olve Maudal.
  *     http://www.pvv.org/~oma/ModernCPPExplained_MoveSemantics_Feb2018.pdf
  */
