04f32fcd6fb366a8d376d7b671fbe155afe92b12
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-02-19 14:04:44 +0100

    Add example on struct typedef.

---
+++ examples/C++98/struct_typedef.cpp
@@ -0,0 +1,30 @@
+/*
+ * References:
+ *
+ *   [criddell20090304] Difference between 'struct' and 'typedef struct' in C++?
+ *     https://stackoverflow.com/questions/612328/difference-between-struct-and-typedef-struct-in-c
+ */
+
+struct Foo {
+  int x;
+  int y;
+};
+
+
+// Disadvantage:
+//   * typedefs cannot be forward declared
+typedef struct {
+  int a;
+  int b;
+} Bar;
+
+
+class Foo;    // OK.
+//class Bar;  // Error!  Typedefs cannot be forward-declared.
+
+
+int main()
+{
+  Foo f;
+  Bar b;
+}
