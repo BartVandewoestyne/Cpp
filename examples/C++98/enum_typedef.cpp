729a781c598ae2ea875d2593ac0739fa83ef376d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-02-21 10:55:53 +0100

    Add reference for typedef enum vs enum.

---
+++ examples/C++98/enum_typedef.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [merrifield20081221] Why do you use typedef when declaring an enum in C++?
+ *     https://stackoverflow.com/questions/385023/why-do-you-use-typedef-when-declaring-an-enum-in-c
+ */
