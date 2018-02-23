0e8702f96008cc147f6f47e259e2ac7c8d1c2567
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-02-23 16:26:19 +0100

    Add reference about namespaces in header and source files.

---
+++ examples/C++98/namespace_in_header_and_source.cpp
@@ -0,0 +1,5 @@
+/*
+ * References:
+ *   [mrboy20111230] Correct way to define C++ namespace methods in .cpp file
+ *     https://stackoverflow.com/questions/8681714/correct-way-to-define-c-namespace-methods-in-cpp-file
+ */
