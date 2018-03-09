88d22bf6ea16bd0eae552a4d9815174be1790c7b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-09 14:11:44 +0100

    Add reference for explicitly typed initializer idiom.

---
+++ idioms/explicitly_typed_initializer.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [clchiou20170109] Item 6: Use the Explicitly Typed Initializer Idiom When auto Deduces Undesired Types
+ *     http://clchiou.github.io/notes-effective-modern-c++/2017-01-09/item-06-use-the-explicitly-typed-initializer-idiom-when-auto-deduces-undesired-types
+ */
