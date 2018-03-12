22ef37d5e3dab50429be7649d4c2637c02a6ec9c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-12 10:55:29 +0100

    Add reference to blogpost about internal and external linkage.

---
+++ examples/C++98/linkage_internal_vs_external.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [goldsborough20160330] Internal and External Linkage in C++
+ *     http://www.goldsborough.me/c/c++/linker/2016/03/30/19-34-25-internal_and_external_linkage_in_c++/
+ */
