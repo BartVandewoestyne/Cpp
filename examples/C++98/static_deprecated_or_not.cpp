7db938dfbd1a2c32becb06fd3636b10f95bb44d2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-12 10:41:15 +0100

    Add article about static being deprecated or not.

---
+++ examples/C++98/static_deprecated_or_not.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [matthieum] Deprecation of the static keyword… no more?
+ *     https://stackoverflow.com/questions/4726570/deprecation-of-the-static-keyword-no-more
+ */
