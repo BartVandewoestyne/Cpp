8c850c7be5520cef83321dd9a1e5bb465802f7c8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-14 11:16:21 +0100

    Rename namespaces files.

---
+++ examples/C++98/namespaces_unnamed_vs_static.cpp
@@ -0,0 +1,13 @@
+/*
+ * References:
+ *
+ *   [nawaz20110212] Why an unnamed namespace is a superior alternative to static? [duplicate]
+ *     https://stackoverflow.com/questions/4977252/why-an-unnamed-namespace-is-a-superior-alternative-to-static
+ *
+ *   [nawaz20101212] Superiority of unnamed namespace over static?
+ *     https://stackoverflow.com/questions/4422507/superiority-of-unnamed-namespace-over-static
+ *
+ *   [headgeek20080930] Unnamed/anonymous namespaces vs. static functions
+ *     https://stackoverflow.com/questions/154469/unnamed-anonymous-namespaces-vs-static-functions
+ *
+ */
