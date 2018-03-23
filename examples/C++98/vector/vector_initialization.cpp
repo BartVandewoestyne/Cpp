0fd003d4f2a353645b767749253ac8832088d513
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-23 12:05:28 +0100

    Add attempt for example on how to initialize vector from array in C++98.

---
+++ examples/C++98/vector/vector_initialization.cpp
@@ -0,0 +1,37 @@
+// This is my own attempt to try to find a way to initialize
+// a vector in a similar way as with C++11 initializer lists.
+// TODO: finish this, init_vect1 is not working because arraySize
+// is incorrect due to the incorrect sizeof.
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::vector<T> init_vec1(const T arr[])
+{
+    // TODO: how to get the size???
+    const size_t arraySize = sizeof(*arr)/sizeof(arr[0]);
+    std::cout << "arraySize = " << arraySize << std::endl;
+
+    std::vector<T> v(arr, arr + arraySize);
+    return v;
+}
+
+template <typename T>
+std::vector<T> init_vec2(const T arr[], int N)
+{
+    std::vector<T> v(arr, arr + N);
+    return v;
+}
+
+int main()
+{
+    // Works in C++11.
+    // std::vector<int> v = {1, 2, 3};
+
+    const int a[] = {1, 2, 3};
+    std::vector<int> va = init_vec1(a);
+
+    const float b[] = {1.0, 2.0, 3.0};
+    std::vector<float> vb = init_vec2(b, 3);
+}
