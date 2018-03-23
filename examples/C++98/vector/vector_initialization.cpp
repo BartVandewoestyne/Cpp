9a69d69973c174411252b000a7857dbf7c2055c3
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-23 12:13:29 +0100

    Add reference.

--- examples/C++98/vector/vector_initialization.cpp
+++ examples/C++98/vector/vector_initialization.cpp
@@ -2,15 +2,23 @@
 // a vector in a similar way as with C++11 initializer lists.
 // TODO: finish this, init_vect1 is not working because arraySize
 // is incorrect due to the incorrect sizeof.
+//
+// https://stackoverflow.com/questions/2236197/what-is-the-easiest-way-to-initialize-a-stdvector-with-hardcoded-elements
 
 #include <iostream>
 #include <vector>
+
+template <typename T, std::size_t N>
+inline std::size_t size_of_array( const T [N] ) {
+   return N;
+}
 
 template <typename T>
 std::vector<T> init_vec1(const T arr[])
 {
     // TODO: how to get the size???
-    const size_t arraySize = sizeof(*arr)/sizeof(arr[0]);
+    //const size_t arraySize = sizeof(*arr)/sizeof(arr[0]);
+    const std::size_t arraySize = size_of_array(arr);
     std::cout << "arraySize = " << arraySize << std::endl;
 
     std::vector<T> v(arr, arr + arraySize);
