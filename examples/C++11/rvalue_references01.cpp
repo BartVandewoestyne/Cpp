a70d58d92061a3556968ef8d0a08a5525e764a96
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-27 08:51:24 +0200

    Improve references for rvalue references.

--- examples/C++11/rvalue_references01.cpp
+++ examples/C++11/rvalue_references01.cpp
@@ -1,24 +1,27 @@
 /**
  * References:
  *
+ *   [müller20180326] Guidelines For Rvalue References In APIs
+ *     https://foonathan.net/blog/2018/03/26/rvalue-references-api-guidelines.html
+ *
  *   [boccara20180206] Lvalues, rvalues and their references
  *     https://www.fluentcpp.com/2018/02/06/understanding-lvalues-rvalues-and-their-references/
  *
- *   [holmes2017] One-Shot Learning of C++ r-value, &&, and Move
+ *   [holmes20170918] One-Shot Learning of C++ r-value, &&, and Move
  *     https://hackernoon.com/one-shot-learning-of-c-r-value-and-move-27e5d6bcec3b
  *
  *   [lavavej2014] CppCon 2014: Stephan Lavavej "STL Features And Implementation Techniques"
  *     https://www.youtube.com/watch?v=dTeKf5Oek2c&feature=youtu.be&t=2501
  *
- *   [tarantula2013] C++11 rvalues and move semantics confusion (return statement)
- *     https://stackoverflow.com/questions/4986673/c11-rvalues-and-move-semantics-confusion-return-statement
- *
- *   [becker2013] C++ Rvalue References Explained
+ *   [becker201303XX] C++ Rvalue References Explained
  *     http://thbecker.net/articles/rvalue_references/section_01.html
  *
- *   [kolpackov2012] What are const rvalue references good for?
+ *   [kolpackov20120724] What are const rvalue references good for?
  *     http://www.codesynthesis.com/~boris/blog/2012/07/24/const-rvalue-references/
  *
- *   [hinnant2008et_al] A Brief Introduction to Rvalue References
+ *   [tarantula20110213] C++11 rvalues and move semantics confusion (return statement)
+ *     https://stackoverflow.com/questions/4986673/c11-rvalues-and-move-semantics-confusion-return-statement
+ *
+ *   [hinnant_stroustrup_kozicki20080310] A Brief Introduction to Rvalue References
  *     http://www.artima.com/cppsource/rvalue.html
  */
