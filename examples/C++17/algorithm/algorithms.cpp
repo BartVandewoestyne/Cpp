b6eeb5d618960df1be0c597c083675430d8330b6
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-03-28 08:36:39 +0200

    Add reference for parallel STL.

--- examples/C++17/algorithm/algorithms.cpp
+++ examples/C++17/algorithm/algorithms.cpp
@@ -1,4 +1,8 @@
 /*
  * References:
+ *
+ *   [20180327ignatchenko] Using Parallel <algorithm> Without a Clue: 90x Performance Loss Instead of 8x Gain
+ *     http://ithare.com/using-parallel-algorithm-without-a-clue-90x-performance-loss-instead-of-8x-gain/
+ *
  *   [1] https://www.heise.de/developer/artikel/Neue-Algorithmen-in-C-17-3704576.html
  */
