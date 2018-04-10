85656f6fd86e63eceb7e1ece5d4890d08a93c457
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-04-10 10:00:53 +0200

    Add references about constructor failures.

---
+++ examples/C++98/constructor_failures.cpp
@@ -0,0 +1,16 @@
+/*
+ * References:
+ *
+ *   [cppfaq] How can I handle a constructor that fails?
+ *     https://isocpp.org/wiki/faq/exceptions#ctors-can-throw
+ *
+ *   [muller20170109] How to handle errors in constructors without exceptions?
+ *     https://foonathan.net/blog/2017/01/09/exceptions-constructor.html
+ *
+ *   [mertz20160127] Constructor Failures
+ *     https://arne-mertz.de/2016/01/constructor-failures/
+ *
+ *   [sutter200011XX] GotW #66: Constructor Failures
+ *     http://www.gotw.ca/gotw/066.htm
+ *     http://www.gotw.ca/publications/mill13.htm
+ */
