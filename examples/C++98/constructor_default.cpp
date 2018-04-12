eba4897ceb0604b39dfa3981b90768ebdcad0065
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-04-12 10:10:12 +0200

    Add references explaining when to have a default constructor.

---
+++ examples/C++98/constructor_default.cpp
@@ -0,0 +1,10 @@
+/*
+ * References:
+ *
+ *   [nicobako20180412] Should my classes/structs have default constructors?
+ *     https://www.reddit.com/r/cpp/comments/8bmkk1/should_my_classesstructs_have_default_constructors/
+ *
+ *   [trupti20101219] Why would someone provide an empty default constructor for a class?
+ *     https://stackoverflow.com/questions/4482113/why-would-someone-provide-an-empty-default-constructor-for-a-class
+ *
+ */
