50e1e39fa5db42b11d4309ed4ea733fa7b7865f3
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-04-17 21:41:51 +0200

    Add more links to RAII examples.

--- idioms/C++98/raii.cpp
+++ idioms/C++98/raii.cpp
@@ -1,6 +1,19 @@
 /*
  * References:
  *
+ *   [wikibooks] C++ Programming/RAII
+ *     https://en.wikibooks.org/wiki/C%2B%2B_Programming/RAII
+ *
+ *   [hanna] The RAII Programming Idiom
+ *     https://www.hackcraft.net/raii
+ *
  *   [boccara20180213] To RAII or Not to RAII?
  *     https://www.fluentcpp.com/2018/02/13/to-raii-or-not-to-raii/
+ *
+ *   [dalling20120517] Resource Acquisition is Initialisation (RAII) Explained
+ *     https://www.tomdalling.com/blog/software-design/resource-acquisition-is-initialisation-raii-explained/
+ *
+ *   [tambe2007] More C++ Idioms/Resource Acquisition Is Initialization
+ *     https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Resource_Acquisition_Is_Initialization
+ *
  */
