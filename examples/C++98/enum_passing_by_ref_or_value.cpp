f91e2aa8802fd89d707ca33d4253279e7d5d7543
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-04-17 10:23:08 +0200

    Add file on when to pass enums by value or reference.

---
+++ examples/C++98/enum_passing_by_ref_or_value.cpp
@@ -0,0 +1,20 @@
+/*
+ * Should one pass an enum by value or by reference?
+ *
+ *   Pass by value:
+ *     Pro's:
+ *     Con's:
+ *   Pass by reference:
+ *     Pro's:
+ *       - If you are later going to replace them with a class, then keeping the API
+ *         the same and enforcing the const-ness might make sense.
+ *     Con's:
+ *
+ * References:
+ *
+ *   [pilpel20160122] Pass an enum to a function - by value or by reference? [duplicate]
+ *     https://stackoverflow.com/questions/34943170/pass-an-enum-to-a-function-by-value-or-by-reference
+ *
+ *   [patton20110726] C++: Is it better to pass an enum as a value or as a const reference?
+ *     https://stackoverflow.com/questions/6834581/c-is-it-better-to-pass-an-enum-as-a-value-or-as-a-const-reference
+ */
