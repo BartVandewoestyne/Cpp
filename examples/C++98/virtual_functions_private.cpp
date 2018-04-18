789cf99cd6ea24aeaa87ac56cbb619a783806be8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-04-18 15:02:41 +0200

    Add reference for private virtual functions.

---
+++ examples/C++98/virtual_functions_private.cpp
@@ -0,0 +1,10 @@
+/*
+ * Herb Sutter's guideline is:
+ *
+ *   "Prefer to make virtual functions private."
+ *
+ * References:
+ *
+ *   [sutter200109XX] Virtuality
+ *     http://www.gotw.ca/publications/mill18.htm
+ */
