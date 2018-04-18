f8202e9d1cdb772870238fdc9d851fe9e3257153
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-04-18 21:48:20 +0200

    Add reference about forward-declaring templates and enums.

---
+++ examples/C++98/forward_declarations.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [mertz20180321] Forward-declaring Templates and Enums
+ *     https://arne-mertz.de/2018/03/forward-declaring-templates-and-enums/
+ *
+ *   [mertz20180314] Forward Declarations to Reduce Compiletime Dependencies
+ *     https://arne-mertz.de/2018/03/forward-declarations/
+ */
