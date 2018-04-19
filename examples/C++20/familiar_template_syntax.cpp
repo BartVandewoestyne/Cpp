a094ac9b355d989b2d71b7587e3ddbe49186f249
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-04-19 09:33:35 +0200

    Add reference for familiar template syntax for generic lambdas.

---
+++ examples/C++20/familiar_template_syntax.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [romeo20180416] compile-time iteration with C++20 lambdas
+ *     https://vittorioromeo.info/index/blog/cpp20_lambdas_compiletime_for.html
+ *
+ *   [dionne20170713] Familiar template syntax for generic lambdas
+ *     http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/p0428r2.pdf
+ */
