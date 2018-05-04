c30dd0d51c801b4ea744818abb0a441fbec6939a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-04 17:02:23 +0200

    Add reference to blogpost about std::move vs std::forward.

---
+++ examples/C++11/utility/move_vs_forward.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [carnie20180426] Your handy cut-out-and-keep guide to std::forward and std::move
+ *     https://blog.feabhas.com/2018/04/handy-cut-keep-guide-stdforward-stdmove/
+ */
