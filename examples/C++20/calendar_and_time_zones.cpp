6d6d71ea8d636992303454c9d1b8db56a616fcae
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-04 17:09:12 +0200

    Add reference.

--- examples/C++20/calendar_and_time_zones.cpp
+++ examples/C++20/calendar_and_time_zones.cpp
@@ -1,5 +1,9 @@
 /*
  * References:
  *
- *   https://www.reddit.com/r/cpp/comments/854mu9/2018_jacksonville_iso_c_committee_reddit_trip/
+ *   [bancila20180327] C++20 calendars and time zones
+ *     http://mariusbancila.ro/blog/2018/03/27/cpp20-calendars-and-time-zones/
+ *
+ *   [lelbach20180317] 2018 Jacksonville ISO C++ Committee Reddit Trip Report
+ *     https://www.reddit.com/r/cpp/comments/854mu9/2018_jacksonville_iso_c_committee_reddit_trip/
  */
