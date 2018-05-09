f7d5cb80144b4cab3a0e1346f13cb6e43fe3308c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-09 08:40:38 +0200

    Add reference to blogpost about converting enum to string and back.

---
+++ examples/C++11/enum_to_string.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [stephens20180505] Converting enum classes to strings and back in C++.
+ *     https://sheep.horse/2018/5/converting_enum_classes_to_strings_and_back_in_c++.html
+ *     https://github.com/andrewstephens75/EnumMapping
+ *     https://www.reddit.com/r/cpp/comments/8hwk91/converting_enum_classes_to_strings_and_back_in_c/
+ *     https://godbolt.org/g/HED96S
+ */
---
+++ examples/C++98/iterator/iterator.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [boccara20180508] std::iterator is deprecated: Why, What It Was, and What to Use Instead
+ *     https://www.fluentcpp.com/2018/05/08/std-iterator-deprecated/
+ */
