0ef3e82d166367b1fa8c9007d6546b1562c70c2b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-16 21:29:43 +0200

    Add reference to articles on span.

--- examples/gsl/span.cpp
+++ examples/gsl/span.cpp
@@ -3,6 +3,4 @@
  *
  *   [jabot20180514] A can of span
  *     https://cor3ntin.github.io/posts/span/
- *
- *   https://www.reddit.com/r/cpp/comments/854mu9/2018_jacksonville_iso_c_committee_reddit_trip/
  */
