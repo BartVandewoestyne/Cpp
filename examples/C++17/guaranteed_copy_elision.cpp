19cc4c1c4c32e2ad74b00b1e3998153ac1e4a8c3
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-16 21:58:58 +0200

    Improve references for C++17 guaranteed copy elision.

--- examples/C++17/guaranteed_copy_elision.cpp
+++ examples/C++17/guaranteed_copy_elision.cpp
@@ -1,11 +1,20 @@
 /*
  * References:
  *
+ *   [andrzej20180516] Rvalues redefined
+ *     https://akrzemi1.wordpress.com/2018/05/16/rvalues-redefined/
+ *
  *   [doumler2017] Timur Doumler - The new C++17 and why it's good for you.
  *     https://youtu.be/ky_eXBMcJK0?t=23m30s
  *
- *   [1] http://insights.dice.com/2016/12/15/breaking-down-current-state-c-17/
- *   [2] http://www.bfilipek.com/2017/01/cpp17features.html#guaranteed-copy-elision
- *   [3] http://www.fluentcpp.com/2016/11/28/return-value-optimizations/
- *   [4] http://definedbehavior.blogspot.be/2011/08/value-semantics-copy-elision.html
+ *   [filipek20170109] C++17 Features - Guaranteed copy elision
+ *     http://www.bfilipek.com/2017/01/cpp17features.html#guaranteed-copy-elision
+ *   [bolton20161215] Breaking Down the Current State of C++ 17
+ *     http://insights.dice.com/2016/12/15/breaking-down-current-state-c-17/
+ *
+ *   [boccara20161128] Return value optimizations
+ *     http://www.fluentcpp.com/2016/11/28/return-value-optimizations/
+ *
+ *   [rodriguez20110810] Value semantics: Copy elision
+ *     http://definedbehavior.blogspot.be/2011/08/value-semantics-copy-elision.html
  */
