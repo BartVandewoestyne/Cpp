7266993c1cbaafe318ea67d671c0c4f2bdce17b2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-17 09:03:30 +0200

    Add reference about namespace composition.

--- examples/C++98/namespace_composition.cpp
+++ examples/C++98/namespace_composition.cpp
@@ -3,4 +3,7 @@
  *
  *   [chen20180516] Stupid C++ namespace tricks
  *     https://blogs.msdn.microsoft.com/oldnewthing/20180516-00/?p=98765
+ *
+ *   [stroustrup2000] The C++ Programming Language, Special Edition,
+ *     Chapter 8.2.8 Namespace Composition.
  */
