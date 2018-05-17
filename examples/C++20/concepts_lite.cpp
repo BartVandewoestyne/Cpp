455e8c8f793732c2e176351f4e63381db5db77cf
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-17 08:58:53 +0200

    Add reference for Concepts.

--- examples/C++20/concepts_lite.cpp
+++ examples/C++20/concepts_lite.cpp
@@ -3,6 +3,9 @@
  *
  * References:
  *
+ *   [dibella20180515] Prepping Yourself to Conceptify Algorithms
+ *     http://cjdb.com.au/blog/2018/05/15/prepping-yourself-to-conceptify-algorithms
+ *
  *   [1] http://www.grimm-jaud.de/index.php/blog/concepts-lite
  *   [2] http://www.modernescpp.com/index.php/concepts-lite
  */
