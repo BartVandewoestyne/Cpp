b6cc528c2a639e3b8cebcb6eef7b288dece4cc27
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-18 14:23:02 +0200

    Add reference on using explicit for multiargument constructors.

---
+++ examples/C++98/explicit_for_multiarg_constructors.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [peterg20160824] Explicit constructor taking multiple arguments
+ *     https://stackoverflow.com/questions/39122101/explicit-constructor-taking-multiple-arguments
+ */
