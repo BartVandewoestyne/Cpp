be76c27ab68d3f542ed35b96765792f60cece65b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-22 08:53:33 +0200

    Add reference for SFINAE.

---
+++ examples/C++11/algorithm/equal.cpp
@@ -0,0 +1,8 @@
+/*
+ * References:
+ *
+ *   [varun20180518] C++ : How to compare two vectors | std::equal() & Comparators
+ *     http://thispointer.com/c-how-to-compare-two-vectors-stdequal-comparators/
+ *
+ * TODO: check if this is C++11.
+ */
