e2474f66f2890579ee3c3a4f341ed6c6fedd344b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-05-30 23:48:52 +0200

    Add reference on fixed width integer parameters.

---
+++ examples/C++11/cstdint/fixed_width_integer_types_and_overloading.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [ignatchenko20180515] C++: On Using int*_t as Overload and Template Parameters
+ *     http://ithare.com/c-on-using-int_t-as-overload-and-template-parameters/
+ */
