5e8781d843d0fce7edbe2226d10421035ebee4d5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-06-04 23:33:56 +0200

    Add reference on const.

--- examples/C++98/const_correctness.cpp
+++ examples/C++98/const_correctness.cpp
@@ -1,4 +1,8 @@
 /*
  * References:
- *   [1] http://www.gotw.ca/gotw/006.htm
+ *   [langkabel20180604] Const - Friend or Foe in C++?
+ *     https://www.sourcetrail.com/blog/const_friend_or_foe_in_cpp/
+ *     
+ *   [sutter] Const-Correctness
+ *     http://www.gotw.ca/gotw/006.htm
  */
