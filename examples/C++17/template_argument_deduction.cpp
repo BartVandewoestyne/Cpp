b7121719da075242ced4bd6db131a1db9aa264d6
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-06-19 10:04:00 +0200

    Add examples for 3 simple C++17 features.

---
+++ examples/C++17/template_argument_deduction.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [boccara20180619] 3 Simple C++17 Features That Will Make Your Code Simpler
+ *     https://www.fluentcpp.com/2018/06/19/3-simple-c17-features-that-will-make-your-code-simpler/
+ */
