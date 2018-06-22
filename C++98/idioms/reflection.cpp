8a7c71dee3383743e8d10d94dc92c47b5c58c25d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-06-22 16:40:00 +0200

    Improved reference?

--- C++98/idioms/reflection.cpp
+++ C++98/idioms/reflection.cpp
@@ -1,4 +1,5 @@
 /*
  * References:
- *   [1] http://jackieokay.com/2017/04/13/reflection1.html
+ *   [kay20170413] An Introduction to Reflection in C++ - Jackie Kay
+ *     http://jackieokay.com/2017/04/13/reflection1.html
  */
