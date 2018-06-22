8c5d7cf3fc513e1918b1fb939907cdf94509a403
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-06-22 16:40:48 +0200

    Improved reference?

--- C++11/idioms/lock_free_programming.cpp
+++ C++11/idioms/lock_free_programming.cpp
@@ -1,5 +1,10 @@
 /*
  * References:
- *   [1] http://preshing.com/20120612/an-introduction-to-lock-free-programming/
- *   [2] http://preshing.com/20170612/can-reordering-of-release-acquire-operations-introduce-deadlock/
+ *
+ *   [preshing20170612] Can Reordering of Release/Acquire Operations Introduce Deadlock?
+ *     http://preshing.com/20170612/can-reordering-of-release-acquire-operations-introduce-deadlock/
+ *
+ *   [preshing20120612] An Introduction to Lock-Free Programming
+ *     http://preshing.com/20120612/an-introduction-to-lock-free-programming/
+ *
  */
