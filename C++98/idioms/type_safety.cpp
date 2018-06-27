83d8b547f5598ca21670b595a42e5b3d74a8c06c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-06-27 20:41:48 +0200

    Add examples on type safety.

---
+++ C++98/idioms/type_safety.cpp
@@ -0,0 +1,4 @@
+/*
+ * https://github.com/isocpp/CppCoreGuidelines/blob/master/CppCoreGuidelines.md#Rp-direct
+ * https://github.com/isocpp/CppCoreGuidelines/blob/master/CppCoreGuidelines.md#Ri-typed
+ */
