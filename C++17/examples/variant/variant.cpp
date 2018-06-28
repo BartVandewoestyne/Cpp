bb63da2be290e06b4c7e8b6744214ea3dc9614b3
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-06-28 18:20:39 +0200

    Add more examples.

--- C++17/examples/variant/variant.cpp
+++ C++17/examples/variant/variant.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [arias20180626] std::variant Doesn't Let Me Sleep
+ *      https://pabloariasal.github.io/2018/06/26/std-variant/
+ *
  *   [filipek20180604] Everything You Need to Know About std::variant from C++17
  *     https://www.bfilipek.com/2018/06/variant.html
  *
