33ea992a1c635ac08b893a42d0672d66d44efad6
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-04 22:53:21 +0200

    Add references for policies.

---
+++ C++98/idioms/policies.cpp
@@ -0,0 +1,10 @@
+/*
+ * References:
+ *
+ *   [wikipedia] Policy-based design
+ *     https://en.wikipedia.org/wiki/Policy-based_design
+ *
+ *   [alexandrescu2001XXXX] Modern C++ Design
+ *
+ *   [alexandrescu????????] Generic<Programming> column in C/C++ Users Journal
+ */
