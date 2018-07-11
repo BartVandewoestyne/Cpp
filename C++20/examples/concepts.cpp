64ac91f0f0da6a04b34cb9bb1578262bf6c5f196
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-11 09:43:33 +0200

    Add references for concepts and add file for atomic_ref.

--- C++20/examples/concepts.cpp
+++ C++20/examples/concepts.cpp
@@ -3,7 +3,12 @@
  *
  *   [dibella20180515] Prepping Yourself to Conceptify Algorithms
  *     http://cjdb.com.au/blog/2018/05/15/prepping-yourself-to-conceptify-algorithms
- *   [1] http://www.stroustrup.com/good_concepts.pdf
- *   [2] http://www.grimm-jaud.de/index.php/blog/concepts-definieren
- *   [3] C++ Concepts Intro: Need Your Input (https://youtu.be/xsSYPD0v5Mg)
+ *   [grimm20170320] Concepts definieren
+ *     http://www.grimm-jaud.de/index.php/blog/concepts-definieren
+ *
+ *   [stroustrup20170131] Concepts: The Future of Generic Programming
+ *     http://www.stroustrup.com/good_concepts.pdf
+ *
+ *   [EverythingCpp] C++ Concepts Intro: Need Your Input
+ *     https://youtu.be/xsSYPD0v5Mg
  */
