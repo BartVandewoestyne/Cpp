31729a803afb98f8939280acff1609f1379b0aff
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-12 14:19:37 +0200

    Add reference for fixed width integer types.

--- C++11/examples/cstdint/fixed_width_integer_types.cpp
+++ C++11/examples/cstdint/fixed_width_integer_types.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [stackoverflow20160404] Fixed-width integer literals in C++?
+ *     https://stackoverflow.com/questions/36406333/fixed-width-integer-literals-in-c
+ *
  *   [learncpp] 2.4a - Fixed-width integers and the unsigned controversy
  *     http://www.learncpp.com/cpp-tutorial/24a-fixed-width-integers/
  */
