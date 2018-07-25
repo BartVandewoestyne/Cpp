dc274fb5c5ad722194e12477ad17428b89449319
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-25 17:55:53 +0200

    Add references for private inheritance vs composition.

---
+++ C++98/examples/private_inheritance_vs_composition.cpp
@@ -0,0 +1,23 @@
+/*
+ * It seems like the general advice is:
+ *
+ *   "Use composition when you can, but private inheritance when you have to."
+ *
+ * See [c++faq] for an example where it is preferred to use private inheritance.
+ *
+ * References:
+ *
+ *   [c++faq] Inheritance - private and protected inheritance
+ *     https://isocpp.org/wiki/faq/private-inheritance
+ *
+ *   [hong2018????] C++ TUTORIAL PRIVATE INHERITANCE - 2018
+ *     http://www.bogotobogo.com/cplusplus/private_inheritance.php
+ *
+ *   [curious20100706] C++: Private inheritance vs Composition
+ *     https://abbeyhill.wordpress.com/2010/07/06/cpp-private-inheritance-vs-composition/
+ *
+ *   [meyers2005????] Effective C++ Third Edition, Item 39 "Use private inheritance judiciously."
+ *     https://flylib.com/books/en/2.887.1.55/1/
+ *
+ *   [meyers1998????] Effective C++ Second Edition, Item 42 "Use private inheritance judiciously."
+ */
