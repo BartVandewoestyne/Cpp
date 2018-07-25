32fc3adc18247f594c60d7f59539ac9eeea33dd2
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-25 18:21:09 +0200

    Improve references for private inheritance vs composition.

--- C++98/examples/private_inheritance_vs_composition.cpp
+++ C++98/examples/private_inheritance_vs_composition.cpp
@@ -16,8 +16,11 @@
  *   [curious20100706] C++: Private inheritance vs Composition
  *     https://abbeyhill.wordpress.com/2010/07/06/cpp-private-inheritance-vs-composition/
  *
- *   [meyers2005????] Effective C++ Third Edition, Item 39 "Use private inheritance judiciously."
+ *   [meyers20050512] Effective C++ Third Edition, Item 39 "Use private inheritance judiciously."
+ *     http://www.informit.com/store/effective-c-plus-plus-55-specific-ways-to-improve-your-9780321334879
  *     https://flylib.com/books/en/2.887.1.55/1/
  *
  *   [meyers1998????] Effective C++ Second Edition, Item 42 "Use private inheritance judiciously."
+ *
+ *   [meyers1992????] Effective C++, Item 41 "Use private inheritance judiciously."
  */
