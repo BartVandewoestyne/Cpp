a88fd186fa0d9ac985c2de1a4984950fd2dfb222
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-07-27 10:29:19 +0200

    Add reference to article on char arrays vs strings.

--- C++98/char_arrays_vs_strings.cpp
+++ C++98/char_arrays_vs_strings.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
- *   [geekgirldecodes] Char array versus string
+ *   [johnston20170726] std::string vs C-strings
+ *     https://embeddedartistry.com/blog/2017/7/24/stdstring-vs-c-strings
+ *
+ *   [geekgirldecodes20170611] Char array versus string
  *     https://geekgirldecodes.com/2017/06/11/char-array-versus-string/
  */
