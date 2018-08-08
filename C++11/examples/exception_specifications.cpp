c46c4c973ca8548adafa5558bcfc4cadff8dafa8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-08-08 10:44:34 +0200

    Add references for exception specifications.

---
+++ C++11/examples/exception_specifications.cpp
@@ -0,0 +1,6 @@
+/*
+ * Exception specifications are deprecated in C++11.
+ *
+ *   [cppreference] dynamic exception specification
+ *     https://en.cppreference.com/w/cpp/language/except_spec
+ */
---
+++ C++98/examples/exception_specifications.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [ibm] Exception specifications (C++ only)
+ *     https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.cbclx01/cplr156.htm
+ */
