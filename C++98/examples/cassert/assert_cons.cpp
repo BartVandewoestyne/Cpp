8a3cb8f5b42b14cfe255abe2a15bf45f8d3c54c3
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-08-20 10:31:25 +0200

    Add reference for assert.

---
+++ C++98/examples/cassert/assert_cons.cpp
@@ -0,0 +1,12 @@
+/*
+ * Some reasons why not to use assert():
+ *
+ *   - Poor programmatic error reporting & handling
+ *   - Poor error logging
+ *   - Disabled in release builds
+ *
+ * References:
+ *
+ *   [david20150116] Assertions in C++, and Why Not to Use assert()
+ *     https://www.softwariness.com/articles/assertions-in-cpp/
+ */
