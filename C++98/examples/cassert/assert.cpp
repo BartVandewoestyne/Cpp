e87c5152fe1437167bf8e81f76af2ce446fb3e9b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-08-20 10:42:51 +0200

    Add reference for assert.

--- C++98/examples/cassert/assert.cpp
+++ C++98/examples/cassert/assert.cpp
@@ -5,4 +5,7 @@
  *
  *   [alex20170902] 7.12a — Assert and static_assert
  *     http://www.learncpp.com/cpp-tutorial/7-12a-assert-and-static_assert/
+ *
+ *   [cppcoreguidelines20150928] I.27: Use assert for self-checking and documentation
+ *     https://github.com/isocpp/CppCoreGuidelines/issues/183
  */
