257e45311e97de99a952e3f0f05596ed3cfb0c85
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-08-20 10:34:55 +0200

    Add reference for assert.

---
+++ C++98/examples/cassert/assert.cpp
@@ -0,0 +1,8 @@
+/*
+ * assert operates at runtime (static_assert at compile time).
+ *
+ * References:
+ *
+ *   [alex20170902] 7.12a — Assert and static_assert
+ *     http://www.learncpp.com/cpp-tutorial/7-12a-assert-and-static_assert/
+ */
