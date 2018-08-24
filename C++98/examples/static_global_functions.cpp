135e174e8b26737db732261218ee3b538320b911
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-08-24 16:16:27 +0200

    Add more comments.

--- C++98/examples/static_global_functions.cpp
+++ C++98/examples/static_global_functions.cpp
@@ -1,8 +1,25 @@
 /*
+ * static global functions are invisible to the linker, allowing other
+ * compilation units to define functions with the same signature. Using
+ * namespaces alleviates this problem to a large degree, but file-static
+ * functions predate namespaces, because they are a feature inherited from the
+ * C programming language.
+ *
+ * static functions have internal linkage, that is, they aren't visible outside
+ * of their translation unit.
+ *
+ * A static function simply means that the linker cannot export the function
+ * (i.e. make it visible to other translation units).  It makes the function
+ * "private" to the current translation unit. It is the same as wrapping the
+ * function in an anonymous namespace.
+ *
  * References:
  *
  *   [arun20130207] C++ static local function vs global function
  *     https://stackoverflow.com/questions/14742664/c-static-local-function-vs-global-function
+ *
+ *   [codingfreak20100630] Static Functions in C
+ *     http://codingfreak.blogspot.com/2010/06/static-functions-in-c.html
  */
 
 #include <iostream>
