26ea211383ac769b928b1efa296fc44da9a73190
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-09-11 09:45:05 +0200

    Add reference to blog article about std::visit.

--- C++17/examples/variant/visit.cpp
+++ C++17/examples/variant/visit.cpp
@@ -1,4 +1,8 @@
 /*
  * References:
- *   [1] http://en.cppreference.com/w/cpp/utility/variant/visit
+ *
+ *   [cppreference] http://en.cppreference.com/w/cpp/utility/variant/visit
+ *
+ *   [filipek20180910] How To Use std::visit With Multiple Variants
+ *     https://www.bfilipek.com/2018/09/visit-variants.html
  */
