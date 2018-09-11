0312a869a973fbdc493bbc80f243803c15f57a64
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-09-11 09:59:26 +0200

    Add reference for type erasure.

--- C++98/idioms/type_erasure.cpp
+++ C++98/idioms/type_erasure.cpp
@@ -1,7 +1,11 @@
 /*
  * References:
  *
- *   [1] https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Type_Erasure
+ *   [tambe2007XXXX] More C++ Idioms - Type Erasure
+ *     https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Type_Erasure
+ *
+ *   [grimm20180910] C++ Core Guidelines: Type Erasure
+ *     http://www.modernescpp.com/index.php/c-core-guidelines-type-erasure
  *
  *   [brand20161202] Type erasure with unified call syntax
  *     https://tartanllama.github.io/c++/2016/12/02/type-erasure-unified-call/
