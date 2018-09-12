1376a93db37cc1f654578761cbbb1ad82c06cce0
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-09-12 20:10:12 +0200

    Add reference for blogposts about std::terminate.

---
+++ C++11/examples/exception/terminate.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [???20111005] Using std::terminate
+ *     https://akrzemi1.wordpress.com/2011/10/05/using-stdterminate/
+ *
+ *   [???20110928] Who calls std::terminate?
+ *     https://akrzemi1.wordpress.com/2011/09/28/who-calls-stdterminate/
+ */
