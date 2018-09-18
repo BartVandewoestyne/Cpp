eb8618a40d86202ac7a937b82d7d01cc2be5cfd8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-09-18 09:10:56 +0200

    Add reference for C++17 class template argument deduction.

--- C++17/examples/class_template_argument_deduction.cpp
+++ C++17/examples/class_template_argument_deduction.cpp
@@ -7,12 +7,15 @@
  *   [doumler2017] The new C++17 and why it's good for you - Timur Doumler, ADC 2017
  *     https://youtu.be/ky_eXBMcJK0?t=7m53s
  *     
- *   [revzin2017] Uniform initialization isn’t
+ *   [yuan20170926] Class Template Argument Deduction: A New Abstraction
+ *     https://youtu.be/4X8gXzi8bx8
+ *
+ *   [revzin2017XXXX] Uniform initialization isn’t
  *     https://medium.com/@barryrevzin/uniform-initialization-isnt-82533d3b9c11
  *
- *   [deane2017] C++17 Class Templates: Deduced or Not?
+ *   [deane2017XXXX] C++17 Class Templates: Deduced or Not?
  *     http://www.elbeno.com/blog/?p=1450
  *
- *   [mertz2017] Modern C++ Features – Class Template Argument Deduction
+ *   [mertz2017XXXX] Modern C++ Features – Class Template Argument Deduction
  *     https://arne-mertz.de/2017/06/class-template-argument-deduction/
  */
