4c5d63ebf024cee1ca555ae8aa28c1d53968cf3a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-09-19 14:46:43 +0200

    Add reference for smart pointers.

--- C++11/examples/smart_pointers.cpp
+++ C++11/examples/smart_pointers.cpp
@@ -1,5 +1,9 @@
 /*
  * References:
+ *
+ *   [george20180919] Being smart about ownership
+ *     https://hackernoon.com/being-smart-about-ownership-5ba2569a3ed7
+ *
  *   [1] https://www.fluentcpp.com/2017/08/22/smart-developers-use-smart-pointers-smart-pointers-basics/
  *   [2] https://www.fluentcpp.com/2017/08/25/knowing-your-smart-pointers/
  *   [3] https://www.fluentcpp.com/2017/08/29/custom-deleters/
