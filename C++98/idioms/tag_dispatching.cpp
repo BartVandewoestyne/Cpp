cccdc67fa2975ed920ed2e4aee166d0c3b9e3050
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-09-21 10:43:45 +0200

    Add references for tag dispatching.

--- C++98/idioms/tag_dispatching.cpp
+++ C++98/idioms/tag_dispatching.cpp
@@ -1,1 +1,9 @@
-// TODO
+/*
+ * References:
+ *
+ *   [boost] Boost Generic Programming Techniques - Tag Dispatching
+ *     https://www.boost.org/community/generic_programming.html#tag_dispatching
+ *
+ *   [boccara20180427] How to Use Tag Dispatching In Your Code Effectively
+ *     https://www.fluentcpp.com/2018/04/27/tag-dispatching/
+ */
