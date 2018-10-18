f3deaf55fad04ebce98616144d0f63a816eb8065
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-10-18 21:15:25 +0200

    Add reference.

--- C++11/examples/mutex/unique_lock.cpp
+++ C++11/examples/mutex/unique_lock.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [brandy20170929] Curiously Recurring C++ Bugs at Facebook (CppCon 2017)
+ *     https://youtu.be/3MB2iiCkGxg?t=1744
+ *
  *   [geier2014] Mutex lock guards in C++11
  *     https://geidav.wordpress.com/2014/01/09/mutex-lock-guards-in-c11/
  */
