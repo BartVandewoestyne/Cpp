9cf01c601faabd0d7a1fba7ea1d21c16921adb1a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-10-23 08:58:47 +0200

    Add reference for C++20 std::jthread.

---
+++ C++20/examples/jthread.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [grimm20181022] Ein neuer Thread mit C++20: std::jthread
+ *     https://www.heise.de/developer/artikel/Ein-neuer-Thread-mit-C-20-std-jthread-4197595.html
+ */
