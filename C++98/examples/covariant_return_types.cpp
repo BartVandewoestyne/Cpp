3a10c37657b960dc2a370b204c339786c1a39688
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-11-13 16:02:38 +0100

    Add references for covariant return types and forward declarations.

--- C++98/examples/covariant_return_types.cpp
+++ C++98/examples/covariant_return_types.cpp
@@ -1,6 +1,12 @@
 /*
  * References:
  *
+ *   [stackoverflow20090811] Is there a way to forward declare covariance?
+ *     https://stackoverflow.com/questions/1259272/is-there-a-way-to-forward-declare-covariance
+ *
+ *   [codeguru20081117] Forward declarations and covariate return types
+ *     http://forums.codeguru.com/showthread.php?465430-RESOLVED-Forward-declarations-and-covariate-return-types
+ *
  *   [withers20060406] HOWTO: Covariant Return Types in C++
  *     http://www.lwithers.me.uk/articles/covariant.html
  */
