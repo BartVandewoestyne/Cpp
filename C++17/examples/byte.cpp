1dcab7d8165cc2745343df9f88fefc248303c338
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2018-11-29 07:34:28 +0100

    Move reference for C++17's std::byte.

--- C++17/examples/byte.cpp
+++
@@ -1,4 +0,0 @@
-/*
- * References:
- *   [1] http://www.grimm-jaud.de/index.php/blog/c-17-std-byte-und-std-filesystem
- */
--- C++17/examples/cstddef/byte.cpp
+++ C++17/examples/cstddef/byte.cpp
@@ -3,4 +3,7 @@
  *
  *   [gregoire20180603] C++17: std::byte
  *     http://www.nuonsoft.com/blog/2018/06/03/c17-stdbyte/
+ *
+ *   [grimm20170417] C++17: std::byte und std::filesystem
+ *     http://www.grimm-jaud.de/index.php/blog/c-17-std-byte-und-std-filesystem
  */
