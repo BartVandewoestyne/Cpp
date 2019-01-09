7c703c995b6bb5a911ec17022e1cae86eda034db
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-01-09 11:53:13 +0100

    Add references for the Copy and Swap idiom.

---
+++ C++98/idioms/copy_and_swap.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [ropert20190107] Copy and Swap, 20 years later
+ *     https://mropert.github.io/2019/01/07/copy_swap_20_years/
+ *
+ *   [sutter2009XXXX] Exception-Safe Class Design, Part 1: Copy Assignment
+ *     http://gotw.ca/gotw/059.htm
+ */
