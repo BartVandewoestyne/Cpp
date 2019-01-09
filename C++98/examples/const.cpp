4326659cdb7efada390d505c1f2e72edb01eb8bb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-01-09 23:05:29 +0100

    Add reference for const.

--- C++98/examples/const.cpp
+++ C++98/examples/const.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [odwyer20190103] const is a contract
+ *     https://quuxplusone.github.io/blog/2019/01/03/const-is-a-contract/
+ *
  *   [meyers20110212] Appearing and Disappearing consts in C++
  *     http://aristeia.com/Papers/appearing%20and%20disappearing%20consts.pdf
  */
