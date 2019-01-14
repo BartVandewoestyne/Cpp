d577a4a0b674e8856e579dd5dc61e7e7544085b5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-01-14 13:27:13 +0100

    Improve references.

--- C++11/idioms/meyers_singleton.cpp
+++ C++11/idioms/meyers_singleton.cpp
@@ -4,9 +4,16 @@
  * References:
  *
  *   [grimm2017] Concurrency with Modern C++ (singletonSingleThreaded.cpp)
- *   [generic20110331] http://www.devartplus.com/3-simple-ways-to-create-singleton-in-c/
- *   [jimenez20091107] https://arkaitzj.wordpress.com/tag/meyers-singleton/
- *   [ankur20091102] https://stackoverflow.com/questions/1661529/is-meyers-implementation-of-the-singleton-pattern-thread-safe
+ *     https://leanpub.com/concurrencywithmodernc
+ *
+ *   [generic20110331] 3 simple ways to create singleton pattern in C++
+ *     http://www.devartplus.com/3-simple-ways-to-create-singleton-in-c/
+ *
+ *   [jimenez20091107] Static locals and threadsafety in g++
+ *     https://arkaitzj.wordpress.com/tag/meyers-singleton/
+ *
+ *   [ankur20091102] Is Meyers' implementation of the Singleton pattern thread safe?
+ *     https://stackoverflow.com/questions/1661529/is-meyers-implementation-of-the-singleton-pattern-thread-safe
  */
 
 class Singleton
