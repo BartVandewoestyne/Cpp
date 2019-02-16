65dc3353a19f73d0146a89611ff56b7f6586f87b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-02-16 02:34:15 +0100

    Add reference for const correctness.

---
+++ C++11/const_correctness.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [chatterjee20170307] Check for const correctness with the C++ Core Guidelines Checker
+ *     https://blogs.msdn.microsoft.com/vcblog/2017/03/07/check-for-const-correctness-with-the-c-core-guidelines-checker/
+ */
