7ccd77d1b392d5bd180e69747635aa264537f0f9
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-02-20 09:30:14 +0100

    Add example.

--- C++98/examples/static_variables.cpp
+++ C++98/examples/static_variables.cpp
@@ -4,3 +4,18 @@
  *   [filipek20180226] What happens to your static variables at the start of the program?
  *     http://www.bfilipek.com/2018/02/staticvars.html
  */
+
+class Test
+{
+public:
+    Test() {}
+public:
+    int _a;
+};
+
+Test t;
+
+int main()
+{
+    return t._a
+}
