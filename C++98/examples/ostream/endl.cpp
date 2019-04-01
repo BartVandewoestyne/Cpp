038400c2298a60666be7be2a404a4e0bc6881974
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-04-02 00:27:52 +0200

    Add reference for std::endl.

---
+++ C++98/examples/ostream/endl.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [sharpe201902XX] Don't Use std::endl
+ *     https://youtu.be/GMqQOEZYVJQ
+ *
+ *   [turner20160418] Stop Using std::endl
+ *     https://youtu.be/GMqQOEZYVJQ
+ */
