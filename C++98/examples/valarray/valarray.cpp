6b399772b81130c8d65b8c467a4c02c49716923b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-04-05 09:28:34 +0200

    Add example on std::valarray.

--- C++98/examples/valarray/valarray.cpp
+++ C++98/examples/valarray/valarray.cpp
@@ -10,6 +10,9 @@
  *   [gonen20170206] Introduction To valarray
  *     http://cppisland.com/?p=518
  *
+ *   [nyhoff20120803] Programming in C++ for Engineering and Science 1st Edition
+ *     https://cs.calvin.edu/activities/books/c++/engr-sci/WebItems/Chap12/Valarrays.pdf
+ *
  *   [andand20100405] What's the future of std::valarray look like?
  *     https://stackoverflow.com/questions/2576688/whats-the-future-of-stdvalarray-look-like
  *
@@ -19,3 +22,26 @@
  *   [sutter199801XX] C++ State of the Union
  *     http://www.gotw.ca/publications/mill01.htm
  */
+
+#include <iostream>
+#include <valarray>
+
+int main()
+{
+    std::valarray<int> val(10);
+
+    // Assign -1 to all elements.
+    val = -1;
+
+    // Get minimum and maximum.
+    std::cout << val.min() << std::endl;
+    std::cout << val.max() << std::endl;
+
+    // Get sum of all elements.
+    std::cout << val.sum() << std::endl;
+
+    // Perform mathematical operation on all elements.
+    std::valarray<int> abs_val = std::abs(val);
+
+    // ... and lots of other goodies.
+}
