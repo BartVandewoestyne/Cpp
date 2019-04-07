e8d66714243a2c491afb27388cde4b1f7cbeaf34
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-04-07 23:20:21 +0200

    Add reference for initialization.

--- C++17/examples/initialization.cpp
+++ C++17/examples/initialization.cpp
@@ -3,4 +3,7 @@
  *
  *   [lui20190103] Initialization in C++ is Seriously Bonkers
  *     https://mikelui.io/2019/01/03/seriously-bonkers.html
+ *
+ *   [doumler2018XXXX] Initialization in modern C++ - Timur Doumler - Meeting C++ 2018
+ *     https://youtu.be/ZfP4VAK21zc
  */
