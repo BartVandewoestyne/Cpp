133457c7a4ba94c478af6bfafe70206af5f2d720
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-04-08 09:33:02 +0200

    Improve references for fold expressions.

--- C++17/examples/fold_expressions.cpp
+++ C++17/examples/fold_expressions.cpp
@@ -1,5 +1,12 @@
 /*
  * References:
- *   [1] http://www.modernescpp.com/index.php/fold-expressions
- *   [2] https://ryanmichaelcollins.github.io/blog/2017/05/14/fun-with-constexpr-and-variadic-templates
+ *
+ *   [grimm20190408] C++ Insights: Variadic Templates
+ *     https://www.heise.de/developer/artikel/C-Insights-Variadic-Templates-4365343.html
+ *
+ *   [20170514] Fun with C++ constexpr, variadic templates, and fold expressions
+ *     https://ryanmichaelcollins.github.io/blog/2017/05/14/fun-with-constexpr-and-variadic-templates
+ *
+ *   [grimm20170204] Fold Expressions
+ *     http://www.modernescpp.com/index.php/fold-expressions
  */
