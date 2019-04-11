022cafb66a2fafd2334ffbd7a774c1c0f43ba9fe
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-04-11 15:17:32 +0200

    Add reference about Microsoft extensions.

--- C++11/examples/rvalues_bind_to_non-const_lvalue_ref.cpp
+++ C++11/examples/rvalues_bind_to_non-const_lvalue_ref.cpp
@@ -1,8 +1,14 @@
 /*
  * References:
  *
+ *   [sushiandwow2016XXXX] Why does C++ not adopt MSVC extension allowing temp objects to be assigned to non-const references (lvalues)?
+ *     https://www.reddit.com/r/cpp/comments/3d3p85/why_does_c_not_adopt_msvc_extension_allowing_temp/
+ *
  *   [trismegistos20140729] Why reference can not capture temporary while const ref and rval ref can [duplicate]
  *     https://stackoverflow.com/questions/25025538/why-reference-can-not-capture-temporary-while-const-ref-and-rval-ref-can
+ *
+ *   [sawicki20120131] Visual C++ is so Liberal
+ *     http://www.asawicki.info/news_1482_visual_c_is_so_liberal.html
  *
  *   [malistov20091014] How come a non-const reference cannot bind to a temporary object?
  *     https://stackoverflow.com/questions/1565600/how-come-a-non-const-reference-cannot-bind-to-a-temporary-object
@@ -16,10 +22,12 @@
 
 #include <iostream>
 
-// rvalue cannot bind to this non-const lvalue reference?
-void f(Foo& f) { std::cout << "f(Foo&)" << std::endl; }
+void f(Foo& f)
+{
+    std::cout << "f(Foo&)" << std::endl;
+}
 
 int main()
 {
-    f(Foo());
+    f(Foo());  // error: cannot bind non-const lvalue reference of type 'Foo&' to an rvalue of type 'Foo'
 }
