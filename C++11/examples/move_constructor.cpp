3bc8e4c865ad019e29704467c0186383044522af
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-04-12 01:12:23 +0200

    Add references for move constructor.

---
+++ C++11/examples/move_constructor.cpp
@@ -0,0 +1,15 @@
+/*
+ * References:
+ *
+ *   [microsoft] Move Constructors and Move Assignment Operators
+ *     https://docs.microsoft.com/en-us/cpp/cpp/move-constructors-and-move-assignment-operators-cpp
+ *
+ *   [alex20170226] https://www.learncpp.com/cpp-tutorial/15-3-move-constructors-and-move-assignment/
+ *     https://www.learncpp.com/cpp-tutorial/15-3-move-constructors-and-move-assignment/
+ *
+ *   [kalev20120806] C++11 Tutorial: Introducing the Move Constructor and the Move Assignment Operator
+ *     https://smartbear.com/blog/develop/c11-tutorial-introducing-the-move-constructor-and/
+ *
+ *   [andrzej20110811] Move Constructor
+ *     https://akrzemi1.wordpress.com/2011/08/11/move-constructor/
+ */
