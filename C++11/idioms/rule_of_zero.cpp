4bac7e39135b1679d8a006e7a41580abfecc8dab
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-04-23 21:35:20 +0200

    Add references for the Rule of Zero.

---
+++ C++11/idioms/rule_of_zero.cpp
@@ -0,0 +1,15 @@
+/*
+ * References:
+ *
+ *   [cppcoreguidelines] C.20: If you can avoid defining default operations, do
+ *     http://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rc-zero
+ *
+ *   [boccara20190423] The Rule of Zero in C++
+ *     https://www.fluentcpp.com/2019/04/23/the-rule-of-zero-zero-constructor-zero-calorie/
+ *
+ *   [meyers20140313] A Concern about the Rule of Zero
+ *     http://scottmeyers.blogspot.com/2014/03/a-concern-about-rule-of-zero.html
+ *
+ *   [fernandes2012XXXX] Rule of Zero
+ *     https://web.archive.org/web/20170705080311/https://rmf.io/cxx11/rule-of-zero
+ */
