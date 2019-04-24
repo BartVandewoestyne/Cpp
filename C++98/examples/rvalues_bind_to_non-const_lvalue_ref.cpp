da9c105084f1f00b8c1e2bcc89ad6f03e69fd3e9
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-04-24 22:03:35 +0200

    Move example as it is also C++98.

--- C++98/examples/rvalues_bind_to_non-const_lvalue_ref.cpp
+++ C++98/examples/rvalues_bind_to_non-const_lvalue_ref.cpp
@@ -1,8 +1,14 @@
 /*
  * References:
  *
+ *   [ibm] IBM XL C/C++ for Blue Gene/Q V12.1 documentation - The IBM XL C++ language extensions - Binding an rvalue to a non-const or volatile lvalue reference
+ *     https://www.ibm.com/support/knowledgecenter/en/SS2LWA_12.1.0/com.ibm.xlcpp121.bg.doc/language_ref/bindingrvalue.html
+ *
  *   [sushiandwow2016XXXX] Why does C++ not adopt MSVC extension allowing temp objects to be assigned to non-const references (lvalues)?
  *     https://www.reddit.com/r/cpp/comments/3d3p85/why_does_c_not_adopt_msvc_extension_allowing_temp/
+ *
+ *   [stackoverflow20150418] Binding r-value to l-value reference is non-standard Microsoft C++ extension
+ *     https://stackoverflow.com/questions/29717800/binding-r-value-to-l-value-reference-is-non-standard-microsoft-c-extension
  *
  *   [trismegistos20140729] Why reference can not capture temporary while const ref and rval ref can [duplicate]
  *     https://stackoverflow.com/questions/25025538/why-reference-can-not-capture-temporary-while-const-ref-and-rval-ref-can
@@ -14,8 +20,6 @@
  *     https://stackoverflow.com/questions/1565600/how-come-a-non-const-reference-cannot-bind-to-a-temporary-object
  *
  *   [stroustrup1994] The Design and Evolution of C++
- *
- * TODO: Visual C++ behaves differently in this respect?  Find doc or blogpost about this.
  */
 
 #include "Foo.h"
