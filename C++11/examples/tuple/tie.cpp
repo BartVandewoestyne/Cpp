48c27fec3627e8efac76b6a68bdedfc1b1aa2f69
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-05-06 10:34:27 +0200

    Improve C++11 std::tie example.

--- C++11/examples/tuple/tie.cpp
+++ C++11/examples/tuple/tie.cpp
@@ -10,3 +10,17 @@
  *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
  *     https://youtu.be/XkDEzfpdcSg?t=34m15s
  */
+
+#include <tuple>
+
+std::tuple<int, int> f()
+{
+    return std::make_tuple(1, 42);
+}
+
+int main()
+{
+    int a;
+    int b;
+    std::tie(a, b) = f();
+}
