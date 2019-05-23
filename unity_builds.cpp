b307dc96a445d4db621e8ee74442a37a50164f79
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-05-23 09:13:36 +0200

    Add reference to QMake-unity.

--- unity_builds.cpp
+++ unity_builds.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [nmoreaud] QMake-unity
+ *     https://github.com/nmoreaud/qmake-unity
+ *
  *   [kirilov20180707] A guide to unity builds
  *     http://onqtam.com/programming/2018-07-07-unity-builds/
  */
