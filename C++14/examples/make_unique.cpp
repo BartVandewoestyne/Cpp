a70412bb4a307fac58480f05959739dc9f091ab4
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-06-11 08:43:41 +0200

    Fix reference for std::make_unique.

--- C++14/examples/make_unique.cpp
+++ C++14/examples/make_unique.cpp
@@ -1,7 +1,7 @@
 /*
  * References:
  * 
- *   [1] http://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique
+ *   [cppreference] http://en.cppreference.com/w/cpp/memory/unique_ptr/make_unique
  *
  *   [sahar2016] Exploring std::unique_ptr.
  *     http://shaharmike.com/cpp/unique-ptr/ (haven't read this one yet)
@@ -9,8 +9,8 @@
  *   [niting2016] Advantages of using make_unique over new operator [duplicate].
  *     https://stackoverflow.com/questions/37514509/advantages-of-using-make-unique-over-new-operator
  *
- *   [NFRCR2014] Differences between std::make_unique and std::unique_ptr.
- *     https://stackoverflow.com/questions/22571202/differences-between-stdmake-unique-and-stdunique-ptr
+ *   [stackoverflow20140321] Differences between std::make_unique and std::unique_ptr.
+ *     https://stackoverflow.com/questions/22571202/differences-between-stdmake-unique-and-stdunique-ptr-with-new
  */
 
 #include <iostream>
