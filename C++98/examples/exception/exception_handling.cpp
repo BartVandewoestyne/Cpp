87f188b3de48e2680ed6be9395179ff8d2a44cd5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-06-18 08:43:34 +0200

    Add reference to Tom Cargill paper on exception handling.

---
+++ C++98/examples/exception/exception_handling.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [cargill1994XXXX] Exception Handling: A False Sense of Security
+ *     https://pdfs.semanticscholar.org/467a/e223730836328c9fe1a27ed12bc0034efb7c.pdf
+ */
