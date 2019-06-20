86172b9d08135085a16deda03ebe823dfaf74495
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-06-20 08:52:14 +0200

    Add reference for SFINAE.

--- C++11/idioms/sfinae.cpp
+++ C++11/idioms/sfinae.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [chen20190619] How do I write a function that accepts any type of standard container?
+ *     https://devblogs.microsoft.com/oldnewthing/20190619-00/?p=102599
+ *
  *   [bendersky20141020] SFINAE and enable_if
  *     https://eli.thegreenplace.net/2014/sfinae-and-enable_if/
  */
