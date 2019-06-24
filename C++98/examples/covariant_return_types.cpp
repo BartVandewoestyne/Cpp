743da3cee3a2c9b0be3d76eaa1aad51ccbbbc828
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-06-24 10:30:44 +0200

    Add example on covariant return types.

--- C++98/examples/covariant_return_types.cpp
+++ C++98/examples/covariant_return_types.cpp
@@ -6,9 +6,15 @@
  *   [stackoverflow20090811] Is there a way to forward declare covariance?
  *     https://stackoverflow.com/questions/1259272/is-there-a-way-to-forward-declare-covariance
  *
+ *   [stackoverflow201108XX] When is C++ covariance the best solution?
+ *     https://stackoverflow.com/questions/1260757/when-is-c-covariance-the-best-solution
+ *
  *   [codeguru20081117] Forward declarations and covariate return types
  *     http://forums.codeguru.com/showthread.php?465430-RESOLVED-Forward-declarations-and-covariate-return-types
  *
  *   [withers20060406] HOWTO: Covariant Return Types in C++
  *     http://www.lwithers.me.uk/articles/covariant.html
+ *
+ *   [dewhurst200502XX] C++ Common Knowledge: Essential Intermediate Programming
+ *     https://www.oreilly.com/library/view/c-common-knowledge/0321321928/ch31.html
  */
