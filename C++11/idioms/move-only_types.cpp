ca8e014192b66b936b400c5c37aa293999e3413c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-06-27 11:33:18 +0200

    Add reference for move-only types.

--- C++11/idioms/move-only_types.cpp
+++ C++11/idioms/move-only_types.cpp
@@ -1,8 +1,14 @@
 /*
  * References:
  *
+ *   [microsoft20190705] Value Types (Modern C++)
+ *     https://docs.microsoft.com/en-us/cpp/cpp/value-types-modern-cpp
+ *
  *   [stackoverflow20151024] Is there a point to define move-only objects in c++11?
  *     https://stackoverflow.com/questions/33316942/is-there-a-point-to-define-move-only-objects-in-c11
+ *
+ *   [Gabor Fekete] Using Auto for Declaring Variables of Move-only Types
+ *     https://www.codeproject.com/Articles/823658/Using-Auto-for-Declaring-Variables-of-Move-only-Ty
  *
  *   [meyers20140724] Should move-only types ever be passed by value?
  *     http://scottmeyers.blogspot.com/2014/07/should-move-only-types-ever-be-passed.html
