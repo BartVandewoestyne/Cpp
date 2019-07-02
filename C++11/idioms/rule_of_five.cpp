0c58e71d5f2111dff2a9238e907197a69fd3adbb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-02 17:03:19 +0200

    Add references for the Rule of Five.

---
+++ C++11/idioms/rule_of_five.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [wikipedia] Rule of three (C++ programming)
+ *     https://en.wikipedia.org/wiki/Rule_of_three_(C%2B%2B_programming)
+ *
+ *   [stackoverflow20110512] How to actually implement the rule of five?
+ *     https://stackoverflow.com/questions/5976459/how-to-actually-implement-the-rule-of-five
+ */
