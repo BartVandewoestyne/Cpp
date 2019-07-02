0c65e3eaf20c14018e46684d20f19473795f0a1a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-02 15:47:39 +0200

    Add reference for move assignment operator.

---
+++ C++11/examples/move_assignment_operator.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [stackoverflow20121115] std::move inside move assignment operator
+ *     https://stackoverflow.com/questions/13405699/stdmove-inside-move-assignment-operator
+ *
+ *   [stackoverflow20120226] Why does moving a pointer variable not set it to null?
+ *     https://stackoverflow.com/questions/9452503/why-does-moving-a-pointer-variable-not-set-it-to-null
+ */
