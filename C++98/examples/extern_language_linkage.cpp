f2664470c69ed3e3bd3af0f458375838316c8386
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-03 11:04:53 +0200

    Add references for extern (language linkage).

---
+++ C++98/examples/extern_language_linkage.cpp
@@ -0,0 +1,11 @@
+/*
+ * References:
+ *
+ *   [cppreference] Language linkage
+ *     https://en.cppreference.com/w/cpp/language/language_linkage
+ *
+ *   [stackoverflow20100509] When to use extern "C" in simple words?
+ *     https://stackoverflow.com/questions/2796796/when-to-use-extern-c-in-simple-words
+ *
+ *   [stackoverflow20090625] What is the effect of extern "C" in C++?
+ *     https://stackoverflow.com/questions/1041866/what-is-the-effect-of-extern-c-in-c
