35a8f54aaba3449b1510533b732f69881f12d626
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-03 11:16:15 +0200

    Add reference for language linkage.

--- C++98/examples/extern_language_linkage.cpp
+++ C++98/examples/extern_language_linkage.cpp
@@ -4,6 +4,12 @@
  *   [cppreference] Language linkage
  *     https://en.cppreference.com/w/cpp/language/language_linkage
  *
+ *   [ibm] Language linkage (C++ only)
+ *     https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.3.0/com.ibm.zos.v2r3.cbclx01/cplr020.htm
+ *
+ *   [mertz20181010] Calling C Code from C++ With 'extern "C"'
+ *     https://arne-mertz.de/2018/10/calling-cpp-code-from-c-with-extern-c/
+ *
  *   [stackoverflow20100509] When to use extern "C" in simple words?
  *     https://stackoverflow.com/questions/2796796/when-to-use-extern-c-in-simple-words
  *
