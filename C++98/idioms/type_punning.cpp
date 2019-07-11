fef5dd3abb8324b4b1c3fe640497553b116d636d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-11 12:00:10 +0200

    Add references for type punning.

---
+++ C++98/idioms/type_punning.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [neumann] Undefined Behavior in C++ - Type Punning
+ *     https://adriann.github.io/undefined_behavior.html
+ *
+ *   [regehr20130611] Type Punning, Strict Aliasing, and Optimization
+ *     https://blog.regehr.org/archives/959
+ */
