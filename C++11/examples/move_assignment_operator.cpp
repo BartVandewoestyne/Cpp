271224c01c2c7f4484d9a12a911f7b2ba0a273a8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-23 09:16:38 +0200

    Add reference for move assignment operator.

--- C++11/examples/move_assignment_operator.cpp
+++ C++11/examples/move_assignment_operator.cpp
@@ -6,4 +6,7 @@
  *
  *   [stackoverflow20120226] Why does moving a pointer variable not set it to null?
  *     https://stackoverflow.com/questions/9452503/why-does-moving-a-pointer-variable-not-set-it-to-null
+ *
+ *   [stackoverflow] How do I write the move assignment function for this derived class?
+ *     https://stackoverflow.com/questions/19109392/how-do-i-write-the-move-assignment-function-for-this-derived-class
  */
