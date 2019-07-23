f4d467f4ea82f272b94a6ff542ffb145ab605719
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-23 15:16:24 +0200

    Add file with references about exception propagation with std::async.

---
+++ C++11/examples/future/async_exception_propagation.cpp
@@ -0,0 +1,9 @@
+/*
+ * References:
+ *
+ *   [vorbrodt20190324] Propagate exceptions across threads
+ *     https://vorbrodt.blog/2019/03/24/propagate-exceptions-across-threads/
+ *
+ *   [stackoverflow20130108] Exception propagation and std::future
+ *     https://stackoverflow.com/questions/14222899/exception-propagation-and-stdfuture
+ */
