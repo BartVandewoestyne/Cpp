4c894457ad983dd9bc34f4ac353cc89c817e3f04
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-24 07:45:32 +0200

    Add reference for inline.

--- C++98/examples/global_constants.cpp
+++ C++98/examples/global_constants.cpp
@@ -3,7 +3,4 @@
  *
  *   [boccara20190723] What Every C++ Developer Should Know to (Correctly) Define Global Constants
  *     https://www.fluentcpp.com/2019/07/23/how-to-define-a-global-constant-in-cpp/
- *
- *   [gregoire20170828] C++17: Inline Variables
- *     http://www.nuonsoft.com/blog/2017/08/28/c17-inline-variables/
  */
