9bb507f5a2438130f48003fd39ae013269cf0c39
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-07-24 07:46:17 +0200

    Fix reference.

--- C++98/examples/extern_for_global_constants/header.h
+++ C++98/examples/extern_for_global_constants/header.h
@@ -1,7 +1,7 @@
 /*
  * References:
  *
- *   [boccara20190723] https://www.fluentcpp.com/2019/07/23/how-to-define-a-global-constant-in-cpp/
+ *   [boccara20190723] What Every C++ Developer Should Know to (Correctly) Define Global Constants
  *     https://www.fluentcpp.com/2019/07/23/how-to-define-a-global-constant-in-cpp/
  */
 
