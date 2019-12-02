dbb1e122eefe38fcf80323c0a3cb7159085fe331
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-12-02 11:07:38 +0100

    Add reference for C++20 Concepts.

--- C++20/examples/concepts.cpp
+++ C++20/examples/concepts.cpp
@@ -1,6 +1,8 @@
 /*
  * References:
  *
+ *   [grimm20191125] C++20: Two Extremes and the Rescue with Concepts
+ *     http://www.modernescpp.com/index.php/concepts-two-extrems-and-the-solution
  *   [grimm20190615] Concepts in C++20: A Revolution (itCppCon19)
  *     https://youtu.be/3XYrw1N-Ev4
  *
