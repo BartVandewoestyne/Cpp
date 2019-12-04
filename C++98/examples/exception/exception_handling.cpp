8a49922949711dfa8d0a38b2d6089f4f8c352654
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-12-04 21:29:45 +0100

    Add reference for exceptions.

--- C++98/examples/exception/exception_handling.cpp
+++ C++98/examples/exception/exception_handling.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [stroustrup20191118] C++ exceptions and alternatives
+ *     http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2019/p1947r0.pdf
+ *
  *   [cargill1994XXXX] Exception Handling: A False Sense of Security
  *     https://pdfs.semanticscholar.org/467a/e223730836328c9fe1a27ed12bc0034efb7c.pdf
  */
