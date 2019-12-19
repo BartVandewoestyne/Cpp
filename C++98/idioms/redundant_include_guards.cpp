e3535bd84180c1a18bef9403c2dbe7243e6d56dd
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2019-12-19 10:10:24 +0100

    Add references for redundant include guards.

---
+++ C++98/idioms/redundant_include_guards.cpp
@@ -0,0 +1,21 @@
+/*
+ * References:
+ *
+ *   [wiki.c2.com] Redundant Include Guards
+ *     http://wiki.c2.com/?RedundantIncludeGuards
+ *
+ *   [reddy20110314] API Design for C++, 7.2.3 Redundant #include Guards
+ *     http://www.apibook.com/
+ *
+ *   [stackoverflow20130227] is "Redundant #include Guards" necessary on Visual Studio?
+ *     https://stackoverflow.com/questions/15119273/is-redundant-include-guards-necessary-on-visual-studio
+ *
+ *   [stackoverflow20100209] Are redundant include guards necessary?
+ *     https://stackoverflow.com/questions/2233401/are-redundant-include-guards-necessary
+ *
+ *   [llopis20040310] Physical Structure and C++ - Part 2: Build Times
+ *     http://gamesfromwithin.com/physical-structure-and-c-part-2-build-times
+ *
+ *   [lakos19960720] Large Scale C++ Software Design
+ *     https://www.amazon.com/Large-Scale-Software-Design-John-Lakos/dp/0201633620
+ */
