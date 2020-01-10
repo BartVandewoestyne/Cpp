9c7481b7b9edc9cefc269c0ecad515dde5071d3a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-01-10 22:58:33 +0100

    Added reference for Singleton.

---
+++ C++98/idioms/singleton_has_issues.cpp
@@ -0,0 +1,10 @@
+/*
+ * Issues with Singleton:
+ *
+ *   -> The thing with DLLs.
+ *   -> Unit testing is harder.
+ *   -> Thread safety?
+ *
+ * [stackoverflow20110804] C++ a singleton class with dll
+ *   https://stackoverflow.com/questions/6935541/c-a-singleton-class-with-dll/
+ */
