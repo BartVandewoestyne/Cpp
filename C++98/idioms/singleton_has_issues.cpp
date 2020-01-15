5b84a2abf4e2ea1955a0933e996446b06780760f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-01-15 10:42:11 +0100

    Add reference for Singleton.

--- C++98/idioms/singleton_has_issues.cpp
+++ C++98/idioms/singleton_has_issues.cpp
@@ -1,10 +1,20 @@
 /*
  * Issues with Singleton:
  *
- *   -> The thing with DLLs.
- *   -> Unit testing is harder.
+ *   * If multiple DLLs are linked against a static library that defines the
+ *     singleton, they will each get their own copy of that singleton.
+ *     If your application is a single EXE with no custom DLLs, this may not
+ *     be a problem.
+ *
+ *   -> Unit testing is harder?
+ *
  *   -> Thread safety?
  *
- * [stackoverflow20110804] C++ a singleton class with dll
- *   https://stackoverflow.com/questions/6935541/c-a-singleton-class-with-dll/
+ * References:
+ *
+ *   [stackoverflow20080926] When to use dynamic vs. static libraries
+ *     https://stackoverflow.com/questions/140061/when-to-use-dynamic-vs-static-libraries
+ *
+ *   [stackoverflow20110804] C++ a singleton class with dll
+ *     https://stackoverflow.com/questions/6935541/c-a-singleton-class-with-dll/
  */
