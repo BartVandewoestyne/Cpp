c1250621e5acff41d21e24ae80251f70f4012b2e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-01-25 20:41:14 +0100

    Add example on defining global constants in headers.

---
+++ C++98/idioms/global_constants_in_headers/file1.cpp
@@ -0,0 +1,12 @@
+#include "constants.h"
+
+#include <iostream>
+
+void printFromFile1()
+{
+    std::cout << "In translation unit 1:" << std::endl;
+    std::cout << "  &ci = " << &ci << std::endl;
+    std::cout << "  &sci = " << &sci << std::endl;
+    std::cout << "  &eci = " << &eci << std::endl;
+    std::cout << "  &x = " << &x << std::endl;
+}
---
+++ C++98/idioms/global_constants_in_headers/file2.h
@@ -0,0 +1,6 @@
+#ifndef FILE2_H
+#define FILE2_H
+
+void printFromFile2();
+
+#endif
