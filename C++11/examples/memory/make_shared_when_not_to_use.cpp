134652dbc430e9359b5f1c626ab5a96caf5474b1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-02-04 22:41:25 +0100

    Add reference for when not to use std::make_shared.

--- C++11/examples/memory/make_shared_when_not_to_use.cpp
+++ C++11/examples/memory/make_shared_when_not_to_use.cpp
@@ -9,4 +9,7 @@
  *
  *   [mertz20180926] std::make_shared vs. the Normal std::shared_ptr Constructor
  *     https://arne-mertz.de/2018/09/make_shared-vs-the-normal-shared_ptr-constructor/
+ *
+ *   [stackoverflow20120112] Is there any reason not to use std::make_shared when constructing objects?
+ *     https://stackoverflow.com/questions/8843720/is-there-any-reason-not-to-use-stdmake-shared-when-constructing-objects
  */
