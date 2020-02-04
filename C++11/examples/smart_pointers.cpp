d3bc60cfb4ada1fd017096f001d3170228fd65cb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-02-04 21:55:31 +0100

    Improve references for C++11 smart pointers.

--- C++11/examples/smart_pointers.cpp
+++ C++11/examples/smart_pointers.cpp
@@ -1,10 +1,18 @@
 /*
  * References:
  *
+ *   [josuttis20190902-05] C++ Smart Pointers - Usage and Secrets
+ *     https://youtu.be/XH4xIyS9B2I
+ *
  *   [george20180919] Being smart about ownership
  *     https://hackernoon.com/being-smart-about-ownership-5ba2569a3ed7
  *
- *   [1] https://www.fluentcpp.com/2017/08/22/smart-developers-use-smart-pointers-smart-pointers-basics/
- *   [2] https://www.fluentcpp.com/2017/08/25/knowing-your-smart-pointers/
- *   [3] https://www.fluentcpp.com/2017/08/29/custom-deleters/
+ *   [boccara20170829] Smart developers use smart pointers (3/7) – Custom deleters
+ *     https://www.fluentcpp.com/2017/08/29/custom-deleters/
+ *
+ *   [boccara20170825] unique_ptr, shared_ptr, weak_ptr, scoped_ptr, raw pointers – Knowing your smart pointers (2/7)
+ *     https://www.fluentcpp.com/2017/08/25/knowing-your-smart-pointers/
+ *
+ *   [boccara20170822] Smart developers use smart pointers (1/7) – Smart pointers basics
+ *     https://www.fluentcpp.com/2017/08/22/smart-developers-use-smart-pointers-smart-pointers-basics/
  */
