aab772f2c6db782b7cf892a1004e15caf0376bf7
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-02-04 11:42:08 +0100

    Add references for shared_ptr in a multithreaded environment.

---
+++ C++11/examples/memory/shared_ptr_multithreaded.cpp
@@ -0,0 +1,21 @@
+/* 
+ * References:
+ *
+ *   [wang20180911] c++ why std::shared_ptr need atomic_store, atomic_load or why we need atomic shared_ptr
+ *     http://www.comrite.com/wp/c-why-need-atomic_store-atomic_load-on-shared_ptr-atomic-shared_ptr/
+ *
+ *   [park20180903] std::shared_ptr is not thread safe
+ *     https://marsettler.com/2018/09/03/shared-ptr-is-not-thread-safe.html
+ * 
+ *   [shahar20161113] Exploring std::shared_ptr - Performance & thread safety
+ *     https://shaharmike.com/cpp/shared-ptr/#performance-thread-safety
+ *
+ *   [stackoverflow20150923] Is shared_ptr destruction safe with multiple threads?
+ *     https://stackoverflow.com/questions/32750945/is-shared-ptr-destruction-safe-with-multiple-threads
+ *
+ *  [stackoverflow20140727] Double Delete using Shared Pointers, even though there is still a shared_ptr to the object, its destructor is being called
+ *     https://stackoverflow.com/questions/24977582/double-delete-using-shared-pointers-even-though-there-is-still-a-shared-ptr-to
+ *
+ *   [stackoverflow20130123] std::shared_ptr thread safety
+ *     https://stackoverflow.com/questions/14482830/stdshared-ptr-thread-safety
+ */
