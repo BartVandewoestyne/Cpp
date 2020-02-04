374e978c4418d6a6ad3c131127e9abfb08829f3c
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-02-04 22:35:44 +0100

    Add example on when not to use std::make_shared.

---
+++ C++11/examples/memory/make_shared_when_not_to_use.cpp
@@ -0,0 +1,9 @@
+/*
+ * Do not *always* use std::make_shared.  This example shows you a
+ * situation where using make_shared is not a good idea!
+ *
+ * References:
+ *
+ *   [josuttis20190902-05] C++ Smart Pointers - Usage and Secrets
+ *     https://youtu.be/XH4xIyS9B2I?t=1115
+ */
