e4198ef3de5b2e191cce9bb8b8f3902db3574ce5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-02-04 23:41:55 +0100

    Add remarks on source and sink functions.

--- C++11/examples/memory/unique_ptr_creation_and_passing.cpp
+++ C++11/examples/memory/unique_ptr_creation_and_passing.cpp
@@ -3,6 +3,9 @@
  *
  * References:
  *
+ *   [josuttis20190902-05] C++ Smart Pointers - Usage and Secrets
+ *     https://youtu.be/XH4xIyS9B2I?t=3153
+ *
  *   [filipek20170220] Modernize: Sink Functions
  *     http://www.bfilipek.com/2017/02/modernize-sink-functions.html
  *
@@ -30,10 +33,19 @@ struct Foo {
 };
 
 
-void sink(std::unique_ptr<Foo> p) {
+// Herb Sutter Style sink function (call by value).
+// (Later, acknowledged by Herb to be the worse of the two options,
+//  but Josuttis still thinks this is the preferred form).
+void sink1(std::unique_ptr<Foo> p) {
   std::cerr << "Sink owns Foo [" << p.get() << "]\n";
 }
 
+// Scott Meyers Style sink function (call by rvalue reference).
+// (later confirmed by Herb Sutter, but Josuttis still thinks
+//  this is the worst form).
+void sink2(std::unique_ptr<Foo>&& p) {
+  std::cerr << "Sink owns Foo [" << p.get() << "]\n";
+}
 
 std::unique_ptr<Foo> source() {
     std::cerr << "Creating Foo in source\n";
@@ -41,18 +53,32 @@ std::unique_ptr<Foo> source() {
 }
 
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
+    {
+        std::cerr << "Calling source()\n";
+        auto pmain = source();
 
-    std::cerr << "Calling source()\n";
-    auto pmain = source();
+        std::cerr << "Now pmain owns Foo [" << pmain.get() << "]\n";
 
-    std::cerr << "Now pmain owns Foo [" << pmain.get() << "]\n";
+        std::cerr << "Passing it to sink1()\n";
+        //sink1(pmain);                    // ERROR: can't copy std::unique_ptr!
+        sink1(std::move(pmain));           // OK: can move it!
+        pmain.release();                   // Josuttis advises to always release after passing unique pointer to sink function.
+    }
 
-    std::cerr << "Passing it to sink()\n";
-    //sink(pmain);                    // ERROR: can't copy std::unique_ptr!
-    sink(std::move(pmain));           // OK: can move it!
+    {
+        std::cerr << "Calling source()\n";
+        auto pmain = source();
+
+        std::cerr << "Now pmain owns Foo [" << pmain.get() << "]\n";
+
+        std::cerr << "Passing it to sink2()\n";
+        //sink2(pmain);                    // ERROR: can't copy std::unique_ptr!
+        sink2(std::move(pmain));           // OK: can move it!
+        pmain.release();                   // Josuttis advises to always release after passing unique pointer to sink function.
+    }
 
     std::cerr << "Main done\n";
     return 0;
-
 }
