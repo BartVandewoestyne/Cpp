c1e7fc1401d189a84ec7f0df70fe67d3237311dd
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-02-04 22:38:07 +0100

    Add reference for when not to use std::make_shared.

--- C++11/examples/memory/make_shared_when_not_to_use.cpp
+++ C++11/examples/memory/make_shared_when_not_to_use.cpp
@@ -6,4 +6,7 @@
  *
  *   [josuttis20190902-05] C++ Smart Pointers - Usage and Secrets
  *     https://youtu.be/XH4xIyS9B2I?t=1115
+ *
+ *   [mertz20180926] std::make_shared vs. the Normal std::shared_ptr Constructor
+ *     https://arne-mertz.de/2018/09/make_shared-vs-the-normal-shared_ptr-constructor/
  */
