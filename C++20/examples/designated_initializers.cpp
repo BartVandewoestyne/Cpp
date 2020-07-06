1ccf4e5437406afa9615700f057616c9a759ecb8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-07-06 09:02:33 +0200

    Add reference for C++20 Designated Initializers.

--- C++20/examples/designated_initializers.cpp
+++ C++20/examples/designated_initializers.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [grimm20200706] C++20: Designated Initializers
+ *     https://www.heise.de/developer/artikel/C-20-Designated-Initializers-4835833.html
+ *
  *   [widlund20181119] Exploring C++20 - Designated initialisers
  *     https://blog.therocode.net/2018/11/exploring-cpp-20-designated-initializers
  */
