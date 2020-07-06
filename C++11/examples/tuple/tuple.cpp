fee8d2114f78156cf9950ff2c6c9660215aa1345
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-07-06 09:06:25 +0200

    Add references for std::tuple.

--- C++11/examples/tuple/tuple.cpp
+++ C++11/examples/tuple/tuple.cpp
@@ -3,4 +3,13 @@
  *
  *   [chen20200629] Stupid std::tuple tricks: Finding a type in a tuple
  *     https://devblogs.microsoft.com/oldnewthing/20200629-00/?p=103910
+ *
+ *   [chen20200626] Mundane std::tuple tricks: Creating more interesting index sequences
+ *     https://devblogs.microsoft.com/oldnewthing/20200626-00/?p=103904
+ *
+ *   [chen20200625] Mundane std::tuple tricks: Creating interesting index sequences
+ *     https://devblogs.microsoft.com/oldnewthing/20200625-00/?p=103903
+ *
+ *   [chen20200624] Mundane std::tuple tricks: Selecting via an index sequence, part 2
+ *     https://devblogs.microsoft.com/oldnewthing/20200624-00/?p=103902
  */
