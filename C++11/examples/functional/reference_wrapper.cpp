c7fdb809f582b3cfd89ef4fb9967264acea17026
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-08-05 16:46:45 +0200

    Add reference for std::reference_wrapper.

---
+++ C++11/examples/functional/reference_wrapper.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [kumar20200625] std::ref and std::reference_wrapper: common use cases
+ *     https://www.nextptr.com/tutorial/ta1441164581/stdref-and-stdreference_wrapper-common-use-cases
+ */
