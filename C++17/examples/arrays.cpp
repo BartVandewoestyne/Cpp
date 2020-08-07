29c9e01814673d7f0cf62bb47663efaabde1bd00
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-08-07 09:21:43 +0200

    Improve comment.

--- C++17/examples/arrays.cpp
+++ C++17/examples/arrays.cpp
@@ -9,6 +9,8 @@
 
 int main()
 {
-    // C++17's CTAD permits us to write
+    // For ints, C++17's CTAD permits us to write:
+    // (note that this doesn't work if you want for example an array of short
+    // or uint32_t).
     std::array myArray = {2, 7, 1, 8, 2};
 }
