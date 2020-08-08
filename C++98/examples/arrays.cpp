d05f335a0878e67e5a357bef864bf57f4e278ad7
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-08-09 00:29:56 +0200

    Add reference for arrays.

--- C++98/examples/arrays.cpp
+++ C++98/examples/arrays.cpp
@@ -3,4 +3,5 @@
  *
  *   [odwyer20200806] The “array size constant” antipattern
  *     https://quuxplusone.github.io/blog/2020/08/06/array-size/
+ *     https://www.reddit.com/r/cpp/comments/i5bl28/the_array_size_constant_antipattern/
  */
