968c247f919810d4e9a6950b5eddc70fb9816ddb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-09-14 09:05:25 +0200

    Add reference for std::span.

--- C++20/examples/span/span.cpp
+++ C++20/examples/span/span.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [grimm20200914] C++20: Geschützter Zugriff auf Sequenzen von Objekten mit std::span
+ *     https://www.heise.de/developer/artikel/C-20-Geschuetzter-Zugriff-auf-Sequenzen-von-Objekten-mit-std-span-4892418.html
+ *
  *   [silisteanu20191103] C++20 span tutorial
  *     https://solarianprogrammer.com/2019/11/03/cpp-20-span-tutorial/
  *
