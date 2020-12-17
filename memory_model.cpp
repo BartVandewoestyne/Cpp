82f47d2d9758da5be857d431789141173363fd92
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-12-17 09:10:45 +0100

    Add reference for the C++ Memory Model.

--- memory_model.cpp
+++ memory_model.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [copperspice20201210] C++ Memory Model
+ *     https://youtu.be/KgzjxfYaScU
+ *
  *   [norberg20191121] The C++ memory model: an intuition
  *     https://youtu.be/OyNG4qiWnmU
  */
