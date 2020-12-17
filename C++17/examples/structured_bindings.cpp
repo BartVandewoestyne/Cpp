e4b8e8830be8f3faeaa36280f99779a0c071abe7
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2020-12-17 09:20:55 +0100

    Add reference for C++17's structured bindings.

--- C++17/examples/structured_bindings.cpp
+++ C++17/examples/structured_bindings.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [khizbullin20200924] C++17 structured bindings for more safe, functional code
+ *     https://itnext.io/c-17-structured-bindings-for-more-safe-functional-code-c0c5b4d31b0d
+ *
  *   [boccara20180619] 3 Simple C++17 Features That Will Make Your Code Simpler
  *     https://www.fluentcpp.com/2018/06/19/3-simple-c17-features-that-will-make-your-code-simpler/
  *
