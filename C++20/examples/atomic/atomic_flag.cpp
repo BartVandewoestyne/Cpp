871affc05ddd33fe7a93be2a04ef2e4fd9291f12
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-01-05 11:29:21 +0100

    Add reference for atomic_flag.

---
+++ C++20/examples/atomic/atomic_flag.cpp
@@ -0,0 +1,11 @@
+/*
+ * With C++20, std::atomic_flag supports new member functions:
+ *   atomicFlag.wait()
+ *   atomicFlag.notify_one()
+ *   and atomicFlag.notify_all().
+ *
+ * References:
+ *
+ *   [grimm20201221] Synchronization with Atomics in C++20
+ *     http://www.modernescpp.com/index.php/synchronization-with-atomics-in-c-20
+ */
