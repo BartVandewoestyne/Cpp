afa834c6492777adbcb2bb533e29ce6ede41242a
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-02-22 09:28:54 +0100

    Add example on synchronized output streams.

---
+++ C++20/examples/synchronized_output_streams.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [grimm20210222] Synchronisierte Ausgabe-Streams mit C++20
+ *     https://www.heise.de/developer/artikel/Synchronisierte-Ausgabe-Streams-mit-C-20-5061013.html
+ */
