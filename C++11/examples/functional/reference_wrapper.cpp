61a031f727379e844b52bcae79027530783dc0fe
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-02-22 16:24:59 +0100

    Add reference for std::reference_wrapper.

--- C++11/examples/functional/reference_wrapper.cpp
+++ C++11/examples/functional/reference_wrapper.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [kumar20200705] https://www.nextptr.com/tutorial/ta1450413058/unique_ptr-shared_ptr-weak_ptr-or-reference_wrapper-for-class-relationships
+ *     https://www.nextptr.com/tutorial/ta1450413058/unique_ptr-shared_ptr-weak_ptr-or-reference_wrapper-for-class-relationships
+ *
  *   [kumar20200625] std::ref and std::reference_wrapper: common use cases
  *     https://www.nextptr.com/tutorial/ta1441164581/stdref-and-stdreference_wrapper-common-use-cases
  */
