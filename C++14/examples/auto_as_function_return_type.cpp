ef0d407b9cb8ded645382a17a74724a3b8922c87
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-03-01 11:04:37 +0100

    Add examples on auto return type deduction.

--- C++14/examples/auto_as_function_return_type.cpp
+++
@@ -1,6 +0,0 @@
-/*
- * References:
- *
- *   [guzman2017] Don't use C++ auto? Restricting auto is not the best decision for your company, and it is unfair to your developers. Instead, train them on how to use it.
- *     http://swdevmastery.com/dont-use-c-auto-restricting-auto-is-not-the-best-decision-for-your-company-and-it-is-unfair-to-your-developers-instead-train-them-on-how-to-use-it/
- */
