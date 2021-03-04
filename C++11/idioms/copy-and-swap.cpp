d7fe401709f3b480f3382904f55bce3fd2d35e77
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-03-04 22:09:36 +0100

    Add reference for copy-and-swap idiom.

--- C++11/idioms/copy-and-swap.cpp
+++ C++11/idioms/copy-and-swap.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [weinmann20181220] C++: Advanced Topics - The copy-and-swap idiom
+ *     https://www.linkedin.com/learning/c-plus-plus-advanced-topics/the-copy-and-swap-idiom
+ *
  *   [mansfeld20171209] Copy-and-swap
  *     https://cpppatterns.com/patterns/copy-and-swap.html
  */
