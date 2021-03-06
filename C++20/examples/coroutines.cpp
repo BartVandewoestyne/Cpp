85a7dd841191c6a593a19f96cc510a67ccbb7c72
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-03-06 20:48:17 +0100

    Add reference for C++20 coroutines.

--- C++20/examples/coroutines.cpp
+++ C++20/examples/coroutines.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [grimm20210301] Implementing Simple Futures with Coroutines
+ *     http://www.modernescpp.com/index.php/implementing-futures-with-coroutines
+ *
  *   [grimm20210301] C++20: Einfache Futures mit Coroutinen implementieren
  *     https://www.heise.de/developer/artikel/C-20-Einfache-Futures-mit-Coroutinen-implementieren-5066861.html
  *
