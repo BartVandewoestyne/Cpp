1942a2ec5459e52b92c975682c31ff0b720ba92b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-03-25 10:32:57 +0100

    Add references for NVI pattern.

--- C++98/idioms/non_virtual_interface.cpp
+++ C++98/idioms/non_virtual_interface.cpp
@@ -7,6 +7,9 @@
  *   [wikibooks] More C++ Idioms - Non-Virtual Interface
  *     https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Non-Virtual_Interface
  *
+ *   [c++faq] Should I use protected virtuals instead of public virtuals?
+ *     https://isocpp.org/wiki/faq/strange-inheritance#protected-virtuals
+ *
  *   [carr20110903] Non-Virtual Interface Design Pattern
  *     http://www.blackwasp.co.uk/NVI.aspx
  *
@@ -14,4 +17,7 @@
  *     http://cpptruths.blogspot.com/2007/04/non-virtual-interface-nvi-idiom-and.html
  *   [sutter200109XX] Virtuality
  *     http://www.gotw.ca/publications/mill18.htm
+ *
+ *   [hyslop_sutter20001201] Conversations: Virtually Yours
+ *     https://www.drdobbs.com/conversations-virtually-yours/184403760
  */
