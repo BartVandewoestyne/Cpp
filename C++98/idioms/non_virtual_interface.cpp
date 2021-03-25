6567529d9d46edad8a17fac8743265c0b25c3e0d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-03-25 10:29:22 +0100

    Add references for the Non Virtual Interface pattern.

---
+++ C++98/idioms/non_virtual_interface.cpp
@@ -0,0 +1,17 @@
+/*
+ * References:
+ *
+ *   [wikipedia] Non-virtual interface pattern
+ *     https://en.wikipedia.org/wiki/Non-virtual_interface_pattern
+ *
+ *   [wikibooks] More C++ Idioms - Non-Virtual Interface
+ *     https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Non-Virtual_Interface
+ *
+ *   [carr20110903] Non-Virtual Interface Design Pattern
+ *     http://www.blackwasp.co.uk/NVI.aspx
+ *
+ *   [tambe20070411] Non-Virtual Interface (NVI) idiom and the design intent
+ *     http://cpptruths.blogspot.com/2007/04/non-virtual-interface-nvi-idiom-and.html
+ *   [sutter200109XX] Virtuality
+ *     http://www.gotw.ca/publications/mill18.htm
+ */
