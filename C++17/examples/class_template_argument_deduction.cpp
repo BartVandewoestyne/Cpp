b953391bcdef339f6601cbabb2b022ba1855cdba
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-04-10 00:10:20 +0200

    Add reference and example code for C++17 class template argument deduction.

--- C++17/examples/class_template_argument_deduction.cpp
+++ C++17/examples/class_template_argument_deduction.cpp
@@ -24,4 +24,44 @@
  *
  *   [mertz2017XXXX] Modern C++ Features – Class Template Argument Deduction
  *     https://arne-mertz.de/2017/06/class-template-argument-deduction/
+ *
+ *   [josuttis201701XX] C++17: The Language Features - Nicolai Josuttis
+ *     https://youtu.be/pEzV32yRu4U
  */
+
+#include <complex>
+#include <iostream>
+#include <string>
+
+template <typename T1, typename T2>
+class D
+{
+  public:
+    D(T1 x = T1(), T2 y = T2()) {}
+};
+
+int main()
+{
+    // OK, T1 and T2 are std::string
+    D<std::string, std::string> d0;
+
+    // OK, T1 and T2 are std::string
+    D<std::string, std::string> d1("hi", "world");
+
+    // OK since C++17, T1 is const char[3], T2 is const char[6]
+    // TODO: check if types are not just const char* ???  See also comment
+    // from Josuttis itself in his presentation...
+    D d2("hi", "world");
+
+    // OK since C++17, T1 is int, T2 is double
+    D d3(22, 44.3);
+
+    // Error: T1 and T2 undefined
+    //D d4;
+    
+    // OK since C++17, deduces a std::complex<int>
+    std::cout << std::complex(5, 3);
+
+    // Error: args do not have the same type.
+    //std::cout << std::complex(5, 3.3);
+}
