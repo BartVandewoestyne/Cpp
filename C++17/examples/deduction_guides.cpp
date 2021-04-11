05632ee5271cbb0605aa49252e05f31add03f292
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-04-12 00:11:12 +0200

    Add example for C++17 deduction guides.

--- C++17/examples/deduction_guides.cpp
+++ C++17/examples/deduction_guides.cpp
@@ -6,4 +6,24 @@
  *
  *   [raggi20180717] How to Construct C++ Objects Without Making Copies
  *     https://www.fluentcpp.com/2018/07/17/how-to-construct-c-objects-without-making-copies/
+ *
+ *   [josuttis201701XX] C++17: The Language Features - Nicolai Josuttis
+ *     https://youtu.be/pEzV32yRu4U
  */
+
+#include <memory>
+
+// deduction guide: maps shared pointer initialization to shared pointer type.
+namespace std {
+    template<typename Y> shared_ptr(Y*) -> shared_ptr<Y>;
+}
+
+int main()
+{
+    // OK
+    std::shared_ptr<int> sp(new int(42));
+
+    // Error without deduction guide (can't deduce type T from type Y)
+    // OK with deduction guide
+    std::shared_ptr sp2(new int(42));
+}
