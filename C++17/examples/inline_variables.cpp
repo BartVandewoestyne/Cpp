aea6493407200264f3a0b4e9679a6a11dff0a742
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-04-12 01:18:54 +0200

    Add example code for C++17 inline variables.

--- C++17/examples/inline_variables.cpp
+++ C++17/examples/inline_variables.cpp
@@ -1,4 +1,12 @@
 /*
+ * Key ideas:
+ *   - Static variables marked with inline count as definitions.
+ *     -> Guaranteed to exist only once in the program,
+ *        even if included multiple times in multiple translation units.
+ *     -> No need for .cpp files to define static/global objects
+ *   - Might be initialized before main() or before first use.
+ *   - constexpr implies inline.
+ *
  * References:
  *
  *   [boccara20190723] What Every C++ Developer Should Know to (Correctly) Define Global Constants
@@ -6,4 +14,39 @@
  *
  *   [gregoire20170828] C++17: Inline Variables
  *     http://www.nuonsoft.com/blog/2017/08/28/c17-inline-variables/
+ *
+ *   [josuttis201701XX] C++17: The Language Features - Nicolai Josuttis
+ *     https://youtu.be/pEzV32yRu4U
  */
+
+#include <iostream>
+
+class WithStaticDataMember
+{
+    // Since C++17, this is a definition (since constexpr implies inline);
+    // no further definition in a CPP file is required.
+    static constexpr const char* debug_string = "DEBUG";
+};
+
+class C
+{
+public:
+    static void log(bool b)
+    {
+        lg = b;
+    }
+
+    C()
+    {
+        if (lg) std::cout << "C::C()\n";
+    }
+
+private:
+    static inline bool lg = false;
+};
+
+int main()
+{
+    C::log(true);
+    C c1;
+}
