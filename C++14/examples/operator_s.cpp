7a51391658011a7af674b945fd4ad67c45768092
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-05-07 12:52:37 +0200

    Add example code for C++14 operator_s.

--- C++14/examples/operator_s.cpp
+++ C++14/examples/operator_s.cpp
@@ -6,3 +6,16 @@
  *   [guzman2017] A SIMPLE way to make your C++ programs easier to read NOW, Suffix for basic_string Literals - this little known feature can be extremely valuable
  *     http://swdevmastery.com/a-simple-way-to-make-your-c-programs-easier-to-read-now-suffix-for-basic_string-literals-this-little-known-feature-can-be-extremely-valuable/
  */
+
+#include <string>
+#include <iostream>
+ 
+int main()
+{
+    using namespace std::string_literals;
+ 
+    std::string s1 = "abc\0\0def";
+    std::string s2 = "abc\0\0def"s;
+    std::cout << "s1: " << s1.size() << " \"" << s1 << "\"\n";
+    std::cout << "s2: " << s2.size() << " \"" << s2 << "\"\n";
+}
