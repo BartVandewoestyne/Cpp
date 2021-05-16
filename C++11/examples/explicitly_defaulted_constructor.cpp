0fece3e508f4b7d4516c5cc6d9e6275ce033a2a6
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-05-16 12:00:38 +0200

    Add reference for explicitly C++11 defaulted constructor.

---
+++ C++11/examples/explicitly_defaulted_constructor.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [stackoverflow20121127] How is "=default" different from "{}" for default constructor and destructor?
+ *     https://stackoverflow.com/questions/13576055/how-is-default-different-from-for-default-constructor-and-destructor
+ */
