bfd3b1714cd495035cab250fb1c9c6af836cf15d
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-07-09 13:06:29 +0200

    Add reference for in-class initializers.

--- C++11/examples/non-static_data_member_initializers.cpp
+++ C++11/examples/non-static_data_member_initializers.cpp
@@ -3,11 +3,17 @@
  * 
  * References:
  *
+ *   [C++ Super-FAQ] In-class member initializers
+ *     https://isocpp.org/wiki/faq/cpp11-language-classes#member-init
+ *
  *   [cppreference] Member initialization
  *     http://en.cppreference.com/w/cpp/language/data_members#Member_initialization
  *
  *   [filipek20200302] In-class Member Initialisation: From C++11 to C++20
  *     https://www.bfilipek.com/2015/02/non-static-data-members-initialization.html
+ *
+ *   [kalev202120823] C++11 Tutorial: New Constructor Features that Make Object Initialization Faster and Smoother
+ *     https://smartbear.com/blog/c11-tutorial-new-constructor-features-that-make-ob/
  */
 
 #include <iostream>
