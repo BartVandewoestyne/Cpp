a1b02bb442e31ceda1da68a5d9bfc9230bbaa7b5
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-07-13 22:37:07 +0200

    Add key idea on constexpr in header files.

--- C++11/examples/constexpr_in_header_files.cpp
+++ C++11/examples/constexpr_in_header_files.cpp
@@ -1,4 +1,12 @@
 /*
+ * Key idea:
+ *
+ *   constexpr implies const and const on global/namespace scope implies static
+ *   (internal linkage), which means that every translation unit including this
+ *   header gets its own copy of PI. The memory for that static is only going
+ *   to be allocated if an address or reference to it is taken, and the address
+ *   is going to be different in each translation unit.
+ *
  * References:
  *
  *   [stackoverflow20180523] use of constexpr in header file
