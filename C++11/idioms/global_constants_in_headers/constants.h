66d550c1f1c37cc77eec17275446e08dc0e9b981
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-08-10 09:41:56 +0200

    Add more comments.

--- C++11/idioms/global_constants_in_headers/constants.h
+++ C++11/idioms/global_constants_in_headers/constants.h
@@ -13,6 +13,11 @@
 
 #include "X.h"
 
+// constexpr implies const and const on global/namespace scope implies static
+// (internal linkage), which means that every translation unit including this
+// header gets its own copy of ci. The memory for that static is only going to
+// be allocated if an address or reference to it is taken, and the address is
+// going to be different in each translation unit.
 constexpr int ci = 10;
 
 static constexpr int sci = 20;
