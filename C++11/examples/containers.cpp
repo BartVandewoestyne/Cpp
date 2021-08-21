3469470f4935143cd7aef0c26762c1dcff415958
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-08-21 15:47:42 +0200

    Move container references from C++17 to C++11.

--- C++11/examples/containers.cpp
+++ C++11/examples/containers.cpp
@@ -3,4 +3,10 @@
  *
  *   [barr20210405] Back To Basics: C++ Containers
  *     https://youtu.be/6OoSgY6NVVk
+ *
+ *   [talbot20190917] How to Choose the Right Standard Library Container, and Why You Should Want Some More
+ *     https://youtu.be/yjPKVOYcw28
+ *
+ *   [johnston20170707] Ditch Those Built-in Arrays for C++ Containers
+ *     https://embeddedartistry.com/blog/2017/07/07/ditch-those-built-in-arrays-for-c-containers/
  */
