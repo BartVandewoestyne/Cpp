9d9b1c161446a5e9a5a6edef2c45139e5b2080f8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-08-21 11:29:55 +0200

    Add references for containers.

---
+++ C++17/examples/containers.cpp
@@ -0,0 +1,4 @@
+/*
+ * [talbot20190917] How to Choose the Right Standard Library Container, and Why You Should Want Some More
+ *   https://youtu.be/yjPKVOYcw28
+ */
---
+++ C++98/examples/containers.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [barr20210405] Back To Basics: C++ Containers
+ *     https://youtu.be/6OoSgY6NVVk
+ */
