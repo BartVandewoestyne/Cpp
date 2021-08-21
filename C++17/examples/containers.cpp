a0f1659975724a65f9a33da0fd45001dff6467ae
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-08-21 15:48:10 +0200

    Remove file as references are in C++11 directory.

--- C++17/examples/containers.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * [talbot20190917] How to Choose the Right Standard Library Container, and Why You Should Want Some More
- *   https://youtu.be/yjPKVOYcw28
- *
- * []
- *   https://embeddedartistry.com/blog/2017/07/07/ditch-those-built-in-arrays-for-c-containers/
- */
