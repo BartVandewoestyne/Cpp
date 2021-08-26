b439ac2b98c97af3ac7a26c4a9c44fcff6e943a8
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-08-26 13:50:45 +0200

    Add reference for C++20 [[no_unique_address]] attribute.

--- C++20/examples/no_unique_address.cpp
+++ C++20/examples/no_unique_address.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [filipek20210823] Empty Base Class Optimisation, no_unique_address and unique_ptr
+ *     https://www.cppstories.com/2021/no-unique-address/
+ *
  *   https://www.reddit.com/r/cpp/comments/854mu9/2018_jacksonville_iso_c_committee_reddit_trip/
  *
  */
