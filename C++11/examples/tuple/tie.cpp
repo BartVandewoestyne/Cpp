d693fc89530717327e791622759a99eba3bdb0bc
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-08-26 13:59:19 +0200

    Add reference on how to define comparison operators.

--- C++11/examples/tuple/tie.cpp
+++ C++11/examples/tuple/tie.cpp
@@ -1,4 +1,7 @@
 /*
+ * Key ideas:
+ *   - std::tie can be used to define comparision operators
+ *
  * Note:
  *   Consider using C++17's structured bindings instead of std::tie.
  *
@@ -6,6 +9,9 @@
  *   std::ignore
  *
  * References:
+ *
+ *   [boccara20210823] How to Define Comparison Operators by Default in C++
+ *     https://www.fluentcpp.com/2021/08/23/how-to-define-comparison-operators-by-default-in-c/
  *
  *   [gregory2017cppcon] 10 Core Guidelines You Need to Start Using Now
  *     https://youtu.be/XkDEzfpdcSg?t=34m15s
