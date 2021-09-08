bfe2b1136b2790239271bc86875d34e198768e79
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-09-08 14:07:00 +0200

    Add references for memset.

---
+++ C++98/examples/cstring/memset.cpp
@@ -0,0 +1,12 @@
+/*
+ * References:
+ *
+ *   [karpov20151203] The most dangerous function in the C/C++ world
+ *     https://pvs-studio.com/en/blog/posts/cpp/0360/
+ *
+ *   [stackoverflow20091229] Should C++ programmer avoid memset?
+ *     https://stackoverflow.com/questions/1975916/should-c-programmer-avoid-memset
+ *
+ *   [webarchive] C++ : Extension to C : memset is EvilTM
+ *     https://web.archive.org/web/20170702122030/https:/augias.org/paercebal/tech_doc/doc.en/cp.memset_is_evil.html
+ */
