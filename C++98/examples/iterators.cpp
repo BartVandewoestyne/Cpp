75adb8b36572c7b622f4dc75b9af444274337497
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-09-14 09:02:25 +0200

    Add reference for iterators.

---
+++ C++98/examples/iterators.cpp
@@ -0,0 +1,6 @@
+/*
+ * References:
+ *
+ *   [20210913reddit] Any good resources to learn the ins-n-outs about iterators?
+ *     https://www.reddit.com/r/cpp/comments/pn18qt/any_good_resources_to_learn_the_insnouts_about/
+ */
