c9e430ee15c560c1d9853a7fa7875c2798ce5544
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-10-08 09:20:48 +0200

    Add reference for chrono literals.

--- C++14/examples/chrono_literals.cpp
+++ C++14/examples/chrono_literals.cpp
@@ -1,6 +1,9 @@
 /**
  * References:
  *
+ *   [cppreference] std::literals::chrono_literals Symbol Index
+ *     https://en.cppreference.com/w/cpp/symbol_index/chrono_literals
+ *
  *   [boccara20211008] A Recap on User Defined Literals
  *     https://www.fluentcpp.com/2021/10/08/a-recap-on-user-defined-literals/
  *
