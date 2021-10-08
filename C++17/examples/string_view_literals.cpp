1b6bc3c3c35f6443e7d8b1e72e6125cc1d00e9ed
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-10-08 09:23:50 +0200

    Add reference for string_view_literals.

---
+++ C++17/examples/string_view_literals.cpp
@@ -0,0 +1,9 @@
+/*
+ * Reference:
+ *
+ *   [cppreference] std::literals::string_view_literals::operator""sv
+ *     https://en.cppreference.com/w/cpp/string/basic_string_view/operator%22%22sv
+ *
+ *   [boccara20211008] A Recap on User Defined Literals
+ *     https://www.fluentcpp.com/2021/10/08/a-recap-on-user-defined-literals/
+ */
