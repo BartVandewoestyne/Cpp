138210cd3c993e4bc657e4a2c914d1fb73150065
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-10-19 09:13:39 +0200

    Add more references for lambda captures.

--- C++20/examples/lambda_captures.cpp
+++ C++20/examples/lambda_captures.cpp
@@ -1,6 +1,12 @@
 /*
  * References:
  *
+ *   [???] Lambda capture as const reference?
+ *     https://newbedev.com/lambda-capture-as-const-reference
+ *
+ *   [stackoverflow20100922] Lambda capture as const reference?
+ *     https://stackoverflow.com/questions/3772867/lambda-capture-as-const-reference
+ *
  *   [filipek20200805] Lambda Week: Capturing Things
  *     https://www.cppstories.com/2020/08/lambda-capturing.html/
  */
