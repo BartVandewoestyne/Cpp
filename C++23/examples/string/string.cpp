a789ca98b25db72b3224aed0902d251534d08cfa
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-11-24 20:32:31 +0100

    Add stuff on std::string.

--- C++23/examples/string/string.cpp
+++ C++23/examples/string/string.cpp
@@ -8,12 +8,28 @@
 #include <string>
 #include <fmt/core.h>  // TODO: change this once its standardized!
 
+// This will construct a string with a nullptr.  In C++20, this
+// gave a runtime exception, in C++23 this fails at compile time.
+std::string doit()
+{
+    // some long block of complex code
+    return nullptr;
+}
+
+
 int main()
 {
   std::string s( "the answer is 42" );
+
+  // 1. Addition of contains() and ends_with() member functions.
   if ( !s.contains( "foo" ) && s.ends_with( "42" ) ) 
   {
-    fmt::print("so long, and thanks for all the fish\n");
+
+    fmt::print("so long, and thanks for all the fish\n");  // TODO: change this once it's standardized!
   }
+
+  // 2. Constructs string from nullptr: runtime error in C++20, but should give compile-time error in C++23.
+  std::string s = doit();
+  fmt::print("{}\n", s);  // TODO: use std::print
 }
 
