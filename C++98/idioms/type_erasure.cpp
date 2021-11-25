34acf6341c97c4eb128e77ffddf63fae7da9c67e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2021-11-25 22:48:59 +0100

    Add references for Type Erasure.

--- C++98/idioms/type_erasure.cpp
+++ C++98/idioms/type_erasure.cpp
@@ -1,9 +1,17 @@
 /*
  * References:
  *
+ *   [cppcoreguidelinesT5] Combine generic and OO techniques to amplify their strengths, not their costs
+ *     https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rt-generic-oo
+ *   [cppcoreguidelinesT49] Where possible, avoid type-erasure
+ *     https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rt-erasure
+ *
  *   [tambe2007XXXX] More C++ Idioms - Type Erasure
  *     https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Type_Erasure
  *
+ *   [grimm20180917] C++ Core Guidelines: Type Erasure with Templates
+ *     https://www.modernescpp.com/index.php/c-core-guidelines-type-erasure-with-templates
+ *     
  *   [grimm20180910] C++ Core Guidelines: Type Erasure
  *     http://www.modernescpp.com/index.php/c-core-guidelines-type-erasure
  *
