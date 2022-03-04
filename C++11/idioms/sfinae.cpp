94eb75840def720100ecfc7265212a77f61fc191
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-03-04 16:46:06 +0100

    Add reference for SFINAE.

--- C++11/idioms/sfinae.cpp
+++ C++11/idioms/sfinae.cpp
@@ -1,6 +1,10 @@
 /*
  * References:
  *
+ *   [wilczek20210610] SFINAE: Substitution Failure is not an Error | Top-Up C++ [C++ #02]
+ *     https://youtu.be/mNxAqLVIaW0
+ *     https://www.thewolfsound.com/sfinae-substitution-failure-is-not-an-error/
+ *
  *   [chen20190619] How do I write a function that accepts any type of standard container?
  *     https://devblogs.microsoft.com/oldnewthing/20190619-00/?p=102599
  *
