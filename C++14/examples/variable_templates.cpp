ce8b4be85d7e948295fded18abc58c2438ee759b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-03-16 13:41:14 +0100

    Update references for variable templates.

--- C++14/examples/variable_templates.cpp
+++ C++14/examples/variable_templates.cpp
@@ -1,8 +1,21 @@
 /*
  * References:
- *   [1] https://baptiste-wicht.com/posts/2017/08/simplify-your-type-traits-with-c%2B%2B14-variable-templates.html
- *   [2] https://www.ibm.com/developerworks/community/blogs/5894415f-be62-4bc0-81c5-3956e82276f3/entry/Introduction_to_Variable_Templates_of_C_14?lang=en
- *   [3] https://meetingcpp.com/tl_files/mcpp/2015/talks/peter%20sommerlad%20-%20meetingc++2015variadicvariabletemplates.pdf
- *   [4] https://isocpp.org/files/papers/N3651.pdf
- *   [5] https://accu.org/index.php/journals/2087
+ *
+ *   [stackoverflow] C++14 Variable Templates: what is their purpose? Any usage example?
+ *     https://stackoverflow.com/questions/21051141/c14-variable-templates-what-is-their-purpose-any-usage-example
+ *
+ *   [wicht20170822] Simplify your type traits with C++14 variable templates
+ *     https://baptiste-wicht.com/posts/2017/08/simplify-your-type-traits-with-c%2B%2B14-variable-templates.html
+ *
+ *   [2]
+ *     https://www.ibm.com/developerworks/community/blogs/5894415f-be62-4bc0-81c5-3956e82276f3/entry/Introduction_to_Variable_Templates_of_C_14?lang=en
+ *
+ *   [3]
+ *     https://meetingcpp.com/tl_files/mcpp/2015/talks/peter%20sommerlad%20-%20meetingc++2015variadicvariabletemplates.pdf
+ *
+ *   [dosreis20130419] Variable Templates (Revision 1)
+ *     https://isocpp.org/files/papers/N3651.pdf
+ *
+ *   [sommerlad201504] Variadic and Variable Templates
+ *     https://accu.org/index.php/journals/2087
  */
