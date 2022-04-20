58c8fd4953d5be79d633e9f702b2a420d8656a3e
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-04-20 14:39:39 +0200

    Add reference for Meyers' singleton.

--- C++11/idioms/meyers_singleton.cpp
+++ C++11/idioms/meyers_singleton.cpp
@@ -3,6 +3,9 @@
  *
  * References:
  *
+ *   [vorbrodt20220420] Singleton Pattern Explained (in C++)
+ *     https://youtu.be/VTd7N2eRgNQ
+ *
  *   [grimm2017] Concurrency with Modern C++ (singletonSingleThreaded.cpp)
  *     https://leanpub.com/concurrencywithmodernc
  *
