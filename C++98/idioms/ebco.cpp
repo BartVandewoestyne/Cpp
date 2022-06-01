0628c35f3bc8dd97488db6f13b29a1e2763dd495
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-06-01 14:29:33 +0200

    Add references for EBCO.

---
+++ C++98/idioms/ebco.cpp
@@ -0,0 +1,12 @@
+/*
+ * References:
+ *
+ *   [cppreference] Empty base optimization
+ *     https://en.cppreference.com/w/cpp/language/ebo
+ *
+ *   [wikibooks] Empty Base Optimization
+ *     https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Empty_Base_Optimization
+ *
+ *   [vandevoorde2003] 16.2 The Empty Base Class Optimization (EBCO)
+ *     https://www.informit.com/articles/article.aspx?p=31473&seqNum=2
+ */
