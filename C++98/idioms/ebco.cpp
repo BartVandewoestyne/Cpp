7da8ddef4490fe324fa0a5e8f372148e07bb4dbb
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-06-02 14:17:05 +0200

    Fix reference.

--- C++98/idioms/ebco.cpp
+++ C++98/idioms/ebco.cpp
@@ -7,6 +7,6 @@
  *   [wikibooks] Empty Base Optimization
  *     https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Empty_Base_Optimization
  *
- *   [vandevoorde2003] 16.2 The Empty Base Class Optimization (EBCO)
+ *   [vandevoorde20021112] 16.2 The Empty Base Class Optimization (EBCO)
  *     https://www.informit.com/articles/article.aspx?p=31473&seqNum=2
  */
