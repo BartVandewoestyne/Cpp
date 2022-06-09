d1e4294c5ca34b92e0ab9bdbc50eb157777f527f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-06-09 14:22:31 +0200

    Add example code for string_view literals

--- C++17/examples/string_view_literals.cpp
+++ C++17/examples/string_view_literals.cpp
@@ -1,4 +1,8 @@
 /*
+ * Key idea:
+ *
+ *   - string_view literals were added in C++17.
+ *
  * Reference:
  *
  *   [cppreference] std::literals::string_view_literals::operator""sv
@@ -7,3 +11,13 @@
  *   [boccara20211008] A Recap on User Defined Literals
  *     https://www.fluentcpp.com/2021/10/08/a-recap-on-user-defined-literals/
  */
+
+#include <string_view>
+
+int main()
+{    
+    using namespace std::literals::string_view_literals;
+
+    // myStringView is of type std::string_view
+    auto myStringView = "hello"sv;
+}
