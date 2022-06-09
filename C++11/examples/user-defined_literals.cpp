71f059ef47903542bd30b17eaa51b9f22ec454fe
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-06-09 14:03:34 +0200

    Add link to Grimm's blogpost about User Defined Literals.

--- C++11/examples/user-defined_literals.cpp
+++ C++11/examples/user-defined_literals.cpp
@@ -3,4 +3,7 @@
  *
  *   [halpern20220406] Embracing User-Defined Literals Safely - Pablo Halpern - ACCU 2022
  *     https://youtu.be/_nW2iB6_BAU
+ *
+ *   [grimm20161029] User-Defined Literals
+ *     https://www.modernescpp.com/index.php/user-defined-literals
  */
