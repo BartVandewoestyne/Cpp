9505e5ac5f3ff31b553f0299caa7d739d90de7d1
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-06-09 14:19:32 +0200

    Add example on chrono literals.

--- C++14/examples/user_defined_literals.cpp
+++ C++14/examples/user_defined_literals.cpp
@@ -9,6 +9,7 @@
  *     https://www.fluentcpp.com/2021/10/08/a-recap-on-user-defined-literals/
  */
 
+#include <chrono>
 #include <string>
 
 int main()
@@ -20,4 +21,15 @@ int main()
 
     // myString2 is of type const char*
     auto myString2 = "hello";
+
+
+    using namespace std::literals::chrono_literals;
+
+    auto threeSeconds = 3s;
+    auto tenMinutes = 10min;
+    auto twoHours = 2h;
+
+    auto oneMillisecond = 1ms;
+    auto oneMicroSecond = 1us;
+    auto oneNanoSecond = 1ns;
 }
