2f02bb1100d0214020fc431f27db9c523e49524f
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-06-09 14:13:46 +0200

    Add some references for User Defined Literals

--- C++11/examples/user-defined_literals.cpp
+++ C++11/examples/user-defined_literals.cpp
@@ -1,8 +1,20 @@
 /*
  * References:
  *
+ *   [geeksforgeeks] User Defined Literals in C++
+ *     https://www.geeksforgeeks.org/user-defined-literals-cpp/
+ *
  *   [halpern20220406] Embracing User-Defined Literals Safely - Pablo Halpern - ACCU 2022
  *     https://youtu.be/_nW2iB6_BAU
+ *
+ *   [boccara20211008] A Recap on User Defined Literals
+ *     https://www.fluentcpp.com/2021/10/08/a-recap-on-user-defined-literals/
+ *
+ *   [microsoft20210308] User-defined literals
+ *     https://docs.microsoft.com/en-us/cpp/cpp/user-defined-literals-cpp?view=msvc-170
+ *
+ *   [mertz20161218] Modern C++ Features: User-Defined Literals
+ *     https://accu.org/journals/overload/24/136/mertz_2318/
  *
  *   [grimm20161029] User-Defined Literals
  *     https://www.modernescpp.com/index.php/user-defined-literals
