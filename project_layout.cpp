bb35376bb902ecabb67e82e35a4b1bd93444227b
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-06-13 10:20:34 +0200

    Add references for C++ project layout.

--- project_layout.cpp
+++ project_layout.cpp
@@ -5,4 +5,10 @@
  *     https://github.com/vector-of-bool/pitchfork
  *     https://api.csswg.org/bikeshed/?force=1&url=https://raw.githubusercontent.com/vector-of-bool/pitchfork/develop/data/spec.bs
  *     https://www.reddit.com/r/cpp/comments/996q8o/prepare_thy_pitchforks_a_de_facto_standard/
+ *
+ *   [lakos20191217] Large-Scale C++ Volume I: Process and Architecture
+ *     https://youtu.be/4NvRnkz6jVM
+ *     https://youtu.be/d3zMfMC8l5U
+ *
+ *   [lakos19960821] Large Scale C++ Software Design
  */
