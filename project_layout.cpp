7df834e0049409ec97eae93eeeecdf327d98b897
Bart Vandewoestyne <Bart.Vandewoestyne@telenet.be>
2022-06-13 10:08:59 +0200

    Add reference on project layout.

---
+++ project_layout.cpp
@@ -0,0 +1,8 @@
+/*
+ * References:
+ *
+ *   [pike] Pitchfork layout
+ *     https://github.com/vector-of-bool/pitchfork
+ *     https://api.csswg.org/bikeshed/?force=1&url=https://raw.githubusercontent.com/vector-of-bool/pitchfork/develop/data/spec.bs
+ *     https://www.reddit.com/r/cpp/comments/996q8o/prepare_thy_pitchforks_a_de_facto_standard/
+ */
