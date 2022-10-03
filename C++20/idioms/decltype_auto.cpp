8e7cc88b3bdd0678fde5f8f310a88143605067ac
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2022-10-04 00:13:07 +0200

    Add reference for decltype(auto) in C++20.

---
+++ C++20/idioms/decltype_auto.cpp
@@ -0,0 +1,9 @@
+/**
+ * TODO:
+ *   - Check if example code also compiles as C++14 and move it there if 'yes'.
+ *
+ * References:
+ *
+ *   [turner20221003] decltype(auto): An Overview of How, Why, and Where
+ *     https://youtu.be/E5L66fkNlpE
+ */
