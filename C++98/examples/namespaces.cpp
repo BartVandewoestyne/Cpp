5099a4cd8ee0c43a5c5db73fc4aaf90fd929e5fd
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2022-10-04 23:46:38 +0200

    Add Microsoft reference for namespaces.

--- C++98/examples/namespaces.cpp
+++ C++98/examples/namespaces.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [microsoft] Namespaces (C++)
+ *     https://learn.microsoft.com/en-us/cpp/cpp/namespaces-cpp
+ *
  *   [reddit20210523] Discussion: When and how to use namespaces
  *     https://www.reddit.com/r/cpp/comments/nj8z1y/discussion_when_and_how_to_use_namespaces/?utm_source=share&utm_medium=web2x&context=3
  */
