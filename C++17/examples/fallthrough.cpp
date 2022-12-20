4e861058b7bb46973b27ab0117c09384119a2786
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2022-12-20 20:27:32 +0100

    Add example code for [[fallthrough]].

--- C++17/examples/fallthrough.cpp
+++ C++17/examples/fallthrough.cpp
@@ -1,4 +1,11 @@
 /*
+ * Key ideas:
+ *   - [[fallthrough]]; helps you to avoid compiler warnings for fallthroughs.
+ *   - no [[fallthrough]] is necessary for empty case statements.
+ *
+ * Compile with:
+ *   gcc --std=c++17 -Wextra
+ *
  * References:
  *
  *   [gregoire20180604] C++17: Fallthrough in switch statements
@@ -6,3 +13,36 @@
  *   [karpov20180127] break and fallthrough
  *     https://www.viva64.com/en/b/0554/
  */
+
+#include <iostream>
+
+bool f()
+{   
+    switch (2)
+    {
+        case 1:
+            std::cout << "hello world" << std::endl;
+            //[[fallthrough]];  // necessary to avoid the fallthrough warning
+        case 2: 
+            return true;
+        default:
+            return false;
+    }  
+}
+
+bool g()
+{
+    switch (2)
+    {
+        case 1:
+            // no [[fallthrough]] necessary because of empty case statement
+        case 2: 
+            return true;
+        default:
+            return false;
+    } 
+}
+
+int main()
+{
+}
