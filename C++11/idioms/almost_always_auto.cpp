e822b87df1e043627d3c1a6540f74bfddb5e7166
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-01-18 00:24:29 +0100

    Add some key ideas of the AAA-style.

--- C++11/idioms/almost_always_auto.cpp
+++ C++11/idioms/almost_always_auto.cpp
@@ -1,4 +1,24 @@
 /**
+ * Key ideas:
+ *   - Using auto is not primarily about saving typing, but the main reasons
+ *     to declare variables using auto are for correctness, performance,
+ *     maintainability and robustness and only lastly about typing convenience.
+ *
+ *   - You can still commit to a certain type by writing declarations of the
+ *     form
+ *
+ *       auto x = type{init};
+ *
+ *     instead of
+ *
+ *       type x{init};
+ *
+ *     Advantages:
+ *       - It is self-documenting that the code is explicitly requesting a
+ *         conversion.
+ *       - It guarantees that the variable will be initialized.
+ *       - It won't allow an accidental implicit narrowing conversion.  Only
+ *         when you do want explicit narrowing, use () instead of {}.
  *
  * Some examples on Herb Sutter's 'AAA Style (Almost Always Auto)'.
  *
