08a1ab67065522b6cc577e8d603d7ce389e6626e
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-01-17 14:08:55 +0100

    Add references for 'if (pointer)' vs 'if (pointer != nullptr)'.

--- C++11/examples/nullptr_if_condition.cpp
+++ C++11/examples/nullptr_if_condition.cpp
@@ -1,5 +1,21 @@
 /*
  * My own test program to check if nullptr can be used in an if-condition.
+ *
+ * Key idea:
+ *
+ *   - It seems like there are not hard objective arguments on why 'if (pointer)' should
+ *     be preferred over 'if (pointer != null)', and things are mostly opinion based.
+ *
+ * References:
+ *
+ *   [stackoverflow20160731] difference between if(pointer) vs if(pointer != NULL) in c++, cpplint issue
+ *     https://stackoverflow.com/questions/38682813/difference-between-ifpointer-vs-ifpointer-null-in-c-cpplint-issue
+ *
+ *   [stackoverflow20150513] Idiomatic way to compare to nullptr: if(p) vs if (p != nullptr)
+ *     https://stackoverflow.com/questions/30218714/idiomatic-way-to-compare-to-nullptr-ifp-vs-if-p-nullptr
+ *
+ *   [stackoverflow20130721] Can I use if (pointer) instead of if (pointer != NULL)?
+ *     https://stackoverflow.com/questions/17772103/can-i-use-if-pointer-instead-of-if-pointer-null
  */
 
 #include <iostream>
