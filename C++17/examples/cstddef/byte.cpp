9567f3f7f387cf7285e9f14fe309083310f96e4c
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-02-08 00:10:02 +0100

    Add reference for C++17 std::byte.

--- C++17/examples/cstddef/byte.cpp
+++ C++17/examples/cstddef/byte.cpp
@@ -1,5 +1,14 @@
 /*
+ * Key ideas:
+ *
+ *   - Using std::byte also has disadvantages.  For example, when compiled in
+ *     debug mode, without optimizations, has more overhead compared to char
+ *     (See [romeo2022XXXX] at 19:52)
+ *
  * References:
+ *
+ *   [romeo2022XXXX] Pragmatic Simplicity - Actionable Guidelines To Tame Cpp Complexity - Vittorio Romeo - CppCon 2022
+ *     https://youtu.be/3eH7JRgLnG8
  *
  *   [codingjesus20200707] C++ - std::byte
  *     https://youtu.be/VTsOJIcBpRQ
