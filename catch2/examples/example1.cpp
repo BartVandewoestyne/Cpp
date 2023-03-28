d2e24cd4fafb8c2d7e2929f9f819f65edac36e4e
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-03-28 15:38:50 +0200

    Add example on comparing floating point numbers.

--- catch2/examples/example1.cpp
+++ catch2/examples/example1.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include <cstdint>
 
@@ -25,4 +26,14 @@ TEST_CASE( "Factorials are computed", "[factorial]" )
     REQUIRE( Factorial(3) == 6 );
     REQUIRE( Factorial(8) == 40320 );
 }
+
+TEST_CASE( "Comparing numbers", "[numbersTest]" )
+{
+    double x = 1.0;
+	REQUIRE_THAT(x, Catch::Matchers::WithinULP(1.0, 4));
+
+    float y = 2.0f;
+	REQUIRE_THAT(y, Catch::Matchers::WithinULP(2.0f, 4));
+
+    REQUIRE_THAT(1.0/3, Catch::Matchers::WithinAbs(1 - 2.0/3, 1e-6));
 }
