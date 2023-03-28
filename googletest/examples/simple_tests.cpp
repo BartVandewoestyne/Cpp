29c4150f4af8bf721ae767ba0fb6d19e32220bd4
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-03-28 15:34:03 +0200

    Add EXPECT_NO_THROW example.

--- googletest/examples/simple_tests.cpp
+++ googletest/examples/simple_tests.cpp
@@ -7,3 +7,10 @@ TEST(SimpleTests, BasicTests)
   EXPECT_EQ(std::max(1, 2), 2);
   EXPECT_STRNE("hello", "world");
 }
+
+TEST(FooTest, ItDoesNotThrow)
+{
+    EXPECT_NO_THROW({
+        auto x = 1 + 2;
+    });
+}
