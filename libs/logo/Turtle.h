8d98e5a86a30ec57f7154ef5a92ec1e2ef73d9d1
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-03-29 23:41:31 +0200

    Remove comment and fix indentation.

--- libs/logo/Turtle.h
+++ libs/logo/Turtle.h
@@ -4,15 +4,14 @@
 class Turtle
 {
 public:
-  // ...
-  virtual ~Turtle() = default;
-  virtual void penUp() = 0;
-  virtual void penDown() = 0;
-  virtual void forward(int distance) = 0;
-  virtual void turn(int degrees) = 0;
-  virtual void goTo(int x, int y) = 0;
-  virtual int getX() const = 0;
-  virtual int getY() const = 0;
+    virtual ~Turtle() = default;
+    virtual void penUp() = 0;
+    virtual void penDown() = 0;
+    virtual void forward(int distance) = 0;
+    virtual void turn(int degrees) = 0;
+    virtual void goTo(int x, int y) = 0;
+    virtual int getX() const = 0;
+    virtual int getY() const = 0;
 };
 
 #endif // TURTLE_H
