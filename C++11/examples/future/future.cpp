da6475a51bccdf6bd20a58dc3c283b3e2de0d921
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-04-26 11:51:46 +0200

    Add code example for std::future.

--- C++11/examples/future/future.cpp
+++ C++11/examples/future/future.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [williams2012] C++ Concurrency in Action
+ *     https://www.manning.com/books/c-plus-plus-concurrency-in-action
+ *
  *   [petriconi2018] There Is A Better Future - Felix Petriconi [ACCU 2018]
  *     https://youtu.be/L63XGqiNuhI
  *
@@ -13,3 +16,22 @@
  *   [varun2015] C++11 Multithreading – Part 8: std::future , std::promise and Returning values from Thread
  *     http://thispointer.com/c11-multithreading-part-8-stdfuture-stdpromise-and-returning-values-from-thread/
  */
+
+#include <future>
+#include <iostream>
+
+int find_the_answer_to_ltuae()
+{
+  return 42;
+}
+
+void do_other_stuff()
+{
+}
+
+int main()
+{
+    std::future<int> the_answer = std::async(find_the_answer_to_ltuae);
+    do_other_stuff();
+    std::cout << "The answer is " << the_answer.get() << std::endl;
+}
