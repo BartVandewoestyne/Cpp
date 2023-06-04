60f100d21bba1b307375ad293c799983b4bccef5
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-06-04 09:15:24 +0200

    Add references for C++23 if consteval.

---
+++ C++23/examples/if_consteval.cpp
@@ -0,0 +1,12 @@
+/*
+ * References:
+ *
+ *   [dargo20220601] C++23: Consteval if to make compile time programming easier
+ *     https://www.sandordargo.com/blog/2022/06/01/cpp23-if-consteval
+ *
+ *   [turner20211227] C++ Weekly - Ep 304 - C++23's 'if consteval'
+ *     https://youtu.be/AtdlMB_n2pI
+ *
+ *   [stackoverflow20210701] What is if consteval needed for?
+ *     https://stackoverflow.com/questions/68213012/what-is-if-consteval-needed-for
+ */
