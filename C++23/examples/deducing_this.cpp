5f9bc8a7c13df4799cf589d48dfd9e7dfa686e26
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-06-04 08:38:27 +0200

    Add references for C++23's deducing this.

---
+++ C++23/examples/deducing_this.cpp
@@ -0,0 +1,15 @@
+/*
+ * References:
+ *
+ *   [berg20230309] Johan Berg: Deducing this, C++23's Explicit Object Parameter
+ *     https://youtu.be/78JQq52A0b0
+ *
+ *   [cpptutor20221207] Applications of C++23’s deducing ‘this’
+ *     https://learnmoderncpp.com/2022/12/07/applications-of-c23s-deducing-this/
+ *
+ *   [brand20220607] C++23’s Deducing this: what it is, why it is, how to use it
+ *     https://devblogs.microsoft.com/cppblog/cpp23-deducing-this/
+ *
+ *   [20220216] C++23: Deducing this
+ *     https://www.sandordargo.com/blog/2022/02/16/deducing-this-cpp23
+ */
