90b9b7db9ef9ad527c67a834056d64c582958156
Bart Vandewoestyne <bvandewoestyne@idirect.net>
2023-06-27 20:32:19 +0200

    Add reference for static initialization order fiasco.

--- C++20/idioms/static_initialization_order_fiasco.cpp
+++ C++20/idioms/static_initialization_order_fiasco.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [turner20200626] C++ Weekly - Ep 382 - The Static Initialization Order Fiasco and C++20's constinit
+ *     https://youtu.be/rEwijXgC_Kg
+ *
  *   [grimm20200720] Solving the Static Initialization Order Fiasco with C++20
  *     https://www.modernescpp.com/index.php/c-20-static-initialization-order-fiasco
  */
