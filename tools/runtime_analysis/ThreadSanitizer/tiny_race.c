3946d236c5828b78790f9b1ad60f587be9594d9e
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2023-12-04 16:03:39 +0100

    Add (NON-WORKING) example program.

---
+++ tools/runtime_analysis/ThreadSanitizer/tiny_race.c
@@ -0,0 +1,17 @@
+// clang -fsanitize=thread -g -O1 tiny_race.c
+
+// TODO: why doesn't this work?
+
+#include <pthread.h>
+int Global;
+void *Thread1(void *x) {
+  Global = 42;
+  return x;
+}
+int main() {
+  pthread_t t;
+  pthread_create(&t, NULL, Thread1, NULL);
+  Global = 43;
+  pthread_join(t, NULL);
+  return Global;
+}
