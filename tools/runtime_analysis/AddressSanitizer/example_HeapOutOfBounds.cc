d2b5fe12dc85da0ae10d65092459ccb3039afbe4
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2023-12-11 10:52:06 +0100

    Minor formatting issues.

--- tools/runtime_analysis/AddressSanitizer/example_HeapOutOfBounds.cc
+++ tools/runtime_analysis/AddressSanitizer/example_HeapOutOfBounds.cc
@@ -1,7 +1,8 @@
 // RUN: g++ -O -g -fsanitize=address example_HeapOutOfBounds.cc
 
-int main(int argc, char **argv) {
-  int *array = new int[100];
+int main(int argc, char **argv)
+{
+  int* array = new int[100];
   array[0] = 0;
   int res = array[argc + 100];  // BOOM
   delete [] array;
