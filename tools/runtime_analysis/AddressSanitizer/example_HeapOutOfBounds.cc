0c2a6a7f8dd3bfa0f306ff457f5acee43df91a3e
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2023-12-11 10:51:01 +0100

    Add remark about AddressSanitizer failing due to code being optimized away.

--- tools/runtime_analysis/AddressSanitizer/example_HeapOutOfBounds.cc
+++ tools/runtime_analysis/AddressSanitizer/example_HeapOutOfBounds.cc
@@ -5,5 +5,11 @@ int main(int argc, char **argv) {
   array[0] = 0;
   int res = array[argc + 100];  // BOOM
   delete [] array;
+
+  // Note that if you comment out this line, then AddressSanitizer does not
+  // detect the issue, because the compiler probably optimized the code away!
+  // See also https://github.com/google/sanitizers/wiki/AddressSanitizer#faq:
+  //   Q: Why didn't ASan report an obviously invalid memory access in my code?
+  //   A1: If your errors is too obvious, compiler might have already optimized it out by the time Asan runs.
   return res;
 }
