a86efe0fb543d43f91b6a61194b8bdf08ae0a7ac
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2024-10-10 00:21:04 +0200

    Remove dead link.

--- C++11/examples/non-static_data_member_initializers.cpp
+++ C++11/examples/non-static_data_member_initializers.cpp
@@ -15,9 +15,6 @@
  *   [filipek20200302] In-class Member Initialisation: From C++11 to C++20
  *     https://www.bfilipek.com/2015/02/non-static-data-members-initialization.html
  *
- *   [kalev202120823] C++11 Tutorial: New Constructor Features that Make Object Initialization Faster and Smoother
- *     https://smartbear.com/blog/c11-tutorial-new-constructor-features-that-make-ob/
- *
  *   [stackoverflow20130513] Why can in-class initializers only use = or {}?
  *     https://stackoverflow.com/questions/16329962/why-can-in-class-initializers-only-use-or
  */
