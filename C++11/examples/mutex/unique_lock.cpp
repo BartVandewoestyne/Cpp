1c8a4c81e1d634383062a0f2ebac061e8aa97253
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2024-11-19 23:19:40 +0100

    Fix YouTube link for std::unique_lock reference.

--- C++11/examples/mutex/unique_lock.cpp
+++ C++11/examples/mutex/unique_lock.cpp
@@ -2,7 +2,7 @@
  * References:
  *
  *   [brandy20170929] Curiously Recurring C++ Bugs at Facebook (CppCon 2017)
- *     https://youtu.be/3MB2iiCkGxg?t=1744
+ *     https://youtu.be/lkgszkPnV8g?si=lq9Tk5kVkAhVtV7n&t=1902
  *
  *   [geier2014] Mutex lock guards in C++11
  *     https://geidav.wordpress.com/2014/01/09/mutex-lock-guards-in-c11/
