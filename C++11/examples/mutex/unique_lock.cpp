6d7a4ab5b61c6af21237ad0bb43a39f1c28bef10
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2024-11-19 23:19:59 +0100

    Add reference for std::unique_lock.

--- C++11/examples/mutex/unique_lock.cpp
+++ C++11/examples/mutex/unique_lock.cpp
@@ -6,4 +6,7 @@
  *
  *   [geier2014] Mutex lock guards in C++11
  *     https://geidav.wordpress.com/2014/01/09/mutex-lock-guards-in-c11/
+ *
+ *   [stackoverflow20131211] std::unique_lock<std::mutex> or std::lock_guard<std::mutex>?
+ *     https://stackoverflow.com/questions/20516773/stdunique-lockstdmutex-or-stdlock-guardstdmutex
  */
