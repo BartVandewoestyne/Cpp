5026e90be08e95ff5f559a61573668ccfdce1a01
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2024-11-19 23:20:13 +0100

    Add key idea for std::unique_lock.

--- C++11/examples/mutex/unique_lock.cpp
+++ C++11/examples/mutex/unique_lock.cpp
@@ -1,4 +1,11 @@
 /*
+ * Key idea:
+ *
+ *   - std::unique_lock has more functionality then std::lock_guard.
+ *     Probably best to see if you can do things with std::lock_guard, but
+ *     if not, then you might need std::unique_lock, which might have a bit
+ *     more overhead.
+ *
  * References:
  *
  *   [brandy20170929] Curiously Recurring C++ Bugs at Facebook (CppCon 2017)
