bdf2fcfb56d5e8471add527a424e6b740986595a
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2024-11-27 21:30:08 +0100

    Add reference for NRVO.

--- C++11/examples/named_return_value_optimization.cpp
+++ C++11/examples/named_return_value_optimization.cpp
@@ -1,6 +1,9 @@
 /*
  * References:
  *
+ *   [turner] C++ Weekly - Ep 456 - RVO + Trivial Types = Faster Code
+ *     https://youtu.be/DzUAqXMUjtc?si=r85Q6wqUDdstfbyt
+ *
  *   [boccara20161128] Return value optimizations
  *     https://www.fluentcpp.com/2016/11/28/return-value-optimizations/
  */
