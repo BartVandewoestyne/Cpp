0fb16f0b751476906089e0f4382b4bffffb0157a
Bart Vandewoestyne <Bart.Vandewoestyne@gmail.com>
2025-03-10 13:01:53 +0100

    Add references for moved-from state.

---
+++ C++11/examples/utility/moved_from_state.cpp
@@ -0,0 +1,10 @@
+/*
+ * Here are some references that explain how the state of a moved-from object should
+ * still be a 'valid' state (whatever that may mean).
+ *
+ * References:
+ *
+ * https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2020/p2226r0.html#biblio-sutter
+ * https://isocpp.github.io/CppCoreGuidelines/CppCoreGuidelines#Rc-move-semantic
+ * https://herbsutter.com/2020/02/17/move-simply/
+ */
